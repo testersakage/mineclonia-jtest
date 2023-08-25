da0227dccf1e6719b7e38dadc4440d97ad0e597f
cora <coradelamouche@gmx.ch>
2023-08-25 22:54:32 +0200

    Add initial staus_effects API

---
+++ mods/CORE/mcl_status_effects/init.lua
@@ -0,0 +1,95 @@
+mcl_status_effects = {}
+mcl_status_effects.registered_effects = {}
+local effect_players = {}
+
+--[[
+{
+	on_start = function(player,def) end,
+	on_stop = function(player,def) end,
+	on_step = functionn(player,def,data,dtime) end,
+}
+
+--]]
+
+function mcl_status_effects.register_effect(name, def)
+	def.name = name
+	mcl_status_effects.registered_effects[name] = def
+	effect_players[name] = {}
+end
+
+function mcl_status_effects.start_effect(object,effect, overrides)
+	local def = table.merge(mcl_status_effects.registered_effects[effect],overrides or {})
+	if def.on_start then
+		def.on_start(object,def)
+	end
+	effect_players[effect][object] = {
+		time_started = minetest.get_gametime(),
+		duration = def.duration,
+		--particlespawner = minetest.add_particlespawner(...)
+	}
+end
+
+function mcl_status_effects.stop_effect(object,effect)
+	if not effect_players[effect][object] then return end
+	local def = mcl_status_effects.registered_effects[effect]
+	if def.on_stop then
+		def.on_stop(object, def)
+	end
+	effect_players[effect][object] = nil
+end
+
+--function mcl_status_effects.save_player_effects(player)
+
+--end
+
+--function mcl_status_effects.restore_player_effects(player)
+
+--end
+
+minetest.register_globalstep(function(dtime)
+	for effect,objects in pairs(effect_players) do
+		local def = mcl_status_effects.registered_effects[effect]
+		for object, data in pairs(objects) do
+			data.etime = ( data.etime or 0 ) + dtime
+			if data.etime > (data.duration or 0) then
+				mcl_status_effects.stop_effect(object,effect)
+			elseif def.on_step then
+				def.on_step(object, def, data, dtime)
+			end
+		end
+	end
+end)
+
+mcl_status_effects.register_effect("test_start_stop",{
+	on_start = function(obj, def)
+		minetest.log(def.name.." started for player "..obj:get_player_name())
+	end,
+	on_stop = function(obj, def)
+		minetest.log(def.name.." stopped for player "..obj:get_player_name())
+	end
+})
+
+mcl_status_effects.register_effect("test_step",{
+	on_start = function(obj, def)
+		minetest.log(def.name.." started for player "..obj:get_player_name())
+	end,
+	on_stop = function(obj, def)
+		minetest.log(def.name.." stopped for player "..obj:get_player_name())
+	end,
+	on_step = function(obj, def, data, dtime)
+		data.timer = (data.timer or 0) + dtime
+		if data.timer < 1 then return end
+		data.timer = 0
+		minetest.log(def.name.." step for player .."..obj:get_player_name())
+	end,
+	duration = 10,
+})
+
+minetest.register_chatcommand("start_effect",{
+	func = function(pn,param)
+		if mcl_status_effects.registered_effects[param] then
+			local pl = minetest.get_player_by_name(pn)
+			mcl_status_effects.start_effect(pl,param)
+		end
+	end
+})
