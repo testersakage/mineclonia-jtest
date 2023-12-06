e524d3a8fa4dff635670279f199d4a9172166df8
codiac <codiac@inbox.lv>
2023-12-06 11:46:36 +1000

    Switch from mod storage to minetest config

--- mods/CORE/mcl_world_settings/init.lua
+++ mods/CORE/mcl_world_settings/init.lua
@@ -1,86 +1,76 @@
 --[[
 TODO
 
-* UI for changes
+* UI for changes?
 * help and description localisation
-	* is it possible to handle since the translations are in a different mod?
+	* is it possible to handle since the translations may be in a different mod?
 
 	local lang = minetest.get_player_information(playername).lang_code
 	minetest.get_translated_string(lang, description)
 
-Handy for testing: storage:from_table(nil)
-
 ]]
 
-local storage = minetest.get_mod_storage()
 local S = minetest.get_translator(minetest.get_current_modname())
 
 mcl_world_settings = {}
 
+local settings = Settings(minetest.get_worldpath() .. DIR_DELIM .. "mcl_settings.conf")
+
 local mcl_world_settings_meta = {}
 
-function mcl_world_settings.register_string(name, default, description, help)
-	if not storage:contains(name) then
+function mcl_world_settings.register(name, default, description, help)
+	if not settings:get(name) then
 		minetest.log("info", S("Registering setting @1", name))
-		storage:set_string(name, default)
+		settings:set(name, default)
+		if not settings:write() then
+			minetest.log("action", S("Setting @1 could not be saved!", name))
+		end
 	end
 
 	mcl_world_settings_meta[name] = { type = "string", default = default, description = description, help = help }
 end
 
 function mcl_world_settings.register_bool(name, default, description, help)
-	if not storage:contains(name) then
-		minetest.log("info", S("Registering setting @1", name))
-		storage:set_int(name, default and 1 or 0)
+	if not settings:get_bool(name) then
+		minetest.log(S("Registering setting @1 = @2", name, tostring(default)))
+		settings:set_bool(name, default)
+		if not settings:write() then
+			minetest.log("action", S("Setting @1 could not be saved!", name))
+		end
 	end
 	mcl_world_settings_meta[name] = { type = "bool", default = default, description = description, help = help }
 end
 
-function mcl_world_settings.register_float(name, default, description, help)
-	if not storage:contains(name) then
-		minetest.log("action", S("Registering setting @1", name))
-		storage:set_float(name, default)
-	end
-	mcl_world_settings_meta[name] = { type = "float", default = default, description = description, help = help }
-end
-
-function mcl_world_settings.register_int(name, default, description, help)
-	if not storage:contains(name) then
-		minetest.log("warning", S("Registering setting @1", name))
-		storage:set_int(name, default)
-	end
-	mcl_world_settings_meta[name] = { type = "int", default = default, description = description, help = help }
-end
-
 function mcl_world_settings.unregister(name)
-	if not storage:contains(name) then
+	if not settings:get(name) then
 		minetest.log("warning", S("Can't unregister the setting @1, it has not been registered", name))
 		return
 	end
 
-	storage:set_string(name, nil)
+	if settings:remove(name) then
+		minetest.log("action", S("The setting @1 has been removed", name))
+	else
+		minetest.log("action", S("The setting @1 could not be removed", name))
+	end
 end
 
 function mcl_world_settings.get(name)
-	if not storage:contains(name) then
+	if not settings:get(name) then
 		return S("Can't get the setting @1, it has not been registered", name)
 	end
 
 	local type = mcl_world_settings_meta[name]["type"]
 
 	if type == "bool" then
-		return storage:get_int(name) and 1 or 0
-	elseif type == "int" then
-		return storage:get_int(name)
-	elseif type == "float" then
-		return storage:get_float(name)
+		return settings:get_bool(name)
 	else
-		return storage:get_string(name)
+		return settings:get(name)
 	end
 end
 
 function mcl_world_settings.set(name, value)
-	if not storage:contains(name) then
+	minetest.log("set 1")
+	if not settings:get(name) then
 		minetest.log("warning", S("Can't set the setting @1, it has not been registered", name))
 		return
 	end
@@ -88,16 +78,19 @@ function mcl_world_settings.set(name, value)
 	local type = mcl_world_settings_meta[name]["type"]
 
 	if type == "bool" then
-		storage:set_int(name, tonumber(value) and 1 or 0)
-	elseif type == "int" then
-		storage:set_int(name, tonumber(value))
-	elseif type == "float" then
-		storage:set_float(name, tonumber(value))
+		if value == "false" or value == "0" then
+			value = false
+		end
+		settings:set_bool(name, value)
 	else
-		storage:set_string(name, value)
+		settings:set(name, value)
 	end
 
-	minetest.log("action", S("Setting @1 is set to @2", name, value))
+	if settings:write() then
+		minetest.log("info", S("Setting @1 is set to @2", name, tostring(value)))
+	else
+		minetest.log(S("warning", "Setting @1 could not be saved!", name, tostring(value)))
+	end
 end
 
 minetest.register_chatcommand("world_settings", {
@@ -106,22 +99,19 @@ minetest.register_chatcommand("world_settings", {
 	privs = { server = true },
 	func = function(name, param)
 		if param == "" then
-			for _, key in pairs(storage:get_keys()) do
+			for _, key in pairs(settings:get_names()) do
 				minetest.chat_send_player(name, string.format("%s: %s", key, mcl_world_settings.get(key)))
 			end
 		elseif param == "info" then
-			for _, key in pairs(storage:get_keys()) do
+			for _, key in pairs(settings:get_names()) do
 				minetest.chat_send_player(
 					name,
 					string.format("%s: %s", key, S(mcl_world_settings_meta[key]["description"]))
 				)
 			end
 		elseif param == "help" then
-			for _, key in pairs(storage:get_keys()) do
-				minetest.chat_send_player(
-					name,
-					string.format("%s: %s", key, S(mcl_world_settings_meta[key]["help"]))
-				)
+			for _, key in pairs(settings:get_names()) do
+				minetest.chat_send_player(name, string.format("%s: %s", key, S(mcl_world_settings_meta[key]["help"])))
 			end
 		else
 			local sparam = param:split(" ")
