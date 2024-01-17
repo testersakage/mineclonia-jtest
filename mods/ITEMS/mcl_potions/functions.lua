6b758356e6869f4f9e565502d4fe4873f530e339
the-real-herowl <wiktor_t-i@proton.me>
2024-01-17 01:38:24 +0100

    Added blindness effect
    (also improved TT descriptions of some effects)

--- mods/ITEMS/mcl_potions/functions.lua
+++ mods/ITEMS/mcl_potions/functions.lua
@@ -375,7 +375,7 @@ mcl_potions.register_effect({
 	name = "levitation",
 	description = S("Levitation"),
 	get_tt = function(factor)
-		return S("moves you upwards at @1 nodes/s", factor)
+		return S("moves body upwards at @1 nodes/s", factor)
 	end,
 	on_step = function(dtime, object, factor, duration)
 		local vel = object:get_velocity().y
@@ -416,7 +416,7 @@ mcl_potions.register_effect({
 	name = "health_boost",
 	description = S("Health Boost"),
 	get_tt = function(factor)
-		return S("increases HP by @1", factor)
+		return S("HP increased by @1", factor)
 	end,
 	res_condition = function(object)
 		return (not object:is_player())
@@ -538,8 +538,86 @@ mcl_potions.register_effect({
 })
 
 mcl_potions.register_effect({
-	name = "food_poisoning",
-	description = S("Food Poisoning"),
+[<<<<<<< HEAD
+=======
+	name = "frost",
+	description = S("Frost"),
+	get_tt = function(factor)
+		return S("-1 HP / 1 s, can kill, -@1% running speed", math.floor(factor*100))
+	end,
+	res_condition = function(object)
+		return (not object:is_player())
+	end,
+	on_start = function(object, factor)
+		mcl_burning.extinguish(object)
+		playerphysics.add_physics_factor(object, "speed", "mcl_potions:frost", 1-factor)
+		EF.frost[object].vignette = object:hud_add({
+			hud_elem_type = "image",
+			position = {x = 0.5, y = 0.5},
+			scale = {x = -101, y = -101},
+			text = "mcl_potions_frost_hud.png",
+			z_index = -400
+		})
+	end,
+	on_hit_timer = function(object, factor, duration)
+		if object:is_player() or object:get_luaentity() then
+			mcl_util.deal_damage(object, 1, {type = "magic"})
+		end
+	end,
+	on_end = function(object)
+		playerphysics.remove_physics_factor(object, "speed", "mcl_potions:frost")
+		if not EF.frost[object] then return end
+		object:hud_remove(EF.frost[object].vignette)
+	end,
+	particle_color = "#5B7DAA",
+	uses_factor = true,
+	lvl1_factor = 0.1,
+	lvl2_factor = 0.2,
+	timer_uses_factor = false,
+	hit_timer_step = 1,
+	damage_modifier = "is_fire",
+	modifier_func = function(damage, effect_vals)
+		effect_vals.timer = effect_vals.dur
+		return 0
+	end,
+})
+
+mcl_potions.register_effect({
+	name = "blindness",
+	description = "Blindness",
+	get_tt = function(factor)
+		return S("impaired sight")
+	end,
+	res_condition = function(object)
+		return (not object:is_player())
+	end,
+	on_start = function(object, factor)
+		EF.blindness[object].vignette = object:hud_add({
+			hud_elem_type = "image",
+			position = {x = 0.5, y = 0.5},
+			scale = {x = -101, y = -101},
+			text = "mcl_potions_blindness_hud.png",
+			z_index = -401
+		})
+		mcl_fovapi.apply_modifier(object, "mcl_potions:blindness")
+	end,
+	on_end = function(object)
+		mcl_fovapi.remove_modifier(object, "mcl_potions:blindness")
+		if not EF.blindness[object] then return end
+		object:hud_remove(EF.blindness[object].vignette)
+	end,
+	particle_color = "#686868",
+	uses_factor = false,
+})
+mcl_fovapi.register_modifier({
+	name = "mcl_potions:blindness",
+	fov_factor = 0.6,
+	time = 1,
+})
+
+mcl_potions.register_effect({
+	name = "hunger",
+	description = S("Hunger"),
 	get_tt = function(factor)
 		return S("exhausts by @1 per second", factor)
 	end,
