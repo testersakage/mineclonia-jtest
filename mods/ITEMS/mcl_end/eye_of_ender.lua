0bc01dbb16f7fc23d14766d777b62518883b4681
the-real-herowl <wiktor_t-i@proton.me>
2024-03-24 05:46:01 +0100

    Further (mcl_)luck functionality
    * XP from mob breeding impacted by luck
    * eye of ender explosion chance impacted by luck
    * fishing loot impacted by luck
    * melee critical damage impacted by luck
    * also fixed 2 scripts marked as "executable"

--- mods/ITEMS/mcl_end/eye_of_ender.lua
+++ mods/ITEMS/mcl_end/eye_of_ender.lua
@@ -30,8 +30,8 @@ minetest.register_entity("mcl_end:ender_eye", {
 		self._age = self._age + dtime
 		if self._age >= 3 then
 			-- End of life
-			local r = math.random(1,5)
-			if r == 1 then
+			local r = math.random(1,15) + self._luck
+			if r <= 3 then
 				-- 20% chance to get destroyed completely.
 				-- 100% if in Creative Mode
 				self.object:remove()
@@ -81,7 +81,8 @@ local function throw_eye(itemstack, user)
 	origin.y = origin.y + 1.5
 	local strongholds = mcl_structures.registered_structures["end_shrine"].static_pos
 	local dim = mcl_worlds.pos_to_dimension(origin)
-	local is_creative = minetest.is_creative_enabled(user:get_player_name())
+	local player_name = user:get_player_name()
+	local is_creative = minetest.is_creative_enabled(player_name)
 
 	-- Just drop the eye of ender if there are no strongholds
 	if #strongholds <= 0 or dim ~= "overworld" then
@@ -117,6 +118,8 @@ local function throw_eye(itemstack, user)
 	local obj = minetest.add_entity(origin, "mcl_end:ender_eye")
 	if not obj or not obj:get_pos() then return end
 	local dir
+	local ent = obj:get_luaentity()
+	ent._luck = mcl_luck.get_luck(player_name)
 
 	if lowest_dist <= 25 then
 		local velocity = 4
