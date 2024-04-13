9350754879a9357614d371bdf1ca5b693e86fc63
the-real-herowl <wiktor_t-i@proton.me>
2024-04-14 01:31:04 +0200

    Typos fixed

--- mods/ITEMS/mcl_potions/potions.lua
+++ mods/ITEMS/mcl_potions/potions.lua
@@ -628,7 +628,7 @@ mcl_potions.register_potion({
 	name = "resistance",
 	desc_suffix = S("of Resistance"),
 	_tt = nil,
-	_longdesc = S("Descreases damage taken."),
+	_longdesc = S("Decreases damage taken."),
 	color = "#2552A5",
 	_effect_list = {
 		resistance = {},
@@ -640,7 +640,7 @@ mcl_potions.register_potion({
 	name = "resistance",
 	desc_suffix = S("of Resistance"),
 	_tt = nil,
-	_longdesc = S("Descreases damage taken."),
+	_longdesc = S("Decreases damage taken."),
 	color = "#2552A5",
 	_effect_list = {
 		resistance = {},
@@ -652,7 +652,7 @@ mcl_potions.register_potion({
 	name = "turtle_master",
 	desc_suffix = S("of Turtle Master"),
 	_tt = nil,
-	_longdesc = S("Descreases damage taken at the cost of speed."),
+	_longdesc = S("Decreases damage taken at the cost of speed."),
 	color = "#255235",
 	_effect_list = {
 		resistance = {
