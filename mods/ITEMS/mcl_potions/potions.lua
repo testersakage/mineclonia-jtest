bd5a4930049dd64c41cf20a44346e2d83e2fce9f
the-real-herowl <wiktor_t-i@proton.me>
2024-04-17 22:52:25 +0200

    Added some potion recipes
    Also:
    * changed the duration of saturation and food poisoning potions
    * minor code changes

--- mods/ITEMS/mcl_potions/potions.lua
+++ mods/ITEMS/mcl_potions/potions.lua
@@ -736,7 +736,7 @@ mcl_potions.register_potion({
 	_longdesc = S("Moves bowels too fast."),
 	color = "#83A061",
 	_effect_list = {
-		food_poisoning = {},
+		food_poisoning = {dur=mcl_potions.DURATION_POISON},
 	},
 	has_arrow = true,
 	-- TODO implement effect stacking?
@@ -749,7 +749,7 @@ mcl_potions.register_potion({
 	_longdesc = S("Satisfies hunger."),
 	color = "#CEAE29",
 	_effect_list = {
-		saturation = {},
+		saturation = {dur=mcl_potions.DURATION_POISON},
 	},
 	has_arrow = true,
 })
@@ -789,9 +789,7 @@ mcl_potions.register_potion({
 		"^mcl_potions_potion_bottle.png",
 	}),
 	_effect_list = {
-		bad_omen = {
-			dur = 6000,
-		},
+		bad_omen = {dur = 6000},
 	},
 	has_splash = false,
 	has_lingering = false,
