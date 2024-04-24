c14341db78fc83036f4dc97d78bccea08f949994
cora <coradelamouche@gmx.ch>
2024-04-24 21:50:59 +0200

    Remove expensive easteregg

--- mods/ENTITIES/mcl_mobs/movement.lua
+++ mods/ENTITIES/mcl_mobs/movement.lua
@@ -1061,7 +1061,6 @@ function mob_class:check_smooth_rotation(dtime)
 			yaw = yaw + (math.random() * 2 - 1) * 5 * dtime
 		end
 		self.object:set_yaw(yaw)
-		self:update_roll()
 	end
 	-- end rotation
 end
