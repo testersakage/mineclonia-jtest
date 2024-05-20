e704278d6c996d9d472a69161446305d9b1aa17f
DragonWrangler1 <dragonwrangler1@noreply.codeberg.org>
2024-05-20 23:05:51 +0000

    Partially redo the code including mob damage
    also more accurate and with a temp model and texture

--- mods/ENTITIES/mcl_charges/init.lua
+++ mods/ENTITIES/mcl_charges/init.lua
@@ -1,33 +1,11 @@
-local cooldown_time = 1 -- Cooldown
+local cooldown_time = 1
 mcl_charges_cooldown_data = {}
 	
 	local S = minetest.get_translator("mcl_charges")
 	
-	--=================================================--
-	--===Wind Explosion Function For the Wind Charge===--
-	--===Based on some code from the tnt mod for mtg===--
-	--=================================================--
-	
 	local function add_effects(pos, radius, drops)
-	--[[minetest.add_particlespawner({
-		amount = 64,
-		time = 0.5,
-		minpos = vector.subtract(pos, radius / 2),
-		maxpos = vector.add(pos, radius / 2),
-		minvel = vector.new(-10, -10, -10),
-		maxvel = vector.new(10, 10, 10),
-		minacc = vector.new(),
-		maxacc = vector.new(),
-		minexptime = 1,
-		maxexptime = 2.5,
-		minsize = radius * 1,
-		maxsize = radius * 3,
-		texture = "mcl_particles_smoke.png",
-	})]]
-
-	-- we just dropped some items. Look at the items entities and pick
-	-- one of them to use as texture
-	local texture = "mcl_particles_smoke.png" --fallback texture
+	
+	local texture = "mcl_particles_smoke.png"
 	local most = 0
 	
 	minetest.add_particlespawner({
@@ -48,8 +26,8 @@ mcl_charges_cooldown_data = {}
 end
 	
 	
-	local function calc_velocity(pos1, pos2, old_vel, power)
-	-- Avoid errors caused by a vector of zero length
+	local function wind_burst_velocity(pos1, pos2, old_vel, power)
+
 	if vector.equals(pos1, pos2) then
 		return old_vel
 	end
@@ -58,22 +36,18 @@ end
 	vel = vector.normalize(vel)
 	vel = vector.multiply(vel, power)
 
-	-- Divide by distance
 	local dist = vector.distance(pos1, pos2)
 	dist = math.max(dist, 1)
 	vel = vector.divide(vel, dist)
 
-	-- Add old velocity
 	vel = vector.add(vel, old_vel)
 
-	-- randomize it a bit
 	vel = vector.add(vel, {
 		x = math.random() - 0.5,
 		y = math.random() - 0.5,
 		z = math.random() - 0.5,
 	})
 
-	-- Limit to terminal velocity
 	dist = vector.length(vel)
 	if dist > 250 then
 		vel = vector.divide(vel, dist / 250)
@@ -83,7 +57,7 @@ end
 local RADIUS = 4
 local damage_radius = (RADIUS / math.max(1, RADIUS)) * RADIUS
 local radius = 2
-local function entity_physics(pos, radius--[[, drops]])
+local function wind_burst(pos, radius)
 	local objs = minetest.get_objects_inside_radius(pos, radius)
 	for _, obj in pairs(objs) do
 		local obj_pos = obj:get_pos()
@@ -92,13 +66,13 @@ local function entity_physics(pos, radius--[[, drops]])
 		local damage = (0 / dist) * radius
 		if obj:is_player() then
 			local dir = vector.normalize(vector.subtract(obj_pos, pos))
-			local moveoff = vector.multiply(dir, math.random(2, 2.5) / dist * RADIUS)
+			local moveoff = vector.multiply(dir, math.random(1.5, 1.8) / dist * RADIUS)
 			obj:add_velocity(moveoff)
 
 			obj:set_hp(obj:get_hp() - damage)
 		else
 			local luaobj = obj:get_luaentity()
-			-- object might have disappeared somehow
+			
 			if luaobj then
 				local do_damage = false
 				local do_knockback = true
@@ -111,7 +85,7 @@ local function entity_physics(pos, radius--[[, drops]])
 
 				if do_knockback then
 					local obj_vel = obj:get_velocity()
-					obj:set_velocity(calc_velocity(pos, obj_pos, obj_vel, radius * 1))
+					obj:set_velocity(wind_burst_velocity(pos, obj_pos, obj_vel, radius * 3))
 				end
 				if do_damage then
 					if not obj:get_armor_groups().immortal then
@@ -128,12 +102,6 @@ local function entity_physics(pos, radius--[[, drops]])
 		end
 	end
 end
-	
-	--=================--
-	--===Wind Charge===--
-	--=================--
-
-
 
 local function mcl_charge(name, descr, def)
  	minetest.register_craftitem("mcl_charges:" .. name .. "", {
@@ -156,7 +124,7 @@ local player_name = placer:get_player_name()
       local ig_time = minetest.get_gametime()
       if ig_time - mcl_charges_cooldown_data[playername] >= cooldown_time then
        mcl_charges_cooldown_data[playername] = ig_time
-			--weapons_shot(itemstack, placer, pointed_thing, def.velocity, name)
+       
 			local velocity = 30
 			local dir = placer:get_look_dir()
 			local playerpos = placer:get_pos()
@@ -193,7 +161,6 @@ local ent = obj:get_luaentity() ; ent.posthrow = playerpos
       if ig_time - mcl_charges_cooldown_data[playername] >= cooldown_time then
        mcl_charges_cooldown_data[playername] = ig_time
 
-			--weapons_shot(itemstack, placer, pointed_thing, def.velocity, name)
 			local velocity = 30
 			local dir = placer:get_look_dir()
 			local playerpos = placer:get_pos()
@@ -222,13 +189,12 @@ local ent = obj:get_luaentity() ; ent.posthrow = playerpos
 	end
 		end,
 		_on_dispense = function(stack, pos, droppos, dropnode, dropdir)
-		-- Throw fire charge
 		local shootpos = vector.add(pos, vector.multiply(dropdir, 0.51))
 		local wind_charge = minetest.add_entity(shootpos, "mcl_charges:".. name .. "_flying")
 		if wind_charge and wind_charge:get_pos() then
 			local ent_wind = wind_charge:get_luaentity()
 			ent_wind._shot_from_dispenser = true
-			local v = ent_wind.velocity or 10
+			local v = ent_wind.velocity or 20
 			wind_charge:set_velocity(vector.multiply(dropdir, v))
 			ent_wind.switch = 1
 		end
@@ -236,64 +202,99 @@ local ent = obj:get_luaentity() ; ent.posthrow = playerpos
 	end,
 })
 
-
-	minetest.register_entity("mcl_charges:" .. name .. "_flying", {
-	
-
+minetest.register_entity("mcl_charges:" .. name .. "_flying", {
+		
 		initial_properties = {
 
-			textures = {"mcl_charges_" .. name .. ".png"},
+			visual = "mesh",
+			mesh = "wind_charge_test3.obj",
+			visual_size = {x=2, y=1.5},
+			textures = {"mcl_charges_" .. name .. "_test.png"},
 			hp_max = 20,
-			collisionbox = {-0.1,-0.1,-0.1, 0.1,0.1,0.1},
-			--collide_with_objects = true,
-			collisiondetection = true,
+			collisionbox = {-0.1,-0.1,-0.1, 0.1,0.0,0.1},
+			collide_with_objects = true,
 		},
+		
+	hit_player = def.hit_player,
+	hit_mob = def.hit_mob,
+
+	on_activate = def.on_activate,
+
+
 
 		on_step = function(self, dtime)
 
 			local pos = self.object:get_pos()
 			local node = minetest.get_node(pos)
 			local n = node.name
-
+			
 			if n ~= "air" then
 				def.hit_node(self, pos)
 				self.object:remove()
-			end
 		end
+		if self.hit_player or self.hit_mob or self.hit_object then
+				for _,player in pairs(minetest.get_objects_inside_radius(pos, 0.9)) do
+					if self.hit_player and player:is_player() then
+						self.hit_player(self, player)
+						return
+					end
+
+					local entity = player:get_luaentity()
+					if entity then
+						if self.hit_mob	and entity.is_mob then
+							self.hit_mob(self, player)
+							def.hit_mob_alt(self, pos)
+							self.object:remove()
+							return
+						end
+
+						if self.hit_object and (not entity.is_mob) and tostring(player) ~= self.owner_id and entity.name ~= self.object:get_luaentity().name then
+							self.hit_object(self, player)
+							def.hit_player_alt(self, pos)
+							self.object:remove()
+							return
+						end
+					end
+				end
+			end
+			self.lastpos = pos
+		end,
 	})
+
+
 end
+
 
 mcl_charge("wind_charge", "Wind Charge", {
 
-	hit_player = function(self, player)
-	entity_physics(pos, damage_radius)
+
+
+	hit_player = mcl_mobs.get_arrow_damage_func(0, "fireball"),
+	hit_mob = mcl_mobs.get_arrow_damage_func(6, "fireball"),
+
+	hit_node = function(self, pos, node)
+	wind_burst(pos, damage_radius)
 	add_effects(pos, radius, drops)
-	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance = 8 }, true)
+	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance }, true)
 	end,
-
-	hit_mob = function(self, player)
-	entity_physics(pos, damage_radius)
-	add_effects(pos, radius, drops)
-	player:punch(self.object, 1.0, {
-			full_punch_interval = 1.0,
-			damage_groups = {fleshy = 6}
-		})
-
-	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance = 8 }, true)
+	hit_player_alt = function(self, pos)
+	local pos_dir = {x = pos.x, dir, y = pos.y, dir, z = pos.z, dir}
+	wind_burst(pos_dir, damage_radius)
+	add_effects(pos_dir, radius, drops)
+	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance }, true)
 	end,
-	hit_node = function(self, pos)
-	entity_physics(pos, damage_radius)
+	hit_mob_alt = function(self, pos)
+	wind_burst(pos, damage_radius)
 	add_effects(pos, radius, drops)
-	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance = 8 }, true)
-	end
-
-})
+	minetest.sound_play("tnt_explode", { pos = pos, gain = 1.0, max_hear_distance }, true)
+	end,
 	
-
-
-minetest.register_craft({
-	output = "mcl_charges:wind_charge 4",
-	type = "shapeless",
-	recipe = {"mcl_mobitems:breeze_rod"}
+	on_activate = function(self, staticdata)
+        	self.object:set_armor_groups({immortal = 1})
+       			minetest.after(3, function()
+				if self.object:get_luaentity() then
+        				self.object:remove()
+				end
+		end)
+end,
 })
-
