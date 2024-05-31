29aeb729e43687c18fa8cbe61af47a9606b0c299
DragonWrangler1 <dragonwrangler1@noreply.codeberg.org>
2024-05-31 00:36:12 +0000

    blacklist iron doors from being opened and closed by wind charge

--- mods/ITEMS/mcl_doors/api_doors.lua
+++ mods/ITEMS/mcl_doors/api_doors.lua
@@ -314,7 +314,9 @@ end
 		on_rightclick = on_rightclick,
 		_on_wind_charge_hit = function(pos)
 			local node = minetest.get_node(pos)
+			if not node.name == "mcl_doors:iron_door_b_1" then
 				on_mesecons_signal_open(pos, node)
+			end
 			return true
 		end,
 
@@ -390,7 +392,9 @@ end
 		on_rightclick = on_rightclick,
 		_on_wind_charge_hit = function(pos)
 			local node = minetest.get_node(pos)
+			if not node.name == "mcl_doors:iron_door_t_1" then
 				on_mesecons_signal_open_top(pos, node)
+			end
 			return true
 		end,
 
@@ -468,7 +472,9 @@ end
 		on_rightclick = on_rightclick,
 		_on_wind_charge_hit = function(pos)
 			local node = minetest.get_node(pos)
+			if not node.name == "mcl_doors:iron_door_b_2" then
 				on_mesecons_signal_close(pos, node)
+			end
 			return true
 		end,
 
@@ -544,7 +550,9 @@ end
 		on_rightclick = on_rightclick,
 		_on_wind_charge_hit = function(pos)
 			local node = minetest.get_node(pos)
+			if not node.name == "mcl_doors:iron_door_t_2" then
 				on_mesecons_signal_close_top(pos, node)
+			end
 			return true
 		end,
 
