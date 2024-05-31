afbb6b1d94d1a98015e471db558d6877b21e91f9
cora <coradelamouche@gmx.ch>
2024-05-31 02:59:23 +0200

    Cleanup mcl_mobs/items.lua, fix mobs picking up and wearing armor

--- mods/ENTITIES/mcl_mobs/init.lua
+++ mods/ENTITIES/mcl_mobs/init.lua
@@ -119,6 +119,7 @@ mcl_mobs.mob_class = {
 	attack_exception = function(p) return false end,
 	player_active_range = tonumber(minetest.settings:get("mcl_mob_active_range")) or 48,
 	persist_in_peaceful = true,
+	wears_armor = false,
 
 	_mcl_fishing_hookable = true,
 	_mcl_fishing_reelable = true,
