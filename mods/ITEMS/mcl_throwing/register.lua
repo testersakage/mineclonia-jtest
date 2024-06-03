c77e6b0cd1c4e68e7eaf99921c4e38a61a20fbe3
cora <coradelamouche@gmx.ch>
2024-06-03 04:39:54 +0200

    mcl_throwing: fix crash when thrower isn't a player

--- mods/ITEMS/mcl_throwing/register.lua
+++ mods/ITEMS/mcl_throwing/register.lua
@@ -66,7 +66,7 @@ local function check_object_hit(self, pos, dmg)
 				self.object:remove()
 				return true
 			elseif (entity.is_mob == true or entity._hittable_by_projectile) and (self._thrower ~= object) then
-				local pl = minetest.get_player_by_name(self._thrower)
+				local pl = self._thrower and self._thrower.is_player and self._thrower or type(self._thrower) == "string" and minetest.get_player_by_name(self._thrower)
 				if pl then
 					object:punch(pl, 1.0, {
 						full_punch_interval = 1.0,
