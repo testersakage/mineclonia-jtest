5372cda2902f862c26c206e4256d4cde3661229d
cora <coradelamouche@gmx.ch>
2024-06-05 17:27:54 +0200

    Detach player before teleporting with ender pearl

--- mods/ITEMS/mcl_throwing/register.lua
+++ mods/ITEMS/mcl_throwing/register.lua
@@ -181,6 +181,11 @@ local function egg_on_step(self, dtime)
 	self._lastpos={x=pos.x, y=pos.y, z=pos.z} -- Set lastpos-->Node will be added at last pos outside the node
 end
 
+local function pearl_tp(player, pos)
+	player:set_pos(pos)
+	player:set_hp(player:get_hp() - 5, { type = "fall", from = "mod" })
+end
+
 -- Movement function of ender pearl
 local function pearl_on_step(self, dtime)
 	self.timer = self.timer + dtime
@@ -259,13 +264,17 @@ local function pearl_on_step(self, dtime)
 				end
 
 				local oldpos = player:get_pos()
-				-- Teleport and hurt player
-				player:set_pos(telepos)
-				player:set_hp(player:get_hp() - 5, { type = "fall", from = "mod" })
+				-- Teleport and hurt player, detach if attached
+				if player:get_attach() then
+					mcl_util.detach_object(player, false, function(player)
+						pearl_tp(player, telepos)
+					end)
+				else
+					pearl_tp(player, telepos)
+				end
 
 				-- 10% chance to spawn endermite at the player's origin
-				local r = math.random(1,10)
-				if r == 1 then
+				if math.random(10) == 1 then
 					minetest.add_entity(oldpos, "mobs_mc:endermite")
 				end
 
