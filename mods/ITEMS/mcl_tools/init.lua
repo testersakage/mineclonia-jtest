3e79c706ae0a9a108f7865bdb030e6ef59b3c742
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-07-20 12:41:09 -0300

    Tool set is global

--- mods/ITEMS/mcl_tools/init.lua
+++ mods/ITEMS/mcl_tools/init.lua
@@ -2,31 +2,35 @@
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 local S = minetest.get_translator(modname)
+
 mcl_tools = {}
 
--- Help texts
-local long_descs = {
-	["pick"] = S("Pickaxes are mining tools to mine hard blocks, such as stone. A pickaxe can also be used as weapon, but it is rather inefficient."),
-	["axe"] = S("An axe is your tool of choice to cut down trees, wood-based blocks and other blocks. Axes deal a lot of damage as well, but they are rather slow."),
-	["sword"] = S("Swords are great in melee combat, as they are fast, deal high damage and can endure countless battles. Swords can also be used to cut down a few particular blocks, such as cobwebs."),
-	["shovel"] = S("Shovels are tools for digging coarse blocks, such as dirt, sand and gravel. They can also be used to turn grass blocks to grass paths. Shovels can be used as weapons, but they are very weak."),
-	["hoe"] = S("Hoes are essential tools for growing crops. They are used to create farmland in order to plant seeds on it. Hoes can also be used as very weak weapons in a pinch.")
-}
-
-local diggroups = {
-	["pick"] = { pickaxey = {} },
-	["shovel"] = { shovely = {} },
-	["axe"] = { axey = {} },
-	["sword"] = { swordy = {}, swordy_cobweb = {} },
-	["hoe"] = { hoey = {} }
-}
-
-local groups = {
-	["pick"] = { tool = 1, pickaxe = 1 },
-	["shovel"] = { tool = 1, shovel = 1 },
-	["axe"] = { tool = 1, axe = 1 },
-	["sword"] = { weapon = 1, sword = 1 },
-	["hoe"] = { tool = 1, hoe = 1 }
+mcl_tools.set = {
+	["axe"] = {
+		longdesc = S("An axe is your tool of choice to cut down trees, wood-based blocks and other blocks. Axes deal a lot of damage as well, but they are rather slow."),
+		groups = { axe = 1, tool = 1 },
+		diggroups = { axey = {} }
+	},
+	["hoe"] = {
+		longdesc = S("Hoes are essential tools for growing crops. They are used to create farmland in order to plant seeds on it. Hoes can also be used as very weak weapons in a pinch."),
+		groups = { hoe = 1, tool = 1 },
+		diggroups = { hoey = {} }
+	},
+	["pick"] = {
+		longdesc = S("Pickaxes are mining tools to mine hard blocks, such as stone. A pickaxe can also be used as weapon, but it is rather inefficient."),
+		groups = { pickaxe = 1, tool = 1 },
+		diggroups = { pickaxey = {} }
+	},
+	["shovel"] = {
+		longdesc = S("Shovels are tools for digging coarse blocks, such as dirt, sand and gravel. They can also be used to turn grass blocks to grass paths. Shovels can be used as weapons, but they are very weak."),
+		groups = { shovel = 1, tool = 1 },
+		diggroups = { shovely = {} }
+	},
+	["sword"] = {
+		longdesc = S("Swords are great in melee combat, as they are fast, deal high damage and can endure countless battles. Swords can also be used to cut down a few particular blocks, such as cobwebs."),
+		groups = { sword = 1, weapon = 1 },
+		diggroups = { swordy = {}, swordy_cobweb = {} }
+	}
 }
 
 local shears_longdesc = S("Shears are tools to shear sheep and to mine a few block types. Shears are a special mining tool and can be used to obtain the original item from grass, leaves and similar blocks that require cutting.")
@@ -84,7 +88,7 @@ for _,tool in pairs({"shovel","shears","axe","sword","pick"}) do
 end
 
 local function get_tool_diggroups(material, toolname)
-	local diggroups = diggroups[toolname]
+	local diggroups = mcl_tools.set[toolname].diggroups
 
 	for _, diggroup in pairs(diggroups) do
 		diggroup.speed = material.speed
@@ -118,9 +122,9 @@ function mcl_tools.register_set(material, tools, overrides)
 		})
 
 		minetest.register_tool(toolname, table.merge({
-			_doc_items_longdesc = long_descs[tool],
+			_doc_items_longdesc = mcl_tools.set[tool].longdesc,
 			wield_scale = wield_scale,
-			groups = table.merge(groups[tool], material.groups),
+			groups = table.merge(mcl_tools.set[tool].groups, material.groups),
 			sound = { breaks = "default_tool_breaks" },
 			on_place = mcl_tools.tool_place_funcs[tool],
 			_repair_material = material.material,
@@ -151,18 +155,18 @@ function mcl_tools.register_set(material, tools, overrides)
 				minetest.register_craft({
 					output = toolname,
 					recipe = {
-						{ material.material, material.material, "" },
-						{ material.material, "mcl_core:stick", "" },
-						{ "", "mcl_core:stick", "" }
+						{ material.material, material.material },
+						{ material.material, "mcl_core:stick" },
+						{ "", "mcl_core:stick" }
 					},
 				})
 
 				minetest.register_craft({
 					output = toolname,
 					recipe = {
-						{ "", material.material, material.material, },
-						{ "", "mcl_core:stick", material.material },
-						{ "", "mcl_core:stick", "" }
+						{ material.material, material.material, },
+						{ "mcl_core:stick", material.material },
+						{ "mcl_core:stick", "" }
 					}
 				})
 			elseif tool == "shovel" then
@@ -178,18 +182,18 @@ function mcl_tools.register_set(material, tools, overrides)
 				minetest.register_craft({
 					output = toolname,
 					recipe = {
-						{ material.material, material.material, "" },
-						{ "", "mcl_core:stick", "" },
-						{ "", "mcl_core:stick", "" }
+						{ material.material, material.material },
+						{ "", "mcl_core:stick" },
+						{ "", "mcl_core:stick" }
 					},
 				})
 
 				minetest.register_craft({
 					output = toolname,
 					recipe = {
-						{ "", material.material, material.material, },
-						{ "", "mcl_core:stick", "" },
-						{ "", "mcl_core:stick", "" }
+						{ material.material, material.material, },
+						{ "mcl_core:stick", "" },
+						{ "mcl_core:stick", "" }
 					}
 				})
 			end
