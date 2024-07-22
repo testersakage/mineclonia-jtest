87dd2092efbea7373990fbd8adc9f5e22ccd2bff
DragonWrangler1 <dragonwrangler1@noreply.codeberg.org>
2024-07-22 16:12:26 +0000

    fix entities other than mobs, items, and players being moved by the wind charge

--- mods/ENTITIES/mcl_charges/init.lua
+++ mods/ENTITIES/mcl_charges/init.lua
@@ -15,7 +15,7 @@ local S = minetest.get_translator("mcl_charges")
 wind_burst_spawner = {
 	texture = "mcl_charges_wind_burst_1.png",
 	texpool = {},
-	amount = 6,
+	--amount = 6,
 	time = 0.2,
 	minvel = vector.zero(),
 	maxvel = vector.zero(),
@@ -91,22 +91,28 @@ end
 
 local RADIUS = 4
 
--- Wind Burst registry
 function mcl_charges.wind_burst(pos, radius)
 	for _, obj in ipairs(minetest.get_objects_inside_radius(pos, radius)) do
 		local obj_pos = obj:get_pos()
 		local dist = math.max(1, vector.distance(pos, obj_pos))
 
 		if obj:is_player() then
-			obj:add_velocity(vector.multiply(vector.normalize(vector.subtract(obj_pos, pos)), math.random(1.8, 2.0) / dist * RADIUS))
+			obj:add_velocity(vector.multiply(vector.normalize(vector.subtract(obj_pos, pos)), math.random(1.8, 2.0) / dist * radius))
 		else
 			local luaobj = obj:get_luaentity()
-			if luaobj and (not minetest.registered_entities[luaobj.name].on_blast or minetest.registered_entities[luaobj.name].on_blast(luaobj, 0)) then
-				obj:set_velocity(mcl_charges.wind_burst_velocity(pos, obj_pos, obj:get_velocity(), radius * 3))
+			if luaobj then
+				local is_builtin_item = luaobj.name == "__builtin:item"
+				if luaobj.is_mob or is_builtin_item then
+					local entity_def = minetest.registered_entities[luaobj.name]
+					if not entity_def.on_blast or entity_def.on_blast(luaobj, 0) then
+						obj:set_velocity(mcl_charges.wind_burst_velocity(pos, obj_pos, obj:get_velocity(), radius * 3))
+					end
+				end
 			end
 		end
 	end
 end
+
 --throwable charge registry
 function register_charge(name, descr, def)
 	minetest.register_craftitem("mcl_charges:" .. name .. "", {
