b26ee4b0f1847d17d54067937d5206760bf83fe5
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-07-24 09:48:46 -0300

    More clean up on mcl_fences

--- mods/ITEMS/mcl_fences/init.lua
+++ mods/ITEMS/mcl_fences/init.lua
@@ -1,29 +1,29 @@
-local S = minetest.get_translator(minetest.get_current_modname())
+local S = minetest.get_translator("mcl_fences")
+
+mcl_fences = {}
+
+-- Node box
+local p = { -0.125, -0.5, -0.125, 0.125, 0.5, 0.125 }
+local x1 = { -0.5, 0.25, -0.0625, -0.125, 0.4375, 0.0625 }
+local x12 = { -0.5, -0.125, -0.0625, -0.125, 0.0625, 0.0625 }
+local x2 = { 0.125, 0.25, -0.0625, 0.5, 0.4375, 0.0625 }
+local x22 = { 0.125, -0.125, -0.0625, 0.5, 0.0625, 0.0625 }
+local z1 = { -0.0625, 0.25, -0.5, 0.0625, 0.4375, -0.125 }
+local z12 = { -0.0625, -0.125, -0.5, 0.0625, 0.0625, -0.125 }
+local z2 = { -0.0625, 0.25, 0.125, 0.0625, 0.4375, 0.5 }
+local z22 = { -0.0625, -0.125, 0.125, 0.0625, 0.0625, 0.5 }
+
+-- Collision box
+local cp = { -0.125, -0.5, -0.125, 0.125, 1.01, 0.125 }
+local cx1 = { -0.5, -0.5, -0.125, -0.125, 1.01, 0.125 }
+local cx2 = { 0.125, -0.5, -0.125, 0.5, 1.01, 0.125 }
+local cz1 = { -0.125, -0.5, -0.5, 0.125, 1.01, -0.125 }
+local cz2 = { -0.125, -0.5, 0.125, 0.125, 1.01, 0.5 }
 
 local on_rotate
 if minetest.get_modpath("screwdriver") then
 	on_rotate = screwdriver.rotate_simple
 end
-
--- Node box
-local p = {-2/16, -0.5, -2/16, 2/16, 0.5, 2/16}
-local x1 = {-0.5, 4/16, -1/16, -2/16, 7/16, 1/16}   --oben(quer) -x
-local x12 = {-0.5, -2/16, -1/16, -2/16, 1/16, 1/16} --unten(quer) -x
-local x2 = {2/16, 4/16, -1/16, 0.5, 7/16, 1/16}   --oben(quer) x
-local x22 = {2/16, -2/16, -1/16, 0.5, 1/16, 1/16} --unten(quer) x
-local z1 = {-1/16, 4/16, -0.5, 1/16, 7/16, -2/16}   --oben(quer) -z
-local z12 = {-1/16, -2/16, -0.5, 1/16, 1/16, -2/16} --unten(quer) -z
-local z2 = {-1/16, 4/16, 2/16, 1/16, 7/16, 0.5}   --oben(quer) z
-local z22 = {-1/16, -2/16, 2/16, 1/16, 1/16, 0.5} --unten(quer) z
-
--- Collision box
-local cp = {-2/16, -0.5, -2/16, 2/16, 1.01, 2/16}
-local cx1 = {-0.5, -0.5, -2/16, -2/16, 1.01, 2/16} --unten(quer) -x
-local cx2 = {2/16, -0.5, -2/16, 0.5, 1.01, 2/16} --unten(quer) x
-local cz1 = {-2/16, -0.5, -0.5, 2/16, 1.01, -2/16} --unten(quer) -z
-local cz2 = {-2/16, -0.5, 2/16, 2/16, 1.01, 0.5} --unten(quer) z
-
-mcl_fences = {}
 
 local function update_gate(pos, node)
 	if node.name:sub(-5) == "_open" then
@@ -34,87 +34,70 @@ local function update_gate(pos, node)
 	minetest.set_node(pos, node)
 end
 
-local function play_sound(pos, definitions)
-	local spec = definitions.spec
-	local gain = definitions.gain
+local function play_sound(pos, node, state)
+	local defs = minetest.registered_nodes[node.name]
+	local sounddefs = defs._mcl_fences_sounds[state]
+	local spec = sounddefs.spec or ("doors_fencegate_"..state)
+	local gain = sounddefs.gain or 0.3
 	minetest.sound_play(spec, { gain = gain, max_hear_distance = 16, pos = pos }, true)
 end
 
-local function punch_gate(pos, node)
+local function punch_gate(pos, node, clicker)
 	local meta = minetest.get_meta(pos)
 	local state = meta:get_int("state")
-	-- Needs repair
-	--local defs = minetest.registered_nodes[node.name]
-	local sounddefs = {}
 
 	if state == 1 then
 		state = 0
-		sounddefs.spec = "doors_fencegate_close"
-		sounddefs.gain = 0.3
-		play_sound(pos, sounddefs)
+		play_sound(pos, node, "close")
 	else
 		state = 1
-		sounddefs.spec = "doors_fencegate_open"
-		sounddefs.gain = 0.3
-		play_sound(pos, sounddefs)
+		play_sound(pos, node, "open")
 	end
 	update_gate(pos, node)
 	meta:set_int("state", state)
 end
 
-local tpl_fence = {
-	_doc_items_longdesc = S("Fences are structures which block the way. Fences will connect to each other and solid blocks. They cannot be jumped over with a simple jump."),
+local function handle_textures(block, definitions)
+    local tiles = definitions.tiles[1]
+    local texture = "mcl_fences_"..block.."_mask.png^"..tiles..
+        "^mcl_fences_"..block.."_mask.png^[makealpha:255,126,126"
+
+    if not definitions.inventory_image then
+        definitions.inventory_image = texture
+    end
+
+    if not definitions.wield_image then
+        definitions.wield_image = texture
+    end
+end
+
+local tpl_fences = {
+    _doc_items_longdesc = S("Fences are structures which block the way. Fences will connect to each other and solid blocks. They cannot be jumped over with a simple jump."),
 	paramtype = "light",
 	is_ground_content = false,
 	connect_sides = { "front", "back", "left", "right" },
 	sunlight_propagates = true,
-	drawtype = "nodebox"
-}
-
-local tpl_fence_gate_open = {
-	paramtype = "light",
-	paramtype2 = "facedir",
-	is_ground_content = false,
-	sunlight_propagates = true,
-	walkable = false,
 	drawtype = "nodebox",
-	node_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -3/16, -1/16, -6/16, 0.5, 1/16},   --links abschluss
-			{6/16, -3/16, -1/16, 0.5, 0.5, 1/16},   --rechts abschluss
-			{-0.5, 4/16, 1/16, -6/16, 7/16, 6/16},   --oben-links(quer) x
-			{-0.5, -2/16, 1/16, -6/16, 1/16, 6/16}, --unten-links(quer) x
-			{6/16, 4/16, 1/16, 0.5, 7/16, 0.5},   --oben-rechts(quer) x
-			{6/16, -2/16, 1/16, 0.5, 1/16, 0.5}, --unten-rechts(quer) x
-			{-0.5, -2/16, 6/16, -6/16, 7/16, 0.5},  --mitte links
-			{6/16, 1/16, 0.5, 0.5, 4/16, 6/16},  --mitte rechts
-		}
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-				{-0.5, -3/16, -1/16, 0.5, 0.5, 1/16},   --gate
-			}
-	},
-	on_rightclick = function(pos, node, clicker)
-		punch_gate(pos, node)
-	end,
-	mesecons = {effector = {
-		action_off = (function(pos, node)
-			punch_gate(pos, node)
-		end),
-	}},
-	on_rotate = on_rotate,
-	_on_wind_charge_hit = function(pos)
-		local node = minetest.get_node(pos)
-			punch_gate(pos, node)
-		return true
-	end
+    node_box = {
+        type = "connected",
+        fixed = { p },
+        connect_front = { z1, z12 },
+        connect_back = { z2, z22 },
+        connect_left = { x1, x12 },
+        connect_right = { x2, x22 }
+    },
+    collision_box = {
+        type = "connected",
+        fixed = { cp },
+        connect_front = { cz1 },
+        connect_back = { cz2 },
+        connect_left = { cx1 },
+        connect_right = { cx2 }
+    }
 }
 
-local tpl_fence_gate_close = {
-	_tt_help = S("Openable by players and redstone power"),
+local tpl_fence_gates = {
+    _tt_help = S("Openable by players and redstone power"),
 	_doc_items_longdesc = S("Fence gates can be opened or closed and can't be jumped over. Fences will connect nicely to fence gates."),
 	_doc_items_usagehelp = S("Right-click the fence gate to open or close it."),
 	paramtype = "light",
@@ -125,27 +108,23 @@ local tpl_fence_gate_close = {
 	node_box = {
 		type = "fixed",
 		fixed = {
-			{-0.5, -3/16, -1/16, -6/16, 0.5, 1/16},   --links abschluss
-			{6/16, -3/16, -1/16, 0.5, 0.5, 1/16},   --rechts abschluss
-			{-2/16, -2/16, -1/16, 0, 7/16, 1/16},  --mitte links
-			{0, -2/16, -1/16, 2/16, 7/16, 1/16},  --mitte rechts
-			{-0.5, 4/16, -1/16, -2/16, 7/16, 1/16},   --oben(quer) -z
-			{-0.5, -2/16, -1/16, -2/16, 1/16, 1/16}, --unten(quer) -z
-			{2/16, 4/16, -1/16, 0.5, 7/16, 1/16},   --oben(quer) z
-			{2/16, -2/16, -1/16, 0.5, 1/16, 1/16}, --unten(quer) z
+			{ -0.5, -0.1875, -0.0625, -0.375, 0.5, 0.0625 },
+			{ 0.375, -0.1875, -0.0625, 0.5, 0.5, 0.0625 },
+			{ -0.125, -0.125, -0.0625, 0, 0.4375, 0.0625 },
+			{ 0, -0.125, -0.0625, 0.125, 0.4375, 0.0625 },
+			{ -0.5, 0.25, -0.0625, -0.125, 0.4375, 0.0625 },
+			{ -0.5, -0.125, -0.0625, -0.125, 0.0625, 0.0625 },
+			{ 0.125, 0.25, -0.0625, 0.5, 0.4375, 0.0625 },
+			{ 0.125, -0.125, -0.0625, 0.5, 0.0625, 0.0625 }
 		}
 	},
 	collision_box = {
 		type = "fixed",
-		fixed = {
-			{-0.5, -3/16, -2/16, 0.5, 1, 2/16},   --gate
-		}
+		fixed = {{ -0.5, -0.1875, -0.125, 0.5, 1, 0.125 }}
 	},
 	selection_box = {
 		type = "fixed",
-		fixed = {
-			{-0.5, -3/16, -1/16, 0.5, 0.5, 1/16},   --gate
-		}
+		fixed = {{ -0.5, -0.1875, -0.0625, 0.5, 0.5, 0.0625 }}
 	},
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
@@ -167,52 +146,67 @@ local tpl_fence_gate_close = {
 	end
 }
 
+local tpl_fence_gates_open = {
+	paramtype = "light",
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	walkable = false,
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{ -0.5, -0.1875, -0.0625, -0.375, 0.5, 0.0625 },
+			{ 0.375, -0.1875, -0.0625, 0.5, 0.5, 0.0625 },
+			{ -0.5, 0.25, 0.0625, -0.375, 0.4375, 0.375 },
+			{ -0.5, -0.125, 0.0625, -0.375, 0.0625, 0.375 },
+			{ 0.375, 0.25, 0.0625, 0.5, 0.4375, 0.5 },
+			{ 0.375, -0.125, 0.0625, 0.5, 0.0625, 0.5 },
+			{ -0.5, -0.125, 0.375, -0.375, 0.4375, 0.5 },
+			{ 0.375, 0.0625, 0.5, 0.5, 0.25, 0.375 }
+		}
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {{ -0.5, -0.1875, -0.0625, 0.5, 0.5, 0.0625 }}
+	},
+	on_rightclick = function(pos, node, clicker)
+		punch_gate(pos, node)
+	end,
+	mesecons = {effector = {
+		action_off = (function(pos, node)
+			punch_gate(pos, node)
+		end),
+	}},
+	on_rotate = on_rotate,
+	_on_wind_charge_hit = function(pos)
+		local node = minetest.get_node(pos)
+			punch_gate(pos, node)
+		return true
+	end
+}
+
 function mcl_fences.register_fence(name, definitions)
-	definitions.groups.deco_block = 1
-	definitions.groups.fence = 1
+    local fence_name = "mcl_fences:"..name
 
-	if definitions.connects_to == nil then
-		definitions.connects_to = {}
-	else
-		definitions.connects_to = table.copy(definitions.connects_to)
-	end
+    if not definitions.groups then
+        definitions.groups = {}
+        definitions.groups.fence = 1
+        definitions.groups.deco_block = 1
+    end
 
-	if definitions.tiles and definitions.tiles[1] then
-		if not definitions.inventory_image then
-			definitions.inventory_image = "mcl_fences_fence_mask.png^"..definitions.tiles[1].."^mcl_fences_fence_mask.png^[makealpha:255,126,126"
-		end
+    if not definitions.connects_to then
+        definitions.connects_to = { fence_name, "group:fence", "group:fence_gate", "group:solid" }
+    end
 
-		if not definitions.wield_image then
-			definitions.wield_image = "mcl_fences_fence_mask.png^"..definitions.tiles[1].."^mcl_fences_fence_mask.png^[makealpha:255,126,126"
-		end
-	end
+    if definitions.tiles and definitions.tiles[1] then
+        handle_textures("fence", definitions)
+    end
 
-	local fence_id = "mcl_fences:"..name
-	table.insert(definitions.connects_to, "group:solid")
-	table.insert(definitions.connects_to, "group:fence_gate")
-	table.insert(definitions.connects_to, fence_id)
+    minetest.register_node(":"..fence_name, table.merge(tpl_fences, definitions))
 
-	minetest.register_node(":"..fence_id, table.merge(tpl_fence, {
-		node_box = {
-			type = "connected",
-			fixed = {p},
-			connect_front = {z1,z12},
-			connect_back = {z2,z22,},
-			connect_left = {x1,x12},
-			connect_right = {x2,x22},
-		},
-		collision_box = {
-			type = "connected",
-			fixed = {cp},
-			connect_front = {cz1},
-			connect_back = {cz2,},
-			connect_left = {cx1},
-			connect_right = {cx2},
-		},
-	}, definitions))
-
-	if definitions._mcl_fences_baseitem then
-		local stick = "mcl_core:stick"
+    if definitions._mcl_fences_baseitem then
+        local stick = "mcl_core:stick"
 		local material = definitions._mcl_fences_baseitem
 		local amount = definitions._mcl_fences_output_amount or 3
 
@@ -221,63 +215,48 @@ function mcl_fences.register_fence(name, definitions)
 		end
 
 		minetest.register_craft({
-			output = fence_id.." "..tostring(amount),
+			output = fence_name.." "..tostring(amount),
 			recipe = {
 				{ material, stick, material },
 				{ material, stick, material }
 			}
 		})
-	end
+    end
 
-	return fence_id
+    return fence_name
 end
 
 function mcl_fences.register_fence_gate(name, definitions)
-	definitions.groups.fence_gate = 1
-	definitions.groups.deco_block = 1
+    local fence_gate_name = "mcl_fences:"..name.."_gate"
+    local fence_gate_name_open = fence_gate_name.."_open"
 
-	local gate_id = "mcl_fences:"..name.."_gate"
-	local open_gate_id = gate_id .. "_open"
-
-	local opendefs = table.copy(definitions)
-	local cgroups = table.copy(opendefs.groups)
-
-	opendefs.description = nil
-	opendefs.inventory_image = nil
-	opendefs.wield_image = nil
-	opendefs._mcl_burntime = nil
-
-	cgroups.mesecon_ignore_opaque_dig = 1
-	cgroups.mesecon_effector_on = 1
-	cgroups.fence_gate = 1
-	cgroups.not_in_creative_inventory = 1
-
-	minetest.register_node(":"..open_gate_id, table.merge(tpl_fence_gate_open, {
-		groups = cgroups,
-		drop = gate_id,
-	}, opendefs))
-
-	local cgroups_closed = table.copy(definitions.groups)
-	cgroups_closed.mesecon_effector_on = nil
-	cgroups_closed.mesecon_effector_off = nil
-
-	if definitions.tiles and definitions.tiles[1] then
-		if not definitions.inventory_image then
-			definitions.inventory_image = "mcl_fences_fence_gate_mask.png^"..definitions.tiles[1]..
-			"^mcl_fences_fence_gate_mask.png^[makealpha:255,126,126"
-
-		end
-		if not definitions.wield_image then
-			definitions.wield_image = "mcl_fences_fence_gate_mask.png^"..definitions.tiles[1]..
-			"^mcl_fences_fence_gate_mask.png^[makealpha:255,126,126"
-		end
+	if not definitions.groups then
+		definitions.groups = {}
+		definitions.groups.fence_gate = 1
+		definitions.groups.deco_block = 1
 	end
 
-	minetest.register_node(":"..gate_id, table.merge(tpl_fence_gate_close, {
-		groups = cgroups_closed,
-	}, definitions))
+	if definitions.tiles and definitions.tiles[1] then
+		handle_textures("fence_gate", definitions)
+	end
 
-	if definitions._mcl_fences_baseitem then
+    minetest.register_node(":"..fence_gate_name, table.merge(tpl_fence_gates, definitions))
+
+	local opendefinitions = table.copy(definitions)
+	opendefinitions.description = nil
+	opendefinitions.inventory_image = nil
+	opendefinitions.wield_image = nil
+	opendefinitions._mcl_burntime = nil
+
+	opendefinitions.groups.not_in_creative_inventory = 1
+	opendefinitions.mesecon_ignore_opaque_dig = 1
+	opendefinitions.mesecon_effector_on = 1
+
+    minetest.register_node(":"..fence_gate_name_open, table.merge(tpl_fence_gates_open, {
+		drop = fence_gate_name
+	}, opendefinitions))
+
+    if definitions._mcl_fences_baseitem then
 		local stick = "mcl_core:stick"
 		local material = definitions._mcl_fences_baseitem
 		local amount = definitions._mcl_fences_output_amount or 1
@@ -287,7 +266,7 @@ function mcl_fences.register_fence_gate(name, definitions)
 		end
 
 		minetest.register_craft({
-			output = gate_id.." "..tostring(amount),
+			output = fence_gate_name.." "..tostring(amount),
 			recipe = {
 				{ stick, material, stick },
 				{ stick, material, stick }
@@ -296,17 +275,17 @@ function mcl_fences.register_fence_gate(name, definitions)
 	end
 
 	if minetest.get_modpath("doc") then
-		doc.add_entry_alias("nodes", gate_id, "nodes", open_gate_id)
+		doc.add_entry_alias("nodes", fence_gate_name, "nodes", fence_gate_name_open)
 	end
 
-	return gate_id, open_gate_id
+	return fence_gate_name, fence_gate_name_open
 end
 
-function mcl_fences.register_fence_and_fence_gate(id, fence_name, fence_gate_name, texture_fence, groups, hardness, blast_resistance, connects_to, sounds, sound_open, sound_close, sound_gain_open, sound_gain_close, texture_fence_gate)
-	if texture_fence_gate == nil then
-		texture_fence_gate = texture_fence
-	end
-	local fence_id = mcl_fences.register_fence(id, fence_name, texture_fence, groups, hardness, blast_resistance, connects_to, sounds)
-	local gate_id, open_gate_id = mcl_fences.register_fence_gate(id, fence_gate_name, texture_fence_gate, groups, hardness, blast_resistance, sounds, sound_open, sound_close, sound_gain_open, sound_gain_close)
-	return fence_id, gate_id, open_gate_id
+function mcl_fences.register_fence_and_fence_gate(name, commondefs, fencedefs, gatedefs)
+	local fence, gate, gate_open
+
+	fence = mcl_fences.register_fence(name, table.merge(commondefs, fencedefs))
+	gate, gate_open = mcl_fences.register_fence_gate(name, table.merge(commondefs, gatedefs))
+
+	return fence, gate, gate_open
 end
--- mods/ITEMS/mclx_fences/init.lua
+++ mods/ITEMS/mclx_fences/init.lua
@@ -1,17 +1,32 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 local extra_nodes = minetest.settings:get_bool("mcl_extra_nodes", true)
 
--- Red Nether Brick Fence
-mcl_fences.register_fence("red_nether_brick_fence", {
-	description = S("Red Nether Brick Fence"),
+-- Red Nether Brick Fence and Red Nether Brick Fence Gate
+mcl_fences.register_fence_and_fence_gate("red_nether_brick_fence", {
 	tiles = { "mcl_fences_fence_red_nether_brick.png" },
 	groups = { pickaxey = 1, fence_nether_brick = 1, not_in_creative_inventory = not extra_nodes and 1 or 0 },
-	connects_to = { "group:fence_nether_brick" },
 	sounds = mcl_sounds.node_sound_stone_defaults(),
-	_mcl_blast_resistance = 6,
-	_mcl_hardness = 2,
+	_mcl_blast_resistance = minetest.registered_nodes["mcl_nether:red_nether_brick"]._mcl_blast_resistance,
+	_mcl_hardness = minetest.registered_nodes["mcl_nether:red_nether_brick"]._mcl_hardness,
+	_mcl_fences_baseitem = "mcl_nether:red_nether_brick",
+	_mcl_fences_stickreplacer = "mcl_nether:netherbrick",
+}, {
+	description = S("Red Nether Brick Fence"),
+	connects_to = { "group:fence_nether_brick" },
+}, {
+	description = S("Red Nether Brick Fence Gate"),
+	_mcl_fences_sounds = {
+		open = {
+			spec = "mcl_fences_nether_brick_fence_gate_open"
+		},
+		close = {
+			spec = "mcl_fences_nether_brick_fence_gate_close"
+		}
+	},
+	_mcl_fences_output_amount = 2
 })
 
+-- Nether Brick Fence Gate
 mcl_fences.register_fence_gate("nether_brick_fence", {
 	description = S("Nether Brick Fence Gate"),
 	tiles = { "mcl_fences_fence_gate_nether_brick.png" },
@@ -31,27 +46,6 @@ mcl_fences.register_fence_gate("nether_brick_fence", {
 	_mcl_fences_stickreplacer = "mcl_nether:netherbrick",
 	_mcl_fences_output_amount = 2
 })
-
--- Crafting
-
-if extra_nodes then
-
-	minetest.register_craft({
-		output = "mclx_fences:red_nether_brick_fence_gate",
-		recipe = {
-			{"mcl_nether:nether_wart_item", "mcl_nether:red_nether_brick", "mcl_nether:netherbrick"},
-			{"mcl_nether:netherbrick", "mcl_nether:red_nether_brick", "mcl_nether:nether_wart_item"},
-		}
-	})
-	minetest.register_craft({
-		output = "mclx_fences:red_nether_brick_fence_gate",
-		recipe = {
-			{"mcl_nether:netherbrick", "mcl_nether:red_nether_brick", "mcl_nether:nether_wart_item"},
-			{"mcl_nether:nether_wart_item", "mcl_nether:red_nether_brick", "mcl_nether:netherbrick"},
-		}
-	})
-end
-
 
 -- Aliases for mcl_supplemental
 minetest.register_alias("mcl_supplemental:red_nether_brick_fence", "mclx_fences:red_nether_brick_fence")
