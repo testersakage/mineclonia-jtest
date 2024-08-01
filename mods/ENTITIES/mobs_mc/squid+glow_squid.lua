621e7b535138f4ab22cb216b7a65bea15c12a3dc
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-08-01 20:45:34 -0300

    Merge squid with glow squid

--- mods/ENTITIES/mobs_mc/squid+glow_squid.lua
+++ mods/ENTITIES/mobs_mc/squid+glow_squid.lua
@@ -1,7 +1,8 @@
---MCmobs v0.4
---maikerumine
---made for MC like Survival game
---License for code WTFPL and otherwise stated in readmes
+-- v1.1
+
+--###################
+--################### SQUID
+--###################
 
 local S = minetest.get_translator("mobs_mc")
 
@@ -22,42 +23,38 @@ local base_psdef = {
 	collisiondetection = true,
 	collision_removal = true,
 }
+
 local psdefs = {}
+
 for i=1,4 do
 	local p = table.copy(base_psdef)
 	p.texture = "extra_mobs_glow_squid_glint"..i..".png"
 	table.insert(psdefs,p)
 end
 
-mcl_mobs.register_mob("mobs_mc:glow_squid", {
-	description = S("Glow Squid"),
+local squid_defs = {
 	type = "animal",
 	spawn_class = "water",
-	can_despawn = true,
-	passive = true,
-	hp_min = 10,
-	hp_max = 10,
-	xp_min = 1,
-	xp_max = 3,
-	armor = 100,
-	rotate = 0,
+    can_despawn = true,
+    passive = true,
+    hp_min = 10,
+    hp_max = 10,
+    xp_min = 1,
+    xp_max = 3,
+    armor = 100,
 	spawn_in_group_min = 2,
-	spawn_in_group = 4,
-	-- tilt_swim breaks the animations.
-	--tilt_swim = true,
-	-- FIXME: If the qlow squid is near the floor, it turns black
+    spawn_in_group = 4,
+	-- FIXME: If the squid is near the floor, it turns black
 	collisionbox = { -0.4, 0.0, -0.4, 0.4, 0.9, 0.4 },
 	visual = "mesh",
-	mesh = "extra_mobs_glow_squid.b3d",
-	textures = {
-		{ "extra_mobs_glow_squid.png" }
-	},
+    mesh = "mobs_mc_squid.b3d",
 	sounds = {
-		damage = { name = "mobs_mc_squid_hurt", gain = 0.3 },
-		death = { name = "mobs_mc_squid_death", gain = 0.4 },
+		damage = {name="mobs_mc_squid_hurt", gain=0.3},
+		death = {name="mobs_mc_squid_death", gain=0.4},
 		flop = "mobs_mc_squid_flop",
+		-- TODO: sounds: random
 		distance = 16,
-	},
+    },
 	animation = {
 		stand_start = 1,
 		stand_end = 60,
@@ -65,13 +62,6 @@ mcl_mobs.register_mob("mobs_mc:glow_squid", {
 		walk_end = 60,
 		run_start = 1,
 		run_end = 60,
-	},
-	drops = {
-		{ name = "mcl_mobitems:glow_ink_sac",
-		  chance = 1,
-		  min = 1,
-		  max = 3,
-		  looting = "common", },
 	},
 	visual_size = { x = 3, y = 3 },
 	makes_footstep_sound = false,
@@ -83,11 +73,59 @@ mcl_mobs.register_mob("mobs_mc:glow_squid", {
 	fear_height = 4,
 	fly = true,
 	fly_in = { "mcl_core:water_source", "mclx_core:river_water_source" },
-		-- don't add "mcl_core:water_flowing", or it won't move vertically.
+}
 
+mcl_mobs.register_mob("mobs_mc:squid", table.merge(squid_defs, {
+	description = S("Squid"),
+	textures = {
+        { "mobs_mc_squid.png" }
+    },
+    drops = {
+		{
+			name = "mcl_mobitems:ink_sac",
+			chance = 1,
+			min = 1,
+			max = 3,
+			looting = "common"
+		}
+	}
+}))
+
+mcl_mobs.register_mob("mobs_mc:glow_squid", table.merge(squid_defs, {
+	description = S("Glow Squid"),
+	textures = {
+		{ "extra_mobs_glow_squid.png" }
+	},
+	drops = {
+		{
+			name = "mcl_mobitems:glow_ink_sac",
+			chance = 1,
+			min = 1,
+			max = 3,
+			looting = "common"
+		}
+	},
 	glow = minetest.LIGHT_MAX,
 	particlespawners = psdefs,
+}))
+
+-- TODO: Behaviour: squirt
+
+-- Spawn near the water surface
+mcl_mobs.spawn_setup({
+	name = "mobs_mc:squid",
+	type_of_spawning = "water",
+	dimension = "overworld",
+	min_height = mobs_mc.water_level - 16,
+	max_height = mobs_mc.water_level + 1,
+	min_light = 0,
+	max_light = minetest.LIGHT_MAX + 1,
+	aoc = 7,
+	chance = 80,
 })
+
+-- spawn eggs
+mcl_mobs.register_egg("mobs_mc:squid", S("Squid"), "#223b4d", "#708999", 0)
 
 -- spawning
 mcl_mobs.spawn_setup({
