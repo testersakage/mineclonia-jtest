a941d7468bab6244430750b8681d35d2577129b3
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-08-12 08:32:01 -0300

    More mclx_fences

--- mods/ITEMS/mclx_fences/init.lua
+++ mods/ITEMS/mclx_fences/init.lua
@@ -47,6 +47,50 @@ mcl_fences.register_fence_gate_def("nether_brick_fence", {
 	_mcl_fences_output_amount = 2
 })
 
+for wood, defs in pairs(mcl_trees.woods) do
+	local groups = {
+		handy = 1, axey = 1, material_wood = 1, fence_wood = 1,
+		flammable = 3, fire_encouragement = 5, fire_flammability = 20
+	}
+
+	local basedefs = minetest.registered_nodes["mcl_trees:bark_"..wood]
+
+	if defs.bark and defs.bark.groups then
+		groups = defs.bark.groups
+		groups.fence_wood = 1
+	end
+
+	mcl_fences.register_fence_def("bark_"..wood, {
+		description = S("@1 Wood Fence", defs.readable_name),
+		connects_to = { "group:fence_wood", "group:solid" },
+		groups = groups,
+		sounds = mcl_sounds.node_sound_wood_defaults(),
+		tiles = basedefs.tiles,
+		_on_axe_place = mcl_trees.strip_tree,
+		_mcl_blast_resistance = basedefs._mcl_blast_resistance,
+		_mcl_hardness = basedefs._mcl_hardness,
+		_mcl_burntime = basedefs._mcl_burntime,
+		_mcl_stripped_variant = "mcl_fences:bark_stripped_"..wood,
+		_mcl_fences_baseitem = "mcl_trees:tree_"..wood,
+		_mcl_fences_output_amount = 8
+	})
+
+	basedefs = minetest.registered_nodes["mcl_trees:bark_stripped_"..wood]
+
+	mcl_fences.register_fence_def("bark_stripped_"..wood, {
+		description = S("Stripped @1 Wood Fence", defs.readable_name),
+		connects_to = { "group:fence_wood", "group:solid" },
+		groups = groups,
+		sounds = mcl_sounds.node_sound_wood_defaults(),
+		tiles = basedefs.tiles,
+		_mcl_blast_resistance = basedefs._mcl_blast_resistance,
+		_mcl_hardness = basedefs._mcl_hardness,
+		_mcl_burntime = basedefs._mcl_burntime,
+		_mcl_fences_baseitem = "mcl_trees:stripped_"..wood,
+		_mcl_fences_output_amount = 8
+	})
+end
+
 -- Aliases for mcl_supplemental
 minetest.register_alias("mcl_supplemental:red_nether_brick_fence", "mclx_fences:red_nether_brick_fence")
 
