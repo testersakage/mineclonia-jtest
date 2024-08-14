b4c1b6f050b2c69bfa5a4138a1ab5ddcba94d6cf
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-08-14 11:19:01 -0300

    (Need fixes) Implementing correct behavior pt2

--- mods/ITEMS/mcl_bundles/init.lua
+++ mods/ITEMS/mcl_bundles/init.lua
@@ -1,37 +1,91 @@
 local S = minetest.get_translator("mcl_bundles")
+local MAX_STACK_POINTS = 256
+
+local function get_stack_points(itemstack)
+    local count = itemstack:get_count()
+    local stack_max = itemstack:get_stack_max()
+    return (64 / stack_max) * count
+end
+
+local function check_for_room(bundlestack, itemstack)
+    local meta = bundlestack:get_meta()
+    local stack_points = meta:get_int("stack_points") or 0
+    local new_sp = stack_points + get_stack_points(itemstack)
+    if new_sp <= MAX_STACK_POINTS then
+        meta:set_int("stack_points", new_sp)
+        return true
+    end
+    return false
+end
+
+local function entity_to_stack(pointed_thing)
+    if pointed_thing.type == "object" then
+        local lentity = pointed_thing.ref:get_luaentity()
+        if lentity and lentity.name == "__builtin:item" then
+            if lentity.itemstring then
+                return ItemStack(lentity.itemstring), lentity
+            end
+        end
+    end
+    return nil
+end
+
+local function is_filled(bundlestack)
+    return bundlestack:get_name():find("_filled")
+end
+
+local function check_room_on_inv(player, itemstring)
+    local inv = player:get_inventory()
+    local stack = ItemStack(itemstring)
+    return inv and inv:room_for_item("main", stack), inv
+end
+
+local function fill_bundle(bundlestack, itemstack, player)
+    local old_name = bundlestack:get_name()
+    local data = minetest.deserialize(bundlestack:get_metadata())
+    table.insert(data or {}, itemstack)
+    local room, inv = check_room_on_inv(player, old_name)
+    if not is_filled(bundlestack) then
+        if room then inv:add_item("main", ItemStack(old_name.."_filled")) end
+        bundlestack:take_item()
+    end
+    bundlestack:set_metadata(minetest.serialize(data))
+    return bundlestack
+end
+
+local function unfill_bundle(bundlestack, player)
+    local old_name = bundlestack:get_name()
+    local inv = player:get_inventory()
+    local items = minetest.deserialize(bundlestack:get_metadata())
+    if items then
+        for _, item in pairs(items) do
+            local stack = ItemStack(item)
+            if inv:room_for_item("main", stack) then
+                inv:add_item("main", stack)
+            else
+                minetest.add_item(player:get_pos(), stack)
+            end
+        end
+    end
+    bundlestack:set_name(old_name:gsub("_filled", ""))
+    return bundlestack
+end
 
 local function use_bundle(itemstack, placer, pointed_thing)
-    local stack_name = itemstack:get_name()
-    local is_filled = stack_name:find("_filled")
-    local player_inv = placer:get_inventory()
-    if pointed_thing.type ~= "object" then
-        if is_filled then
-            local pos = pointed_thing.under or placer:get_pos()
-            local data = minetest.deserialize(itemstack:get_metadata())
-            for _, itemstring in pairs(data) do
-                minetest.add_item(pos, ItemStack(itemstring))
-            end
-            itemstack:take_item()
-            player_inv:add_item("main", ItemStack(stack_name:gsub("_filled", "")))
+    local item_stack, lentity = entity_to_stack(pointed_thing)
+    if item_stack then
+        if check_for_room(itemstack, item_stack) then
+            lentity._removed = true
+            return fill_bundle(itemstack, item_stack, placer)
         end
-        return itemstack
     end
-    local lentity = pointed_thing.ref:get_luaentity()
-    if lentity and lentity.name == "__builtin:item" then
-        local data, new_stack
-        lentity._removed = true
-        if not is_filled then
-            data = {}
-            table.insert(data, lentity.itemstring)
-            new_stack = ItemStack(stack_name.."_filled")
-            new_stack:set_metadata(minetest.serialize(data))
-            player_inv:add_item("main", new_stack)
-            itemstack:take_item()
-        else
-            data = minetest.deserialize(itemstack:get_metadata())
-            table.insert(data, lentity.itemstring)
-            itemstack:set_metadata(minetest.serialize(data))
+    if pointed_thing.type ~= "node" then
+        if is_filled(itemstack) then
+            return unfill_bundle(itemstack, placer)
         end
+    else
+        local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
+        if rc then return rc end
     end
     return itemstack
 end
