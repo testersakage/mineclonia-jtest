302196c14d76218b2ff5ce9cc5037feae805c47a
cora <coradelamouche@gmx.ch>
2024-08-16 08:13:14 +0200

    Add script in tools/ to only optipng files if >50 bytes are saved

---
+++ tools/opticmp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cutoff=50
+tmp=$(mktemp -d)
+find ../mods/ -name "*.png"| while read f; do
+	of=$(basename $f)
+	cp $f $tmp/$of
+	optipng -quiet -strip all -o7 $tmp/$of
+	s1=$(cat $f|wc -c)
+	s2=$(cat $tmp/$of |wc -c)
+	saved=$(echo "$s1 - $s2" |bc) 
+	if [ $saved -gt $cutoff ]; then
+		mv $tmp/$of $f
+		echo $f $saved bytes saved
+	fi
+done
+
+rm -rf $tmp
