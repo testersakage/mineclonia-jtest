32f818388fe511cfec089e4dafc8d5bcd3cb88ea
codiac <codiac@inbox.lv>
2024-08-21 16:50:10 +1000

    Fix new WT sometimes not having trades

--- mods/ENTITIES/mobs_mc/villagers/wandering_trader.lua
+++ mods/ENTITIES/mobs_mc/villagers/wandering_trader.lua
@@ -141,7 +141,7 @@ function wandering_trader:on_rightclick(clicker)
 	local name = clicker:get_player_name()
 	self._trading_players[name] = true
 
-	if self._trade_xp == nil then
+	if self._trade_xp == nil or self._trades == nil then
 		self._max_trade_tier = 5
 		self._trade_xp = 255
 		self:init_trades(get_wandering_trades())
