thatguyatrix/SandBlocks

Patch mcl_farming for pickblock

mods/ITEMS/mcl_farming/beetroot.lua
@@ -36,6 +36,7 @@ minetest.register_node("mcl_farming:beetroot_0", {
 	groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1},
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
+	_vl_pickblock = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot_1", {
@@ -60,6 +61,7 @@ minetest.register_node("mcl_farming:beetroot_1", {
 	groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1},
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
+	_vl_pickblock = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot_2", {
@@ -84,6 +86,7 @@ minetest.register_node("mcl_farming:beetroot_2", {
 	groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1},
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
+	_vl_pickblock = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot", {
@@ -135,6 +138,7 @@ minetest.register_node("mcl_farming:beetroot", {
 	groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1,beetroot=4},
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
+	_vl_pickblock = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_craftitem("mcl_farming:beetroot_item", {

mods/ITEMS/mcl_farming/carrots.lua
@@ -45,6 +45,7 @@ for i=1, 7 do
 		groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1},
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
+		_vl_pickblock = "mcl_farming:carrot_item",
 	})
 end
 
@@ -84,7 +85,8 @@ minetest.register_node("mcl_farming:carrot", {
 		min_count = 2,
 		max_count = 4,
 		cap = 5,
-	}
+	},
+	_vl_pickblock = "mcl_farming:carrot_item",
 })
 
 minetest.register_craftitem("mcl_farming:carrot_item", {

mods/ITEMS/mcl_farming/melon.lua
@@ -109,6 +109,7 @@ for s=1,7 do
 		groups = {dig_immediate=3, not_in_creative_inventory=1, plant=1,attached_node=1, dig_by_water=1,destroy_by_lava_flow=1, plant_melon_stem=s},
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
+		_vl_pickblock = "mcl_farming:melon_seeds",
 	})
 end
 
@@ -120,6 +121,7 @@ local stem_def = {
 	tiles = {"mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127"},
 	wield_image = "mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127",
 	inventory_image = "mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127",
+	_vl_pickblock = "mcl_farming:melon_seeds",
 }
 
 -- Register stem growth

mods/ITEMS/mcl_farming/potatoes.lua
@@ -49,6 +49,7 @@ for i=1, 7 do
 		groups = {dig_immediate=3, not_in_creative_inventory=1,plant=1,attached_node=1,dig_by_water=1,destroy_by_lava_flow=1,dig_by_piston=1},
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
+		_vl_pickblock = "mcl_farming:potato_item",
 	})
 end
 
@@ -89,7 +90,8 @@ minetest.register_node("mcl_farming:potato", {
 		min_count = 2,
 		max_count = 4,
 		cap = 5
-	}
+	},
+	_vl_pickblock = "mcl_farming:potato_item",
 })
 
 minetest.register_craftitem("mcl_farming:potato_item", {

mods/ITEMS/mcl_farming/pumpkin.lua
@@ -79,6 +79,7 @@ for s=1,7 do
 		groups = {dig_immediate=3, not_in_creative_inventory=1, plant=1,attached_node=1, dig_by_water=1,destroy_by_lava_flow=1,},
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
+		_vl_pickblock = "mcl_farming:pumpkin_seeds",
 	})
 end
 
@@ -89,6 +90,7 @@ local stem_def = {
 	tiles = {"mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127"},
 	wield_image = "mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127",
 	inventory_image = "mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127",
+	_vl_pickblock = "mcl_farming:pumpkin_seeds",
 }
 
 -- Template for pumpkin

mods/ITEMS/mcl_farming/wheat.lua
@@ -60,6 +60,7 @@ for i=1,7 do
 			dig_by_water=1,destroy_by_lava_flow=1, dig_by_piston=1},
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
+		_vl_pickblock = "mcl_farming:wheat_seeds",
 	})
 end
 
@@ -95,7 +96,8 @@ minetest.register_node("mcl_farming:wheat", {
 		min_count = 1,
 		max_count = 6,
 		cap = 7
-	}
+	},
+	_vl_pickblock = "mcl_farming:wheat_seeds",
 })
 
 mcl_farming:add_plant("plant_wheat", "mcl_farming:wheat", {"mcl_farming:wheat_1", "mcl_farming:wheat_2", "mcl_farming:wheat_3", "mcl_farming:wheat_4", "mcl_farming:wheat_5", "mcl_farming:wheat_6", "mcl_farming:wheat_7"}, 25, 20)

testersakage/mineclonia-jtest

mods/ITEMS/mcl_farming/beetroot.lua
@@ -43,6 +43,7 @@ minetest.register_node("mcl_farming:beetroot_0", {
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
 	_on_bone_meal = on_bone_meal,
+	_mcl_basenode = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot_1", {
@@ -68,6 +69,7 @@ minetest.register_node("mcl_farming:beetroot_1", {
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
 	_on_bone_meal = on_bone_meal,
+	_mcl_basenode = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot_2", {
@@ -93,6 +95,7 @@ minetest.register_node("mcl_farming:beetroot_2", {
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
 	_on_bone_meal = on_bone_meal,
+	_mcl_basenode = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_node("mcl_farming:beetroot", {
@@ -146,6 +149,7 @@ minetest.register_node("mcl_farming:beetroot", {
 	sounds = mcl_sounds.node_sound_leaves_defaults(),
 	_mcl_blast_resistance = 0,
 	_on_bone_meal = on_bone_meal,
+	_mcl_basenode = "mcl_farming:beetroot_seeds",
 })
 
 minetest.register_craftitem("mcl_farming:beetroot_item", {

mods/ITEMS/mcl_farming/carrots.lua
@@ -50,6 +50,7 @@ for i=1, 7 do
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
 		_on_bone_meal = on_bone_meal,
+		_mcl_basenode = "mcl_farming:carrot_item",
 	})
 end
 
@@ -90,7 +91,8 @@ minetest.register_node("mcl_farming:carrot", {
 		min_count = 2,
 		max_count = 4,
 		cap = 5,
-	}
+	},
+	_mcl_basenode = "mcl_farming:carrot_item",
 })
 
 minetest.register_craftitem("mcl_farming:carrot_item", {

mods/ITEMS/mcl_farming/melon.lua
@@ -111,6 +111,7 @@ for s=1,7 do
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
 		_on_bone_meal = on_bone_meal,
+		_mcl_basenode = "mcl_farming:melon_seeds",
 	})
 end
 
@@ -122,6 +123,7 @@ local stem_def = {
 	tiles = {"mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127"},
 	wield_image = "mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127",
 	inventory_image = "mcl_farming_melon_stem_disconnected.png^[colorize:#FFA800:127",
+	_mcl_basenode = "mcl_farming:melon_seeds",
 }
 
 -- Register stem growth

mods/ITEMS/mcl_farming/potatoes.lua
@@ -54,6 +54,7 @@ for i=1, 7 do
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
 		_on_bone_meal = on_bone_meal,
+		_mcl_basenode = "mcl_farming:potato_item",
 	})
 end
 
@@ -94,7 +95,8 @@ minetest.register_node("mcl_farming:potato", {
 		min_count = 2,
 		max_count = 4,
 		cap = 5
-	}
+	},
+	_mcl_basenode = "mcl_farming:potato_item",
 })
 
 minetest.register_craftitem("mcl_farming:potato_item", {

mods/ITEMS/mcl_farming/pumpkin.lua
@@ -102,6 +102,7 @@ for s=1,7 do
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
 		_on_bone_meal = on_bone_meal,
+		_mcl_basenode = "mcl_farming:pumpkin_seeds",
 	})
 end
 
@@ -112,6 +113,7 @@ local stem_def = {
 	tiles = {"mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127"},
 	wield_image = "mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127",
 	inventory_image = "mcl_farming_pumpkin_stem_disconnected.png^[colorize:#FFA800:127",
+	_mcl_basenode = "mcl_farming:pumpkin_seeds",
 }
 
 -- Template for pumpkin

mods/ITEMS/mcl_farming/wheat.lua
@@ -70,6 +70,7 @@ for i=1,7 do
 		sounds = mcl_sounds.node_sound_leaves_defaults(),
 		_mcl_blast_resistance = 0,
 		_on_bone_meal = on_bone_meal,
+		_mcl_basenode = "mcl_farming:wheat_seeds",
 	})
 end
 
@@ -108,7 +109,8 @@ minetest.register_node("mcl_farming:wheat", {
 		min_count = 1,
 		max_count = 6,
 		cap = 7
-	}
+	},
+	_mcl_basenode = "mcl_farming:wheat_seeds",
 })
 
 mcl_farming:add_plant("plant_wheat", "mcl_farming:wheat", {"mcl_farming:wheat_1", "mcl_farming:wheat_2", "mcl_farming:wheat_3", "mcl_farming:wheat_4", "mcl_farming:wheat_5", "mcl_farming:wheat_6", "mcl_farming:wheat_7"}, 25, 20)