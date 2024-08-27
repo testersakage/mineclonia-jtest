thatguyatrix/SandBlocks

Patch mcl_beds for pickblock

mods/ITEMS/mcl_beds/api.lua
@@ -302,6 +302,7 @@ function mcl_beds.register_bed(name, def)
 		on_rotate = rotate,
 		after_destruct = destruct_bed,
 		on_dig = dig_bed,
+		_vl_pickblock = name .. "_bottom",
 	})
 
 	minetest.register_alias(name, name .. "_bottom")

testersakage/mineclonia-jtest

mods/ITEMS/mcl_beds/api.lua
@@ -236,6 +236,7 @@ function mcl_beds.register_bed(name, def)
 
 		on_rotate = rotate,
 		after_destruct = destruct_bed,
+		_mcl_basenode = name .. "_bottom",
 	})
 
 	minetest.register_alias(name, name .. "_bottom")