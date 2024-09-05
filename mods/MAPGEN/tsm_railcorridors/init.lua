17a076d5a5de61ffde5f92e11007218fe2138684
cora <coradelamouche@gmx.ch>
2024-09-05 17:12:45 +0200

    properly construct chests in dungeons and mineshafts

--- mods/MAPGEN/tsm_railcorridors/init.lua
+++ mods/MAPGEN/tsm_railcorridors/init.lua
@@ -373,6 +373,7 @@ end
 -- Chests
 local function PlaceChest(pos, param2)
 	if SetNodeIfCanBuild(pos, {name=tsm_railcorridors.nodes.chest, param2=param2}) then
+		mcl_structures.construct_nodes(pos, pos, {"mcl_chests:chest"})
 		local meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
 		local items = tsm_railcorridors.get_treasures(pr)
