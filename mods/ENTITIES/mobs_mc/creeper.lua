4f431f02a30497ce8b55e9c1f91de48a3456c591
goblin_mode <grzwWka@proton.me>
2024-09-07 15:44:09 +0200

    fix creeper dropping music discs

--- mods/ENTITIES/mobs_mc/creeper.lua
+++ mods/ENTITIES/mobs_mc/creeper.lua
@@ -87,15 +87,11 @@ local creeper_defs = {
 	end,
 	on_die = function(_, pos, cmi_cause)
 		-- Drop a random music disc when killed by skeleton or stray
-		if cmi_cause and cmi_cause.type == "punch" then
-			local luaentity = cmi_cause.puncher and cmi_cause.puncher:get_luaentity()
-			if luaentity and luaentity.name:find("arrow") then
-				local shooter_luaentity = luaentity._shooter and luaentity._shooter:get_luaentity()
-				if shooter_luaentity and (shooter_luaentity.name == "mobs_mc:skeleton" or shooter_luaentity.name == "mobs_mc:stray") then
-					local loot = mcl_jukebox.get_random_creeper_loot()
-					if loot then
-						minetest.add_item({x=pos.x, y=pos.y+1, z=pos.z}, loot)
-					end
+		if cmi_cause and cmi_cause.type == "arrow" then
+			if cmi_cause.mob_name == "mobs_mc:skeleton" or cmi_cause.mob_name == "mobs_mc:stray" then
+				local loot = mcl_jukebox.get_random_creeper_loot()
+				if loot then
+					minetest.add_item({x=pos.x, y=pos.y+1, z=pos.z}, loot)
 				end
 			end
 		end
