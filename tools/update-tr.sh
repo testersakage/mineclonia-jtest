7895e4638494cd6db7ab37948a31d76cdda28375
Elias Åström <ryvnf@riseup.net>
2024-09-21 18:50:55 +0200

    Run translation scripts twice in update-tr.sh

--- tools/update-tr.sh
+++ tools/update-tr.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 find -name 'mod.conf' | while read modconf; do
-	moddir=$(dirname "$modconf")
-	mtt_convert.py --po2tr "$moddir"
-	mod_translation_updater.py "$moddir"
-	mtt_convert.py --tr2po "$moddir"
+	# Run the scripts twice to make 'git diff' stable.
+	for i in 0 1; do
+		moddir=$(dirname "$modconf")
+		mtt_convert.py --po2tr "$moddir"
+		mod_translation_updater.py "$moddir"
+		mtt_convert.py --tr2po "$moddir"
+	done
 done
