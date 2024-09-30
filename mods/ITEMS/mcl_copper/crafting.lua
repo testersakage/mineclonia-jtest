647182afe3b91aed906a5f59844916f6807a3fd9
nooneatall <pretendemail@gmail.com>
2024-09-30 09:00:12 +0300

    Add missing wax and crafting recipes. Clean up craft registration for all items.

--- mods/ITEMS/mcl_copper/crafting.lua
+++ mods/ITEMS/mcl_copper/crafting.lua
@@ -1,17 +1,21 @@
+local block_type = { "", "_cut", "_chiseled", "_grate" }
+local block_exposure_level = { "", "_exposed", "_weathered", "_oxidized" }
+local waxed = { "", "_preserved" }
+
 minetest.register_craft({
 	output = "mcl_copper:block_raw",
 	recipe = {
 		{ "mcl_copper:raw_copper", "mcl_copper:raw_copper", "mcl_copper:raw_copper" },
 		{ "mcl_copper:raw_copper", "mcl_copper:raw_copper", "mcl_copper:raw_copper" },
-		{ "mcl_copper:raw_copper", "mcl_copper:raw_copper", "mcl_copper:raw_copper" },
-	},
+		{ "mcl_copper:raw_copper", "mcl_copper:raw_copper", "mcl_copper:raw_copper" }
+	}
 })
 
 minetest.register_craft({
 	output = "mcl_copper:raw_copper 9",
 	recipe = {
-		{ "mcl_copper:block_raw" },
-	},
+		{ "mcl_copper:block_raw" }
+	}
 })
 
 minetest.register_craft({
@@ -19,304 +23,104 @@ minetest.register_craft({
 	recipe = {
 		{ "mcl_copper:copper_ingot", "mcl_copper:copper_ingot", "mcl_copper:copper_ingot" },
 		{ "mcl_copper:copper_ingot", "mcl_copper:copper_ingot", "mcl_copper:copper_ingot" },
-		{ "mcl_copper:copper_ingot", "mcl_copper:copper_ingot", "mcl_copper:copper_ingot" },
-	},
+		{ "mcl_copper:copper_ingot", "mcl_copper:copper_ingot", "mcl_copper:copper_ingot" }
+	}
 })
 
 minetest.register_craft({
 	output = "mcl_copper:copper_ingot 9",
 	recipe = {
-		{ "mcl_copper:block" },
-	},
+		{ "mcl_copper:block" }
+	}
 })
 
 minetest.register_craft({
 	output = "mcl_copper:copper_ingot 9",
 	recipe = {
-		{ "mcl_copper:block_preserved" },
-	},
-})
-
--- TO HERE
-minetest.register_craft({
-	output = "mcl_copper:block_cut 4",
-	recipe = {
-		{ "mcl_copper:block", "mcl_copper:block" },
-		{ "mcl_copper:block", "mcl_copper:block" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_cut_preserved 4",
-	recipe = {
-		{ "mcl_copper:block_preserved", "mcl_copper:block_preserved" },
-		{ "mcl_copper:block_preserved", "mcl_copper:block_preserved" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_cut 4",
-	recipe = {
-		{ "mcl_copper:block_exposed", "mcl_copper:block_exposed" },
-		{ "mcl_copper:block_exposed", "mcl_copper:block_exposed" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_cut_preserved 4",
-	recipe = {
-		{ "mcl_copper:block_exposed_preserved", "mcl_copper:block_exposed_preserved" },
-		{ "mcl_copper:block_exposed_preserved", "mcl_copper:block_exposed_preserved" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_cut 4",
-	recipe = {
-		{ "mcl_copper:block_oxidized", "mcl_copper:block_oxidized" },
-		{ "mcl_copper:block_oxidized", "mcl_copper:block_oxidized" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_cut_preserved 4",
-	recipe = {
-		{ "mcl_copper:block_oxidized_preserved", "mcl_copper:block_oxidized_preserved" },
-		{ "mcl_copper:block_oxidized_preserved", "mcl_copper:block_oxidized_preserved" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_cut 4",
-	recipe = {
-		{ "mcl_copper:block_weathered", "mcl_copper:block_weathered" },
-		{ "mcl_copper:block_weathered", "mcl_copper:block_weathered" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_cut_preserved 4",
-	recipe = {
-		{ "mcl_copper:block_weathered_preserved", "mcl_copper:block_weathered_preserved" },
-		{ "mcl_copper:block_weathered_preserved", "mcl_copper:block_weathered_preserved" },
-	},
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_grate 4",
-	recipe = {
-		{ "", "mcl_copper:block", "" },
-		{ "mcl_copper:block", "", "mcl_copper:block" },
-		{ "", "mcl_copper:block", "" }
+		{ "mcl_copper:block_preserved" }
 	}
 })
 
-minetest.register_craft({
-	output = "mcl_copper:block_grate_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_preserved", "" },
-		{ "mcl_copper:block_preserved", "", "mcl_copper:block_preserved" },
-		{ "", "mcl_copper:block_preserved", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	for _, w in ipairs(waxed) do
+		minetest.register_craft({
+			output = "mcl_copper:block"..x.."_cut"..w.." 4",
+			recipe = {
+				{ "mcl_copper:block"..x..w, "mcl_copper:block"..x..w },
+				{ "mcl_copper:block"..x..w, "mcl_copper:block"..x..w }
+			}
+		})
+	end
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_grate 4",
-	recipe = {
-		{ "", "mcl_copper:block_exposed", "" },
-		{ "mcl_copper:block_exposed", "", "mcl_copper:block_exposed" },
-		{ "", "mcl_copper:block_exposed", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	for _, w in ipairs(waxed) do
+		minetest.register_craft({
+			output = "mcl_copper:block"..x.."_grate"..w.." 4",
+			recipe = {
+				{ "", "mcl_copper:block"..x..w, "" },
+				{ "mcl_copper:block"..x..w, "", "mcl_copper:block"..x..w },
+				{ "", "mcl_copper:block"..x..w, "" }
+			}
+		})
+	end
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_grate_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_exposed_preserved", "" },
-		{ "mcl_copper:block_exposed_preserved", "", "mcl_copper:block_exposed_preserved" },
-		{ "", "mcl_copper:block_exposed_preserved", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	for _, w in ipairs(waxed) do
+		minetest.register_craft({
+			output = "mcl_copper:block"..x.."_chiseled"..w.." 4",
+			recipe = {
+				{ "mcl_stairs:slab_copper"..x.."_cut"..w },
+				{ "mcl_stairs:slab_copper"..x.."_cut"..w }
+			}
+		})
+	end
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_grate 4",
-	recipe = {
-		{ "", "mcl_copper:block_weathered", "" },
-		{ "mcl_copper:block_weathered", "", "mcl_copper:block_weathered" },
-		{ "", "mcl_copper:block_weathered", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_stairs:slab_copper"..x.."_cut_preserved 6",
+		recipe = {
+			{ "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved" }
+		}
+	})
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_grate_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_weathered_preserved", "" },
-		{ "mcl_copper:block_weathered_preserved", "", "mcl_copper:block_weathered_preserved" },
-		{ "", "mcl_copper:block_weathered_preserved", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_stairs:stair_copper"..x.."_cut_preserved 4",
+		recipe = {
+			{ "", "", "mcl_copper:block"..x.."_cut_preserved" },
+			{"", "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved"},
+			{"mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved" }
+		}
+	})
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_grate 4",
-	recipe = {
-		{ "", "mcl_copper:block_oxidized", "" },
-		{ "mcl_copper:block_oxidized", "", "mcl_copper:block_oxidized" },
-		{ "", "mcl_copper:block_oxidized", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_stairs:stair_copper"..x.."_cut_preserved 4",
+		recipe = {
+			{ "mcl_copper:block"..x.."_cut_preserved", "", "" },
+			{ "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved", "" },
+			{ "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved", "mcl_copper:block"..x.."_cut_preserved" }
+		}
+	})
+end
 
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_grate_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_oxidized_preserved", "" },
-		{ "mcl_copper:block_oxidized_preserved", "", "mcl_copper:block_oxidized_preserved" },
-		{ "", "mcl_copper:block_oxidized_preserved", "" }
-	}
-})
-
--- NOONE working here for slabs
-minetest.register_craft({
-	output = "mcl_stairs:slab_copper_cut_preserved 6",
-	recipe = {
-		{ "mcl_copper:block_cut_preserved", "mcl_copper:block_cut_preserved", "mcl_copper:block_cut_preserved" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_chiseled",
-	recipe = {
-		{ "mcl_stairs:slab_copper_cut" },
-		{ "mcl_stairs:slab_copper_cut" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_chiseled_preserved",
-	recipe = {
-		{ "mcl_stairs:slab_copper_cut_preserved" },
-		{ "mcl_stairs:slab_copper_cut_preserved" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_chiseled",
-	recipe = {
-		{ "mcl_stairs:slab_copper_exposed_cut" },
-		{ "mcl_stairs:slab_copper_exposed_cut" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_exposed_chiseled_preserved",
-	recipe = {
-		{ "mcl_stairs:slab_copper_exposed_cut_preserved" },
-		{ "mcl_stairs:slab_copper_exposed_cut_preserved" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_chiseled",
-	recipe = {
-		{ "mcl_stairs:slab_copper_weathered_cut" },
-		{ "mcl_stairs:slab_copper_weathered_cut" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_weathered_chiseled_preserved",
-	recipe = {
-		{ "mcl_stairs:slab_copper_weathered_cut_preserved" },
-		{ "mcl_stairs:slab_copper_weathered_cut_preserved" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_chiseled",
-	recipe = {
-		{ "mcl_stairs:slab_copper_oxidized_cut" },
-		{ "mcl_stairs:slab_copper_oxidized_cut" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:block_oxidized_chiseled_preserved",
-	recipe = {
-		{ "mcl_stairs:slab_copper_oxidized_cut_preserved" },
-		{ "mcl_stairs:slab_copper_oxidized_cut_preserved" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_off 4",
-	recipe = {
-		{ "", "mcl_copper:block", "" },
-		{ "mcl_copper:block", "mcl_mobitems:blaze_rod", "mcl_copper:block" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_off_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_preserved", "" },
-		{ "mcl_copper:block_preserved", "mcl_mobitems:blaze_rod", "mcl_copper:block_preserved" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_exposed_off 4",
-	recipe = {
-		{ "", "mcl_copper:block_exposed", "" },
-		{ "mcl_copper:block_exposed", "mcl_mobitems:blaze_rod", "mcl_copper:block_exposed" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_exposed_off_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_exposed_preserved", "" },
-		{ "mcl_copper:block_exposed_preserved", "mcl_mobitems:blaze_rod", "mcl_copper:block_exposed_preserved" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_weathered_off 4",
-	recipe = {
-		{ "", "mcl_copper:block_weathered", "" },
-		{ "mcl_copper:block_weathered", "mcl_mobitems:blaze_rod", "mcl_copper:block_weathered" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_weathered_off_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_weathered_preserved", "" },
-		{ "mcl_copper:block_weathered_preserved", "mcl_mobitems:blaze_rod", "mcl_copper:block_weathered_preserved" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_oxidized_off 4",
-	recipe = {
-		{ "", "mcl_copper:block_oxidized", "" },
-		{ "mcl_copper:block_oxidized", "mcl_mobitems:blaze_rod", "mcl_copper:block_oxidized" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
-
-minetest.register_craft({
-	output = "mcl_copper:bulb_oxidized_off_preserved 4",
-	recipe = {
-		{ "", "mcl_copper:block_oxidized_preserved", "" },
-		{ "mcl_copper:block_oxidized_preserved", "mcl_mobitems:blaze_rod", "mcl_copper:block_oxidized_preserved" },
-		{ "", "mesecons:redstone", "" }
-	}
-})
+for _, x in ipairs(block_exposure_level) do
+	for _, w in ipairs(waxed) do
+		minetest.register_craft({
+			output = "mcl_copper:bulb"..x.."_off"..w.." 4",
+			recipe = {
+				{ "", "mcl_copper:block"..x..w, "" },
+				{ "mcl_copper:block"..x..w, "mcl_mobitems:blaze_rod", "mcl_copper:block"..x..w },
+				{ "", "mesecons:redstone", "" }
+			}
+		})
+	end
+end
 
 minetest.register_craft({
 	output = "mcl_copper:door 3",
@@ -335,13 +139,58 @@ minetest.register_craft({
 	}
 })
 
-local waxable_blocks = { "block", "block_cut", "block_exposed", "block_exposed_cut", "block_weathered", "block_weathered_cut", "block_oxidized", "block_oxidized_cut" }
+for _, t in ipairs(block_type) do
+	for _, x in ipairs(block_exposure_level) do
+		minetest.register_craft({
+			output = "mcl_copper:block"..x..t.."_preserved",
+			recipe = {
+				{ "mcl_copper:block"..x..t, "mcl_honey:honeycomb" }
+			}
+		})
+	end
+end
 
-for _, w in ipairs(waxable_blocks) do
+-- for _, x in ipairs(block_exposure_level) do
+-- 	minetest.register_craft({
+-- 		output = "mcl_copper:door"..x.."_preserved",
+-- 		recipe = {
+-- 			{ "mcl_copper:door"..x, "mcl_honey:honeycomb" }
+-- 		}
+-- 	})
+-- end
+
+for _, x in ipairs(block_exposure_level) do
 	minetest.register_craft({
-		output = "mcl_copper:"..w.."_preserved",
+		output = "mcl_copper:trapdoor"..x.."_preserved",
 		recipe = {
-			{ "mcl_copper:"..w, "mcl_honey:honeycomb" },
-		},
+			{ "mcl_copper:trapdoor"..x, "mcl_honey:honeycomb" }
+		}
+	})
+end
+
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_copper:bulb"..x.."_off_preserved",
+		recipe = {
+			{ "mcl_copper:bulb"..x.."_off", "mcl_honey:honeycomb" }
+		}
+	})
+end
+
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_stairs:slab_copper"..x.."_cut_preserved",
+		recipe = {
+			{ "mcl_stairs:slab_copper"..x.."_cut", "mcl_honey:honeycomb" }
+		}
+	})
+end
+
+for _, x in ipairs(block_exposure_level) do
+	minetest.register_craft({
+		output = "mcl_stairs:stair_copper"..x.."_cut_preserved",
+		recipe = {
+			{ "mcl_stairs:stair_copper"..x.."_cut", "mcl_honey:honeycomb" }
+		}
 	})
 end
