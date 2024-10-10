b29b5725e2173decbe68eb0afdd6690c359f7a7e
Jürgen Rühle <j-r@online.de>
2024-10-10 11:36:34 +0200

    Add automatic handling of ringbuffer stored in node meta data
    Initialization from node meta data and automatic update on insert.
    
    This also adds weak caching to prevent repeated calls to deserialize; the
    serialization still happens on every write access.

--- mods/CORE/mcl_util/ringbuffer.lua
+++ mods/CORE/mcl_util/ringbuffer.lua
@@ -23,6 +23,16 @@ function ringbuffer_class:insert(record)
 		table.remove(self.data, 1)
 	end
 	table.insert(self.data, record)
+	if self.auto_update_node_meta_key then
+		local pos = minetest.get_position_from_hash(self.auto_update_node_meta_key:sub(1, 16))
+		local key = self.auto_update_node_meta_key:sub(17)
+		-- auto update node meta data
+		local meta = minetest.get_meta(pos)
+		meta:set_string(key, self:serialize())
+		if self.node_meta_private then
+			meta:mark_as_private(key)
+		end
+	end
 end
 
 function ringbuffer_class:indexof(val)
@@ -45,6 +55,22 @@ end
 function ringbuffer.deserialize(size, serialized_data)
 	local rb_data = minetest.deserialize(serialized_data)
 	return rb_data and ringbuffer.new(size, rb_data)
+end
+
+local node_meta_rb_cache = setmetatable({}, { __mode = "v" })
+
+function ringbuffer.get_from_node_meta(pos, key, size, private)
+	local node_meta_key = string.format("%0.16d", minetest.hash_node_position(pos)) .. key
+	local rb = node_meta_rb_cache[node_meta_key]
+	if not rb then
+		-- read from node meta data or intialize new
+		local meta = minetest.get_meta(pos)
+		rb = ringbuffer.deserialize(size, meta:get_string(key)) or ringbuffer.new(size)
+		rb.auto_update_node_meta_key = node_meta_key
+		rb.node_meta_private = private
+		node_meta_rb_cache[node_meta_key] = rb
+	end
+	return rb
 end
 
 return setmetatable(ringbuffer, ringbuffer_class)
