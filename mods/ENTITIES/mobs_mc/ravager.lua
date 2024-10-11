7b487e975e0e5599723ee3ac2f9cf2e622e07361
cora <coradelamouche@gmx.ch>
2024-10-11 04:33:08 +0200

    Update ravager definition from wiki values

--- mods/ENTITIES/mobs_mc/ravager.lua
+++ mods/ENTITIES/mobs_mc/ravager.lua
@@ -9,7 +9,6 @@ mcl_mobs.register_mob("mobs_mc:ravager", {
 	xp_min = 5,
 	xp_max = 5,
 	attack_type = "dogshoot",
-	--double_melee_attack = false,
 	reach = 3, -- Height: 2.9 blocks
 	armor = 10,
 	collisionbox = { -0.8, 0, -0.8, 0.8, 2.2, 0.8 },
@@ -23,32 +22,21 @@ mcl_mobs.register_mob("mobs_mc:ravager", {
 	   -- random = "",
 	},
 	drops = {
-	   --{name = " ", min = 1, max = 2},
+		{ name = "mcl_mobitems:saddle", min = 1, max = 1, chance = 1 },
 	},
-	view_range = 16, -- 16 nodes
+	view_range = 32,
 	stepheight = 1.1,
-	--instant_death = true,
 	fire_resistant = true,
 	suffocation = false,
 	all_damage = false,
-	knock_back = false,
+	knock_back = true,
+	specific_attack = { "player", "mobs_mc:iron_golem", "mobs_mc:villager", "mobs_mc:wandering_trader" },
 	animation = {
 		stand_start = 1, stand_end = 40, stand_speed = 10,
 		walk_start = 50, walk_end = 90, speed_normal = 10,
 		run_start = 50, run_end = 90, speed_run = 20,
 		punch_start = 100, punch_end = 140, punch_speed = 20,
 	},
-	--[[
-	do_custom = function(self,dtime)
-		---
-	end,
-	on_spawn = function(self, pos)
-	 --
-		end
-	  on_die = function(self, pos)
-
-	end
-	]]
 })
 
 mcl_mobs.register_egg("mobs_mc:ravager", "Ravager", "#2b2522", "#1c1c1c", 0)
