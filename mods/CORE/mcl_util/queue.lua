d842d734d08c75b55a47ab43d0a15c2028adb0f0
goblin_mode <grzwWka@proton.me>
2024-11-07 22:20:36 +0100

    mcl_util: Optimize queues
    Co-authored-by: Mikita Wiśniewski <rudzik8@protonmail.com>

--- mods/CORE/mcl_util/queue.lua
+++ mods/CORE/mcl_util/queue.lua
@@ -1,24 +1,43 @@
-local function queue()
-	return {
-		front = 1,
-		back = 1,
-		queue = {},
-		enqueue = function(self, value)
-			self.queue[self.back] = value
-			self.back = self.back + 1
-		end,
-		dequeue = function(self) local value = self.queue[self.front]
-			if not value then
-				return
+-- the queue class is using clever tricks to avoid allocating more memory than needed
+-- it stores its back and front indices in the same table as the elements
+-- index 1: the queue's back
+-- index 2: the queue's front
+local queue_class = {
+	enqueue = function(self, value)
+		self[self[1]] = value
+		self[1] = self[1] + 1
+	end,
+
+	dequeue = function(self)
+		local value = self[self[2]]
+		if value == nil then
+			return
+		end
+		self[self[2]] = nil
+		self[2] = self[2] + 1
+		return value
+	end,
+
+	size = function(self)
+		return self[1] - self[2]
+	end,
+
+	iterate = function(self)
+		local idx = self[2] - 1
+		return function()
+			idx = idx + 1
+			if self[1] <= idx then
+				return nil
 			end
-			self.queue[self.front] = nil
-			self.front = self.front + 1
-			return value
-		end,
-		size = function(self)
-			return self.back - self.front
-		end,
-	}
+			return self[idx]
+		end
+	end,
+}
+
+queue_class.__index = queue_class
+
+local function queue()
+	return setmetatable({3, 3}, queue_class)
 end
 
 return queue
