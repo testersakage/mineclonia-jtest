922b90d1a3970e86425c4edde74e5ce2cac8f2b2
Jürgen Rühle <j-r@online.de>
2024-11-14 00:01:11 +0100

    Support comparator_signal = 0
    Comparators should read unlit copper bulbs as power level 0 even if
    powered.

--- mods/ITEMS/REDSTONE/mcl_comparators/init.lua
+++ mods/ITEMS/REDSTONE/mcl_comparators/init.lua
@@ -398,13 +398,13 @@ minetest.set_node = function(pos, newnode)
 end
 
 minetest.register_on_mods_loaded(function()
-	for name, _ in pairs(minetest.registered_nodes) do
+	for name, def in pairs(minetest.registered_nodes) do
 		if minetest.get_item_group(name, "shulker_box") ~= 0 then
 			measure_tab[name] = measure_inventory
 		elseif minetest.get_item_group(name, "brewing_stand") ~= 0 then
 			measure_tab[name] = measure_brewing_stand
-		elseif minetest.get_item_group(name, "comparator_signal") ~= 0 then
-			measure_tab[name] = measure_constant(minetest.get_item_group(name, "comparator_signal"))
+		elseif def.groups and def.groups.comparator_signal then
+			measure_tab[name] = measure_constant(def.groups.comparator_signal)
 		end
 	end
 end)
