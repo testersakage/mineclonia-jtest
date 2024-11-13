b346ee1a6522b47edda237aa475004262d93708e
Jürgen Rühle <j-r@online.de>
2024-11-13 22:41:49 +0100

    Fix copper bulb connection
    Also unify update functions in on/off bulb variants.

--- mods/ITEMS/mcl_copper/nodes.lua
+++ mods/ITEMS/mcl_copper/nodes.lua
@@ -123,7 +123,7 @@ for n, desc in pairs(n_desc) do
 		_mcl_hardness = 3,
 		drop = "mcl_copper:bulb"..n.."_off",
 		_mcl_redstone = {
-			connects_to = function(node)
+			connects_to = function(node, dir)
 				return true
 			end,
 			update = function(pos, node)
@@ -154,11 +154,10 @@ for n, desc in pairs(n_desc) do
 		_mcl_blast_resistance = 6,
 		_mcl_hardness = 3,
 		_mcl_redstone = {
-			connects_to = function(node)
+			connects_to = function(node, dir)
 				return true
 			end,
-			update = function(pos)
-				local node = minetest.get_node(pos)
+			update = function(pos, node)
 				local oldpowered = node.param2 ~= 0
 				local powered  = mcl_redstone.get_power(pos) ~= 0
 				local newname = node.name
