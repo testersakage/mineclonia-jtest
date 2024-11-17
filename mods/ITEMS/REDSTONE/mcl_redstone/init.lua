8337957916725e9c0f85e62cd4302218ed480894
Elias Åström <ryvnf@riseup.net>
2024-11-17 15:23:35 +0100

    Combine wireflags and logical wireflags

--- mods/ITEMS/REDSTONE/mcl_redstone/init.lua
+++ mods/ITEMS/REDSTONE/mcl_redstone/init.lua
@@ -1,6 +1,14 @@
 mcl_redstone = {}
 
-mcl_redstone._solid_opaque_tab = {}
+mcl_redstone._solid_opaque_tab = {} -- True if node is opaque by name
+
+--- Wireflags are numbers with binary representation YYYYXXXX where XXXX
+--- determines if there is a visible connection in each of the four cardinal
+--- directions and YYYY if the respective connection also goes up over the
+--- neighbouring node. Order of the bits (right to left) are -z, +x, +z, -x.
+--
+-- This table contains wireflags by node name.
+mcl_redstone._wireflag_tab = {}
 
 minetest.register_on_mods_loaded(function()
 	for name, ndef in pairs(minetest.registered_nodes) do
