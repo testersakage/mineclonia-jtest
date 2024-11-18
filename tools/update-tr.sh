8943b551bad840bf7806cf85f5d70dbe4271b5be
Elias Åström <ryvnf@riseup.net>
2024-11-18 21:50:48 +0100

    Make update-tr.sh to use new convert script

--- tools/update-tr.sh
+++ tools/update-tr.sh
@@ -4,8 +4,8 @@ find -name 'mod.conf' | while read modconf; do
 	# Run the scripts twice to make 'git diff' stable.
 	for i in 0 1; do
 		moddir=$(dirname "$modconf")
-		mtt_convert.py --po2tr "$moddir"
+		ltt_convert.py --po2tr "$moddir"
 		mod_translation_updater.py "$moddir"
-		mtt_convert.py --tr2po "$moddir"
+		ltt_convert.py --tr2po "$moddir"
 	done
 done
