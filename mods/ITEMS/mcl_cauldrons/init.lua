a25a5e940e7c56cd7173305feed9950d4d6ca531
cora <coradelamouche@gmx.ch>
2024-11-29 11:39:16 +0100

    Move leather armor washing to mcl_armor

--- mods/ITEMS/mcl_cauldrons/init.lua
+++ mods/ITEMS/mcl_cauldrons/init.lua
@@ -161,14 +161,6 @@ local function register_filled_cauldron(water_level, description, liquid)
 			end
 			return itemstack
 		end,
-		on_rightclick = function(pos, _, _, itemstack)
-			local outcome = mcl_armor.wash_leather_armor(itemstack)
-			if outcome then
-				minetest.sound_play("mcl_potions_bottle_pour",
-					{pos=pos, gain=0.5, max_hear_range=16},true)
-			end
-			return outcome or itemstack
-		end,
 	})
 
 	-- Add entry aliases for the Help
