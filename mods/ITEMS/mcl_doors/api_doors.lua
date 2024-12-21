a2184cd46f5675305e0037510fffd76da56c5e6c
JoseDouglas26 <josedouglas20002014@gmail.com>
2024-12-21 20:16:23 -0300

    Using vector.offset and vector.copy on doors

--- mods/ITEMS/mcl_doors/api_doors.lua
+++ mods/ITEMS/mcl_doors/api_doors.lua
@@ -129,8 +129,7 @@ function mcl_doors:register_door(name, def)
 			else
 				pt = pointed_thing.above
 			end
-			local pt2 = {x=pt.x, y=pt.y, z=pt.z}
-			pt2.y = pt2.y+1
+			local pt2 = vector.copy(pt):offset(0, 1, 0)
 			local ptname = minetest.get_node(pt).name
 			local pt2name = minetest.get_node(pt2).name
 			if
@@ -141,7 +140,7 @@ function mcl_doors:register_door(name, def)
 			end
 
 			-- get left coordinate for checking if another door is there
-			local pt_left = {x=pt.x, y=pt.y, z=pt.z}
+			local pt_left = vector.copy(pt)
 			local p2 = minetest.dir_to_facedir(placer:get_look_dir())
 
 			if p2 == 0 then
@@ -207,7 +206,7 @@ function mcl_doors:register_door(name, def)
 
 	local function on_open_close(pos, dir, check_name, replace, replace_dir)
 		local meta1 = minetest.get_meta(pos)
-		pos.y = pos.y+dir
+		pos:offset(0, dir, 0)
 		local meta2 = minetest.get_meta(pos)
 
 		-- if name of other door is not the same as check_name -> return
@@ -318,7 +317,7 @@ function mcl_doors:register_door(name, def)
 				meta_bottom:set_int("rotation", 0)
 			else
 				minetest.add_item(bottom, name)
-				local top = { x = bottom.x, y = bottom.y + 1, z = bottom.z }
+				local top = vector.copy(bottom):offset(0, 1, 0)
 				if minetest.get_node(bottom).name ~= name.."_b_2" and minetest.get_node(top).name == name.."_t_1" then
 					minetest.remove_node(top)
 				end
@@ -347,7 +346,7 @@ function mcl_doors:register_door(name, def)
 				node.param2 = screwdriver.rotate.facedir(bottom, node, mode)
 				minetest.swap_node(bottom, node)
 
-				local top = {x=bottom.x,y=bottom.y+1,z=bottom.z}
+				local top = bottom:offset(0, 1, 0)
 				local meta_top = minetest.get_meta(top)
 				meta_top:set_int("rotation", 1)
 				node.name = name .."_t_1"
@@ -399,7 +398,7 @@ function mcl_doors:register_door(name, def)
 			if meta_top:get_int("rotation") == 1 then
 				meta_top:set_int("rotation", 0)
 			else
-				local bottom = { x = top.x, y = top.y - 1, z = top.z }
+				local bottom = vector.copy(top):offset(0, -1, 0)
 				if minetest.get_node(top).name ~= name.."_t_2" and minetest.get_node(bottom).name == name.."_b_1" and oldnode.name == name.."_t_1" then
 					minetest.dig_node(bottom)
 				end
@@ -428,7 +427,7 @@ function mcl_doors:register_door(name, def)
 				node.param2 = screwdriver.rotate.facedir(top, node, mode)
 				minetest.swap_node(top, node)
 
-				local bottom = {x=top.x,y=top.y-1,z=top.z}
+				local bottom = top:offset(0, -1, 0)
 				local meta_bottom = minetest.get_meta(bottom)
 				meta_bottom:set_int("rotation", 1)
 				node.name = name .."_b_1"
@@ -480,7 +479,7 @@ function mcl_doors:register_door(name, def)
 			if meta_bottom:get_int("rotation") == 1 then
 				meta_bottom:set_int("rotation", 0)
 			else
-				local top = { x = bottom.x, y = bottom.y + 1, z = bottom.z }
+				local top = vector.copy(bottom):offset(0, 1, 0)
 				minetest.add_item(bottom, name)
 				if minetest.get_node(bottom).name ~= name.."_b_1" and minetest.get_node(top).name == name.."_t_2" then
 					minetest.remove_node(top)
@@ -510,7 +509,7 @@ function mcl_doors:register_door(name, def)
 				node.param2 = screwdriver.rotate.facedir(bottom, node, mode)
 				minetest.swap_node(bottom, node)
 
-				local top = {x=bottom.x,y=bottom.y+1,z=bottom.z}
+				local top = bottom:offset(0, 1, 0)
 				local meta_top = minetest.get_meta(top)
 				meta_top:set_int("rotation", 1)
 				node.name = name .."_t_2"
@@ -562,7 +561,7 @@ function mcl_doors:register_door(name, def)
 			if meta_top:get_int("rotation") == 1 then
 				meta_top:set_int("rotation", 0)
 			else
-				local bottom = { x = top.x, y = top.y - 1, z = top.z }
+				local bottom = vector.copy(top):offset(0, -1, 0)
 				if minetest.get_node(top).name ~= name.."_t_1" and minetest.get_node(bottom).name == name.."_b_2" and oldnode.name == name.."_t_2" then
 					minetest.dig_node(bottom)
 				end
@@ -591,7 +590,7 @@ function mcl_doors:register_door(name, def)
 				node.param2 = screwdriver.rotate.facedir(top, node, mode)
 				minetest.swap_node(top, node)
 
-				local bottom = {x=top.x,y=top.y-1,z=top.z}
+				local bottom = top:offset(0, -1, 0)
 				local meta_bottom = minetest.get_meta(bottom)
 				meta_bottom:set_int("rotation", 1)
 				node.name = name .."_b_2"
