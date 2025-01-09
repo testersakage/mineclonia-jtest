d2236d2f1bf0b3075f4e2389982853b22787a984
noone <pretendemail@gmail.com>
2025-01-09 09:30:41 +0200

    update minetest to core

--- mods/ITEMS/mcl_crimson/init.lua
+++ mods/ITEMS/mcl_crimson/init.lua
@@ -1,6 +1,6 @@
-local modname = minetest.get_current_modname()
-local S = minetest.get_translator(modname)
-local modpath = minetest.get_modpath(modname)
+local modname = core.get_current_modname()
+local S = core.get_translator(modname)
+local modpath = core.get_modpath(modname)
 -- Warped and Crimson fungus
 -- by debiankaios
 -- adapted for mcl2 by cora
@@ -21,18 +21,18 @@ local nether_plants = {
 }
 
 local place_fungus = mcl_util.generate_on_place_plant_function(function(pos)
-	return minetest.get_item_group(minetest.get_node(vector.offset(pos,0,-1,0)).name, "soil_fungus") > 0
+	return core.get_item_group(core.get_node(vector.offset(pos,0,-1,0)).name, "soil_fungus") > 0
 end)
 
 local function spread_nether_plants(pos,node)
 	local n = node.name
-	local nn = minetest.find_nodes_in_area_under_air(vector.offset(pos,-5,-3,-5),vector.offset(pos,5,3,5),{n})
+	local nn = core.find_nodes_in_area_under_air(vector.offset(pos,-5,-3,-5),vector.offset(pos,5,3,5),{n})
 	table.shuffle(nn)
 	nn[1] = pos
 	for i=1,math.random(1,math.min(#nn,12)) do
 		local p = vector.offset(nn[i],0,1,0)
-		if minetest.get_node(p).name == "air" then
-			minetest.set_node(p,{name=nether_plants[n][math.random(#nether_plants[n])]})
+		if core.get_node(p).name == "air" then
+			core.set_node(p,{name=nether_plants[n][math.random(#nether_plants[n])]})
 		end
 	end
 end
@@ -45,41 +45,41 @@ local function on_bone_meal(_, _, pt, _, node)
 end
 
 local function check_for_bedrock(pos)
-	local br = minetest.find_nodes_in_area(pos, vector.offset(pos, 0, 12, 0), {"mcl_core:bedrock"})
+	local br = core.find_nodes_in_area(pos, vector.offset(pos, 0, 12, 0), {"mcl_core:bedrock"})
 	return br and #br > 0
 end
 
 local function generate_fungus_tree(pos, typ)
-	return minetest.place_schematic(pos,modpath.."/schematics/"..typ.."_fungus_"..tostring(math.random(1,3))..".mts","random",nil,false,"place_center_x,place_center_z")
+	return core.place_schematic(pos,modpath.."/schematics/"..typ.."_fungus_"..tostring(math.random(1,3))..".mts","random",nil,false,"place_center_x,place_center_z")
 end
 
 local max_vines_age = 25
 local grow_vines_direction = {[1] = 1, [2] = -1}
 
 function set_vines_age(pos, node)
-	local dir = grow_vines_direction[minetest.get_item_group(node.name, "vinelike_node")]
+	local dir = grow_vines_direction[core.get_item_group(node.name, "vinelike_node")]
 	local vpos, i = mcl_util.traverse_tower(pos, -dir)
 	for i = 1, i do
-		minetest.swap_node(vpos, { name = node.name, param2 = i })
+		core.swap_node(vpos, { name = node.name, param2 = i })
 		vpos = vector.offset(vpos, 0, dir, 0)
 	end
 	return i
 end
 
 function get_vines_age(pos)
-	local node = minetest.get_node(pos)
+	local node = core.get_node(pos)
 	return node.param2 > 0 and node.param2 or set_vines_age(pos, node)
 end
 
 function grow_vines(pos, amount, vine, dir, max_age)
-	dir = dir or grow_vines_direction[minetest.get_item_group(vine, "vinelike_node")] or 1
+	dir = dir or grow_vines_direction[core.get_item_group(vine, "vinelike_node")] or 1
 	local tip, i = mcl_util.traverse_tower(pos, dir)
 	local age = get_vines_age(pos) + i -1
 	amount = math.min(amount, max_age and max_age - age or amount)
 	for i=1, amount do
 		local p = vector.offset(tip,0,dir*i,0)
-		if minetest.get_node(p).name == "air" then
-			minetest.set_node(p,{name=vine, param2=age +i})
+		if core.get_node(p).name == "air" then
+			core.set_node(p,{name=vine, param2=age +i})
 		else
 			return i-1
 		end
@@ -215,7 +215,7 @@ mcl_trees.register_wood("warped",{
 	slab = { overrides = { _mcl_burntime = 0 }},
 })
 
-minetest.register_node("mcl_crimson:warped_fungus", {
+core.register_node("mcl_crimson:warped_fungus", {
 	description = S("Warped Fungus"),
 	_tt_help = S("Warped fungus is a mushroom found in the nether's warped forest."),
 	_doc_items_longdesc = S("Warped fungus is a mushroom found in the nether's warped forest."),
@@ -236,10 +236,10 @@ minetest.register_node("mcl_crimson:warped_fungus", {
 	node_placement_prediction = "",
 	on_place = place_fungus,
 	_on_bone_meal = function(_, _, _, pos)
-		if minetest.get_node_or_nil(vector.offset(pos,0,-1,0)).name == "mcl_crimson:warped_nylium" then
+		if core.get_node_or_nil(vector.offset(pos,0,-1,0)).name == "mcl_crimson:warped_nylium" then
 			if math.random() > 0.40 then return end --fungus has a 40% chance to grow when bone mealing
 			if check_for_bedrock(pos) then return false end
-			minetest.remove_node(pos)
+			core.remove_node(pos)
 			return generate_fungus_tree(pos, "warped")
 		end
 	end,
@@ -257,25 +257,25 @@ local call_on_place = function(itemstack, placer, pointed_thing)
 	if rc then return rc end
 
 	local dir = vector.direction(pointed_thing.under, pointed_thing.above).y
-	local node = minetest.get_node(pointed_thing.under)
+	local node = core.get_node(pointed_thing.under)
 	local idef = itemstack:get_definition()
 
-	local grow_dir = grow_vines_direction[minetest.get_item_group(idef.name, "vinelike_node")]
+	local grow_dir = grow_vines_direction[core.get_item_group(idef.name, "vinelike_node")]
 	local pos = vector.offset(pointed_thing.under, 0, grow_dir, 0)
 	if mcl_util.check_position_protection(pos, placer) then return itemstack end
 
 	if node.name == idef.name then
 		if grow_vines(pointed_thing.under, 1, node.name) == 0 then return end
-	elseif grow_dir == dir and minetest.get_item_group(node.name, "solid") ~= 0 then
-		minetest.item_place_node(itemstack, placer, pointed_thing, 1)
+	elseif grow_dir == dir and core.get_item_group(node.name, "solid") ~= 0 then
+		core.item_place_node(itemstack, placer, pointed_thing, 1)
 	else
 		return itemstack
 	end
 
 	if idef.sounds and idef.sounds.place then
-		minetest.sound_play(idef.sounds.place, {pos=pointed_thing.above, gain=1}, true)
+		core.sound_play(idef.sounds.place, {pos=pointed_thing.above, gain=1}, true)
 	end
-	if not minetest.is_creative_enabled(placer:get_player_name()) then
+	if not core.is_creative_enabled(placer:get_player_name()) then
 		itemstack:take_item(1)
 	end
 	return itemstack
@@ -285,7 +285,7 @@ local function register_vines(name, def, extra_groups)
 	local groups = table.merge({
 		dig_immediate=3, shearsy=1, dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1, deco_block=1, compostability=50
 	}, extra_groups or {})
-	minetest.register_node(name, table.merge({
+	core.register_node(name, table.merge({
 		drawtype = "plantlike",
 		sunlight_propagates = true,
 		paramtype = "light",
@@ -341,7 +341,7 @@ register_vines("mcl_crimson:weeping_vines", {
 	},
 }, {vinelike_node=2})
 
-minetest.register_node("mcl_crimson:nether_sprouts", {
+core.register_node("mcl_crimson:nether_sprouts", {
 	description = S("Nether Sprouts"),
 	drawtype = "plantlike",
 	tiles = { "nether_sprouts.png" },
@@ -366,7 +366,7 @@ minetest.register_node("mcl_crimson:nether_sprouts", {
 	_mcl_blast_resistance = 0,
 })
 
-minetest.register_node("mcl_crimson:warped_roots", {
+core.register_node("mcl_crimson:warped_roots", {
 	description = S("Warped Roots"),
 	drawtype = "plantlike",
 	tiles = { "warped_roots.png" },
@@ -396,7 +396,7 @@ mcl_flowerpots.register_potted_flower("mcl_crimson:warped_roots", {
 })
 
 
-minetest.register_node("mcl_crimson:warped_wart_block", {
+core.register_node("mcl_crimson:warped_wart_block", {
 	description = S("Warped Wart Block"),
 	tiles = {"warped_wart_block.png"},
 	groups = {handy = 1, hoey = 7, swordy = 1, deco_block = 1, compostability = 85},
@@ -408,11 +408,11 @@ minetest.register_node("mcl_crimson:warped_wart_block", {
 	}),
 })
 
-minetest.register_node("mcl_crimson:shroomlight", {
+core.register_node("mcl_crimson:shroomlight", {
 	description = S("Shroomlight"),
 	tiles = {"shroomlight.png"},
 	groups = {handy = 1, hoey = 7, swordy = 1, deco_block = 1, compostability = 65},
-	light_source = minetest.LIGHT_MAX,
+	light_source = core.LIGHT_MAX,
 	_mcl_hardness = 1,
 	sounds = mcl_sounds.node_sound_leaves_defaults({
 			footstep={name="default_dirt_footstep", gain=0.7},
@@ -420,7 +420,7 @@ minetest.register_node("mcl_crimson:shroomlight", {
 	}),
 })
 
-minetest.register_node("mcl_crimson:warped_nylium", {
+core.register_node("mcl_crimson:warped_nylium", {
 	description = S("Warped Nylium"),
 	tiles = {
 		"warped_nylium.png",
@@ -439,7 +439,7 @@ minetest.register_node("mcl_crimson:warped_nylium", {
 	_on_bone_meal = on_bone_meal,
 })
 
-minetest.register_node("mcl_crimson:crimson_fungus", {
+core.register_node("mcl_crimson:crimson_fungus", {
 	description = S("Crimson Fungus"),
 	_tt_help = S("Crimson fungus is a mushroom found in the nether's crimson forest."),
 	_doc_items_longdesc = S("Crimson fungus is a mushroom found in the nether's crimson forest."),
@@ -460,10 +460,10 @@ minetest.register_node("mcl_crimson:crimson_fungus", {
 	node_placement_prediction = "",
 	on_place = place_fungus,
 	_on_bone_meal = function(_, _, _, pos)
-		if minetest.get_node(vector.offset(pos,0,-1,0)).name == "mcl_crimson:crimson_nylium" then
+		if core.get_node(vector.offset(pos,0,-1,0)).name == "mcl_crimson:crimson_nylium" then
 			if math.random() > 0.40 then return end --fungus has a 40% chance to grow when bone mealing
 			if check_for_bedrock(pos) then return false end
-			minetest.remove_node(pos)
+			core.remove_node(pos)
 			return generate_fungus_tree(pos, "crimson")
 		end
 	end,
@@ -476,7 +476,7 @@ mcl_flowerpots.register_potted_flower("mcl_crimson:crimson_fungus", {
 	image = "farming_crimson_fungus.png",
 })
 
-minetest.register_node("mcl_crimson:crimson_roots", {
+core.register_node("mcl_crimson:crimson_roots", {
 	description = S("Crimson Roots"),
 	drawtype = "plantlike",
 	tiles = { "crimson_roots.png" },
@@ -505,7 +505,7 @@ mcl_flowerpots.register_potted_flower("mcl_crimson:crimson_roots", {
 	image = "crimson_roots.png",
 })
 
-minetest.register_node("mcl_crimson:crimson_nylium", {
+core.register_node("mcl_crimson:crimson_nylium", {
 	description = S("Crimson Nylium"),
 	tiles = {
 		"crimson_nylium.png",
@@ -524,27 +524,27 @@ minetest.register_node("mcl_crimson:crimson_nylium", {
 	_on_bone_meal = on_bone_meal,
 })
 
-minetest.register_abm({
+core.register_abm({
 	label = "Turn Crimson Nylium and Warped Nylium below solid block into Netherrack",
 	nodenames = {"mcl_crimson:crimson_nylium","mcl_crimson:warped_nylium"},
 	neighbors = {"group:solid"},
 	interval = 8,
 	chance = 50,
 	action = function(pos, node)
-		if minetest.get_item_group(minetest.get_node(vector.offset(pos, 0, 1, 0)).name, "solid") > 0 then
+		if core.get_item_group(core.get_node(vector.offset(pos, 0, 1, 0)).name, "solid") > 0 then
 			node.name = "mcl_nether:netherrack"
-			minetest.swap_node(pos, node)
+			core.swap_node(pos, node)
 		end
 	end
 })
 
-minetest.register_abm({
+core.register_abm({
 	label = "Weeping Vines and Twisting Vines growth",
 	nodenames = { "mcl_crimson:weeping_vines", "mcl_crimson:twisting_vines" },
 	interval = 47 * 2.5,
 	chance = 4,
 	action = function(pos, node)
-		if grow_vines_direction[minetest.get_item_group(node.name, "vinelike_node")] and node.param2 < max_vines_age then
+		if grow_vines_direction[core.get_item_group(node.name, "vinelike_node")] and node.param2 < max_vines_age then
 			grow_vines(pos, 1, node.name, nil, max_vines_age)
 		end
 	end
@@ -554,7 +554,7 @@ minetest.register_abm({
 local crimson_wood = "mcl_crimson:crimson_hyphae_wood"
 local warped_wood = "mcl_crimson:warped_hyphae_wood"
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:crimson_door 3",
 	recipe = {
 		{crimson_wood, crimson_wood},
@@ -563,7 +563,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:warped_door 3",
 	recipe = {
 		{warped_wood, warped_wood},
@@ -572,7 +572,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:crimson_trapdoor 2",
 	recipe = {
 		{crimson_wood, crimson_wood, crimson_wood},
@@ -580,7 +580,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:warped_trapdoor 2",
 	recipe = {
 		{warped_wood, warped_wood, warped_wood},
@@ -588,7 +588,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:crimson_fence 3",
 	recipe = {
 		{crimson_wood, "mcl_core:stick", crimson_wood},
@@ -596,7 +596,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:warped_fence 3",
 	recipe = {
 		{warped_wood, "mcl_core:stick", warped_wood},
@@ -604,7 +604,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:crimson_fence_gate",
 	recipe = {
 		{"mcl_core:stick", crimson_wood, "mcl_core:stick"},
@@ -612,7 +612,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
+core.register_craft({
 	output = "mcl_crimson:warped_fence_gate",
 	recipe = {
 		{"mcl_core:stick", warped_wood, "mcl_core:stick"},
