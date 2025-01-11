6392e2cf9da6d27dbfe8a2d148171e80303fae83
JoseDouglas26 <josedouglas20002014@gmail.com>
2025-01-11 16:22:24 -0300

    Draw for firework entity

---
+++ mods/ITEMS/mcl_fireworks/entity.lua
@@ -0,0 +1,25 @@
+core.register_entity("mcl_fireworks:rocket", {
+    _flight_duration = 0,
+    _stars = {},
+    initial_properties = {
+        _climb_speed = 3,
+        collide_with_objects = false,
+        glow = 14,
+        physical = true,
+        pointable = false,
+        textures = {"mcl_fireworks_rocket.png"},
+        visual = "sprite",
+    },
+    on_activate = function(self, staticdata)
+        local properties = core.deserialize(staticdata)
+        self._flight_duration = properties.flight_duration
+    end,
+    on_step = function(self, dtime, moveresult)
+        self.object:set_velocity(vector.new(0, self.initial_properties._climb_speed, 0))
+        self._flight_duration = self._flight_duration - dtime
+        if self._flight_duration <= 0 then
+            self.object:remove()
+        end
+    end
+})
+
--- mods/ITEMS/mcl_fireworks/init.lua
+++ mods/ITEMS/mcl_fireworks/init.lua
@@ -1,4 +1,5 @@
 local path = minetest.get_modpath("mcl_fireworks")
 
+--dofile(path .. "entity.lua")
 dofile(path .. "/register.lua")
 dofile(path .. "/crafting.lua")
