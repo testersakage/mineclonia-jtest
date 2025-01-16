ef135b20ce612e1dc4200557d332ef84619e130c
JoseDouglas26 <josedouglas20002014@gmail.com>
2025-01-16 10:17:33 -0300

    Some update on API.md e more cleaning

--- mods/ITEMS/mcl_doors/api_trapdoors.lua
+++ mods/ITEMS/mcl_doors/api_trapdoors.lua
@@ -5,7 +5,6 @@ local function get_fpos(placer, pointed_thing)
 	return finepos.y % 1 or 0
 end
 
----- Trapdoor ----
 local on_rotate
 if core.get_modpath("screwdriver") then
 	on_rotate = function(pos, node, _, mode, _)
@@ -14,22 +13,17 @@ if core.get_modpath("screwdriver") then
 			local minor = node.param2
 			if node.param2 >= 20 then
 				minor = node.param2 - 20
-				if minor == 3 then
-					minor = 1
-				elseif minor == 1 then
-					minor = 3
-				end
+				if minor == 3 then minor = 1 elseif minor == 1 then minor = 3 end
+
 				node.param2 = minor
 			else
-				if minor == 3 then
-					minor = 1
-				elseif minor == 1 then
-					minor = 3
-				end
+				if minor == 3 then minor = 1 elseif minor == 1 then minor = 3 end
+
 				node.param2 = minor
 				node.param2 = node.param2 + 20
 			end
 			core.set_node(pos, node)
+
 			return true
 		end
 	end
@@ -37,16 +31,10 @@ end
 
 function mcl_doors:register_trapdoor(name, def)
 	local groups = table.copy(def.groups)
-	if groups == nil then
-		groups = {}
-	end
+	if groups == nil then groups = {} end
 
-	if not def.sound_open then
-		def.sound_open = "doors_door_open"
-	end
-	if not def.sound_close then
-		def.sound_close = "doors_door_close"
-	end
+	if not def.sound_open then def.sound_open = "doors_door_open" end
+	if not def.sound_close then def.sound_close = "doors_door_close" end
 
 	local function punch(pos)
 		local me = core.get_node(pos)
@@ -65,14 +53,14 @@ function mcl_doors:register_trapdoor(name, def)
 
 	local on_rightclick
 	if not def.only_redstone_can_open then
-		on_rightclick = function(pos, _, _)
-			punch(pos)
-		end
+		on_rightclick = function(pos, _, _) punch(pos) end
 	end
 
 	-- Default help texts
 	local longdesc, usagehelp, tt_help
 	longdesc = def._doc_items_longdesc
+	usagehelp = def._doc_items_usagehelp
+
 	if not longdesc then
 		if def.only_redstone_can_open then
 			longdesc = S("Trapdoors are horizontal barriers which can be opened or closed and climbed like a ladder when open. They occupy the upper or lower part of a block, depending on how they have been placed. This trapdoor can only be opened or closed by redstone power.")
@@ -80,23 +68,21 @@ function mcl_doors:register_trapdoor(name, def)
 			longdesc = S("Trapdoors are horizontal barriers which can be opened or closed and climbed like a ladder when open. They occupy the upper or lower part of a block, depending on how they have been placed. This trapdoor can be opened or closed by hand or redstone power.")
 		end
 	end
-	usagehelp = def._doc_items_usagehelp
+
 	if not usagehelp and not def.only_redstone_can_open then
 		usagehelp = S("To open or close this trapdoor, rightclick it or send a redstone signal to it.")
 	end
-    if def.only_redstone_can_open then
+
+	if def.only_redstone_can_open then
         tt_help = S("Openable by redstone power")
     else
         tt_help = S("Openable by players and redstone power")
     end
-
 	-- Closed trapdoor
 	local tile_front = def.tile_front
 	local tile_side = def.tile_side
 
-	if not tile_side then
-		tile_side = tile_front
-	end
+	if not tile_side then tile_side = tile_front end
 
 	local tiles_closed = {
 		tile_front,
@@ -144,7 +130,7 @@ function mcl_doors:register_trapdoor(name, def)
 		inventory_image = def.inventory_image,
 		node_box = {
 			fixed = {
-				{-8/16, -8/16, -8/16, 8/16, -5/16, 8/16}
+				{-0.5, -0.5, -0.5, 0.5, -0.3125, 0.5}
 			},
 			type = "fixed"
 		},
@@ -164,11 +150,7 @@ function mcl_doors:register_trapdoor(name, def)
 			or (fpos < -0.5 and fpos > -0.999999999) then
 				param2 = param2 + 20
 
-				if param2 == 21 then
-					param2 = 23
-				elseif param2 == 23 then
-					param2 = 21
-				end
+				if param2 == 21 then param2 = 23 elseif param2 == 23 then param2 = 21 end
 			end
 
 			return core.item_place(itemstack, placer, pointed_thing, param2)
@@ -206,7 +188,7 @@ function mcl_doors:register_trapdoor(name, def)
 		drop = name,
 		groups = groups_open,
 		node_box = {
-			fixed = {-0.5, -0.5, 5/16, 0.5, 0.5, 0.5},
+			fixed = {-0.5, -0.5, 0.3125, 0.5, 0.5, 0.5},
 			type = "fixed"
 		},
 		tiles = tiles_open
