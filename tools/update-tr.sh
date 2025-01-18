54b9e40dbda042ccd6fcca17590e82849b4d4bd5
Elias Åström <ryvnf@riseup.net>
2025-01-18 16:20:27 +0100

    Make update-tr.sh ensure every mod has one po file

--- tools/update-tr.sh
+++ tools/update-tr.sh
@@ -9,3 +9,12 @@ for i in 0 1; do
     tools/generate_translation_strings/generate.sh mod_translation_updater.py
     ltt_convert.py mods --tr2po -r
 done
+
+# makes sure at least one translation file is present for every mod so it can
+# be detected by weblate; there is probably a cleaner way to do this
+find -name poconvert | while read poconvert; do
+	if (( $(ls "$poconvert" | wc -l) == 1 )); then
+		sed 's/"Language: /"Language: fr/' "$poconvert/template.pot" > "$poconvert/fr.po"
+		echo "Created new translation file in $poconvert"
+	fi
+done
