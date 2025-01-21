7d67c9c4cab33beecc735977ab9e45ac6526799f
Jürgen Rühle <j-r@online.de>
2025-01-21 21:34:34 +0100

    Don't translate unknown ingredient log message
    It just makes the log less readable.

--- mods/HELP/mcl_craftguide/init.lua
+++ mods/HELP/mcl_craftguide/init.lua
@@ -761,7 +761,7 @@ local function get_init_items()
 								usages_cache[ingredient] = usages_cache[ingredient] or {}
 								table.insert(usages_cache[ingredient], recipe)
 							else
-								minetest.log("warning", S("Invalid crafting ingredient: \"@1\" (dosent exist)", ingredient))
+								minetest.log("warning", "[mcl_craftguide] ingredient \"" .. ingredient .. "\" doesn't exist")
 							end
 						end
 					end
