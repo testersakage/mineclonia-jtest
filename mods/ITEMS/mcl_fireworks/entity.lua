54533caecfdcbce421e6e9e89de321c4e4d32bb9
cora <coradelamouche@gmx.ch>
2025-01-24 00:01:39 +0100

    Fix trail particlespawner to keep emitting particles

--- mods/ITEMS/mcl_fireworks/entity.lua
+++ mods/ITEMS/mcl_fireworks/entity.lua
@@ -18,22 +18,24 @@ core.register_entity("mcl_fireworks:rocket", {
         self._flight_duration = properties.flight_duration
         --self._stars = core.deserialize(properties.stars)
 
-        core.add_particlespawner({
+        self._trail_spawner = core.add_particlespawner({
             attached = self.object,
             amount = math.ceil(15 * self._flight_duration),
             maxpos = vector.new(0.25, -1, 0.25),
             minpos = vector.new(-0.25, -1, -0.25),
-            maxexptime = 0.75,
-            minexptime = 1.25,
             maxvel = vector.zero(),
             minvel = vector.zero(),
             maxsize = 1.25,
             minsize = 0.75,
             collisiondetection = false,
             vertical = true,
-            texture =  "mcl_particles_instant_effect.png"
+            texture =  "mcl_particles_instant_effect.png",
+			time = 0,
         })
     end,
+	on_deactivate = function(self)
+		core.delete_particlespawner(self._trail_spawner)
+	end,
     on_step = function(self, dtime)
         self.object:set_velocity(vector.new(0, self.initial_properties._climb_speed, 0))
         self._flight_duration = self._flight_duration - dtime
