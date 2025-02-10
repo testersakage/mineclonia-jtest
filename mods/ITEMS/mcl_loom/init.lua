4bb324fd158e38463a0337108a2ad8ec8f0ab3d4
ellesheepy <your.sheepy@gmail.com>
2025-02-11 00:17:21 +0800

    Refactor loom craft-ability checks.
    Update crafted banner's layer description to display limit.
    luacheck amendments.

--- mods/ITEMS/mcl_loom/init.lua
+++ mods/ITEMS/mcl_loom/init.lua
@@ -24,7 +24,7 @@ table.sort(dyerecipes)
 
 local selected_pattern_by_pos = {}
 
-function get_formspec_preview_button (x, y, layers, base_unicolor, unicolor, pattern_id)
+local function get_formspec_preview_button (x, y, layers, base_unicolor, unicolor, pattern_id)
 	local name = "item_button_" .. preview_item_prefix .. pattern_id
 	local new_layers = layers and table.copy(layers) or {}
 	table.insert(new_layers, { color = "unicolor_"..unicolor, pattern = pattern_id })
@@ -34,71 +34,74 @@ function get_formspec_preview_button (x, y, layers, base_unicolor, unicolor, pat
 	return result
 end
 
+local function can_add_layer(pos) -- Make sure banner and dye are valid, and return all 3 input stacks and data
+	if not pos then return end
+	local meta = core.get_meta(pos)
+	if not meta then return end
+	local inv = meta:get_inventory()
+	if not inv or inv:is_empty("banner") or inv:is_empty("dye") then return end
+
+	local banner = inv:get_stack("banner", 1)
+	local layers = mcl_banners.read_layers(banner:get_meta())
+	if #layers >= mcl_banners.max_craftable_layers then return end
+
+	local dye, pattern = inv:get_stack("dye", 1), inv:get_stack("pattern", 1)
+	local d_name, p_name = dye:get_name(), pattern:get_name()
+	local d_def, p_def = core.registered_items[d_name], core.registered_items[p_name]
+	if not d_def or not d_def._color or core.get_item_group(d_name, "dye") <= 0 then return end
+	local color_def = mcl_dyes.colors[d_def._color]
+	if not color_def or not color_def.unicolor then return end
+
+	return inv, banner, layers, dye, color_def.unicolor, pattern, p_def and p_def._pattern
+end
+
 local function get_formspec(pos)
 	local patterns = {}
 	local max_y = -2.2 -- Pattern container content height 3.5, minus border 0.2, minus first row 1.1
 	local preview_texture = "blank.png"
-	if pos then
-		local meta = minetest.get_meta(pos)
-		local inv = meta:get_inventory()
-		if not inv:is_empty("banner") then
-			local banner = inv:get_stack("banner", 1)
-			local layers = mcl_banners.read_layers(banner:get_meta())
-			if #layers < mcl_banners.max_craftable_layers then
-				local selected_preview = selected_pattern_by_pos[tostring(pos)] or ""
-				local color
-				local dye_name = inv:get_stack("dye", 1):get_name()
-				local dye_def = minetest.registered_items[dye_name]
-				if dye_def and core.get_item_group(dye_name, "dye") > 0 and dye_def._color then
-					color = mcl_dyes.colors[dye_def._color].unicolor
-					if color then
-						local pitem = inv:get_stack("pattern", 1):get_name()
-						local pdef = minetest.registered_items[pitem]
-						local light_colours = { "white", "grey", "green", "light_red" } -- If white / light-grey / lime / pink,
-						local base_color = table.indexof( light_colours, color ) > 0 and "darkgrey" or "grey" -- Make base dark grey
-						if pdef and pdef._pattern then
-							table.insert(patterns, get_formspec_preview_button(0.1, 0.1, nil, base_color, color, pdef._pattern))
-							selected_preview = pdef._pattern
-							selected_pattern_by_pos[tostring(pos)] = selected_preview
-						else
-							local x_len = 0.1
-							local y_len = 0.1
-							local found_preview = false
-							for _, v in ipairs(dyerecipes) do
-								if x_len > 5 then
-									max_y = max_y + 1.1
-									y_len = y_len + 1.1
-									x_len = 0.1
-								end
-								table.insert(patterns, get_formspec_preview_button(x_len, y_len, nil, base_color, color, v))
-								x_len = x_len + 1.1
-								if v == selected_preview then
-									found_preview = true
-								end
-							end
-							if not found_preview then
-								selected_preview = ""
-							end
-						end
-						if selected_preview ~= "" then
-							local banner_color = banner:get_definition()._unicolor
-							local preview_layers = table.copy( layers )
-							table.insert(preview_layers, {color = "unicolor_"..color, pattern = selected_preview})
-							preview_texture = mcl_banners.make_banner_texture(banner_color, preview_layers, "item")
-							preview_texture = "[combine:30x48:-9,0=" .. mcl_banners.escape_texture(preview_texture)
-							preview_texture = core.formspec_escape(preview_texture)
-						end
-					end
+	local _, banner, layers, _, color, _, pattern_id = can_add_layer(pos)
+	if color then
+		local selected_preview = selected_pattern_by_pos[tostring(pos)] or ""
+		local light_colours = { "white", "grey", "green", "light_red" } -- If white / light-grey / lime / pink,
+		local base_color = table.indexof( light_colours, color ) > 0 and "darkgrey" or "grey" -- Make base dark grey
+		if pattern_id then
+			-- With pattern, one single option
+			table.insert(patterns, get_formspec_preview_button(0.1, 0.1, nil, base_color, color, pattern_id))
+			selected_preview = pattern_id
+			selected_pattern_by_pos[tostring(pos)] = selected_preview
+		else
+			-- No pattern, all dye options
+			local x_len = 0.1
+			local y_len = 0.1
+			for _, v in ipairs(dyerecipes) do
+				if x_len > 5 then
+					max_y = max_y + 1.1
+					y_len = y_len + 1.1
+					x_len = 0.1
 				end
+				table.insert(patterns, get_formspec_preview_button(x_len, y_len, nil, base_color, color, v))
+				x_len = x_len + 1.1
+			end
+			if table.indexof(dyerecipes, selected_preview) <= 0 then
+				selected_preview = ""
 			end
 		end
+		if selected_preview ~= "" then
+			local banner_color = banner:get_definition()._unicolor
+			local preview_layers = table.copy(layers)
+			table.insert(preview_layers, {color = "unicolor_"..color, pattern = selected_preview})
+			preview_texture = mcl_banners.make_banner_texture(banner_color, preview_layers, "item")
+			preview_texture = "[combine:30x48:-9,0=" .. mcl_banners.escape_texture(preview_texture)
+			preview_texture = core.formspec_escape(preview_texture)
+		end
 	end
-	if max_y < 0 then max_y = 0 else max_y = math.ceil(max_y * 10) end
+	if max_y < 0 then max_y = 0 else max_y = math.ceil(max_y * 10) end -- Convert content height to scrollbar max
 
 	local formspec = "formspec_version[4]"..
 	"size[11.75,10.425]"..
 	"label[0.375,0.375;" .. F(C(mcl_formspec.label_color, S("Loom"))) .. "]"..
 
+	-- Inputs
 	mcl_formspec.get_itemslot_bg_v4(0.375,0.75,1,1)..
 	mcl_formspec.get_itemslot_bg_v4(0.375,0.75,1,1,0,"mcl_loom_itemslot_bg_banner.png")..
 	"list[context;banner;0.34,0.75;1,1;]"..
@@ -109,22 +112,21 @@ local function get_formspec(pos)
 	mcl_formspec.get_itemslot_bg_v4(0.375,3.25,1,1,0,"mcl_loom_itemslot_bg_pattern.png")..
 	"list[context;pattern;0.375,3.25;1,1;]"..
 
+	-- Options and preview
 	"box[1.575,0.7;5.7,3.6;"..mcl_colors.DARK_GRAY.."]"..
 	"scroll_container[1.575,0.7;6,3.6;pattern_scroll;vertical;0.1]"..
 	table.concat(patterns)..
 	"scroll_container_end[]"..
 	"scrollbaroptions[arrows=show;thumbsize=30;min=0;max="..max_y.."]"..
 	"scrollbar[7.25,0.7;0.4,3.6;vertical;pattern_scroll;]"..
-
 	"image_button[7.85,0.7;2.25,3.6;"..preview_texture..";item_button_loom_craft_banner;]"..
 
+	-- Output and inventory
 	mcl_formspec.get_itemslot_bg_v4(10.375,2,1,1)..
 	"list[context;output;10.375,2;1.1,1.1;]"..
-
 	"label[0.375,4.7;" .. F(C(mcl_formspec.label_color, S("Inventory"))) .. "]"..
 	mcl_formspec.get_itemslot_bg_v4(0.375, 5.1, 9, 3)..
 	"list[current_player;main;0.375,5.1;9,3;9]"..
-
 	mcl_formspec.get_itemslot_bg_v4(0.375, 9.05, 9, 1)..
 	"list[current_player;main;0.375,9.05;9,1;]"..
 
@@ -146,27 +148,13 @@ local function update_formspec(pos)
 	meta:set_string("formspec", get_formspec(pos))
 end
 
-local function create_banner(stack, pattern, color)
-	local im = stack:get_meta()
-	local layers = mcl_banners.read_layers(im)
-	local max = mcl_banners.max_craftable_layers
-	if #layers >= max then return end
-	table.insert(layers,{
-		pattern = pattern,
-		color = "unicolor_"..mcl_dyes.colors[color].unicolor
-	})
-	mcl_banners.write_layers(im, layers)
-	tt.reload_itemstack_description(stack)
-	return stack
-end
-
 local function sort_stack(stack)
 	for group, list in pairs({ banner = "banner", dye = "dye", banner_pattern = "pattern" }) do
 		if minetest.get_item_group(stack:get_name(), group) > 0 then return list end
 	end
 end
 
-local function allow_put(pos, listname, index, stack, player) ---@diagnostic disable-line: unused-local
+local function allow_put(pos, listname, _, stack, player)
 	local name = player:get_player_name()
 	if minetest.is_protected(pos, name) then
 		minetest.record_protection_violation(pos, name)
@@ -223,60 +211,56 @@ minetest.register_node("mcl_loom:loom", {
 		mcl_util.drop_items_from_meta_container({"banner", "dye", "pattern", "output"})(pos, nil, old_meta)
 	end,
 	on_rightclick = update_formspec,
-	on_receive_fields = function(pos, formname, fields, sender) ---@diagnostic disable-line: unused-local
+	on_receive_fields = function(pos, _, fields, sender)
 		local sender_name = sender:get_player_name()
 		if minetest.is_protected(pos, sender_name) then
 			minetest.record_protection_violation(pos, sender_name)
 			return
 		end
 
-		local meta = minetest.get_meta(pos)
-		local inv = meta:get_inventory()
-		local pos_str = tostring(pos)
-		local pattern_id = selected_pattern_by_pos[pos_str]
-		if not inv:is_empty("banner") and not inv:is_empty("dye") then
+		local inv, banner, layers, dye, color, p_item, p_id = can_add_layer(pos)
+		if color then
+			local pos_str = tostring(pos)
+			local pattern_id = selected_pattern_by_pos[pos_str]
 			for k, _ in pairs(fields or {}) do
-				if tostring(k) ~= "" and k:find("^item_button_"..preview_item_prefix) then
+				if tostring(k) ~= "" and k:find("^item_button_"..preview_item_prefix) then -- Select pattern
 					local str = k:gsub("^item_button_"..preview_item_prefix,"")
 					selected_pattern_by_pos[pos_str] = str
-				elseif k == "item_button_loom_craft_banner" and pattern_id then
-					local pattern = inv:get_stack("pattern",1)
-					local dye = inv:get_stack("dye",1)
-					local cdef = minetest.registered_items[dye:get_name()]
-					if not inv:is_empty("pattern") then
-						local pdef = minetest.registered_items[pattern:get_name()]
-						pattern_id = pdef._pattern
-					elseif not mcl_dyes.colors[cdef._color] or table.indexof(dyerecipes,pattern_id) == -1 then
-						pattern_id = nil
+				elseif k == "item_button_loom_craft_banner" and ( pattern_id or p_id ) then -- Craft
+					if p_id then
+						pattern_id = p_id -- If using pattern, override pattern_id.
+					elseif not pattern_id or table.indexof(dyerecipes,pattern_id) == -1 then
+						pattern_id = nil -- Make sure selected pattern use only dye.
 					end
-					if pattern_id then
-						local banner = inv:get_stack("banner",1)
-						local cbanner = banner:peek_item()
-						local new_banner = create_banner(cbanner,pattern_id,cdef._color)
-						if new_banner and not inv:is_empty("output") then
+					if pattern_id then -- Try make a new banner, may still fail to output.
+						local new_banner = banner:peek_item()
+						table.insert(layers, { color = "unicolor_"..color, pattern = pattern_id })
+						mcl_banners.write_layers(new_banner:get_meta(), layers)
+						if not inv:is_empty("output") then -- Output is non-empty?
 							local output_stack = inv:get_stack("output",1)
 							if output_stack:get_free_space() > 0 then
 								local max_layers = mcl_banners.max_craftable_layers
 								local out_desc, _, out_name = mcl_banners.update_description(output_stack, max_layers)
 								local new_desc, _, new_name = mcl_banners.update_description(new_banner, max_layers)
-								if out_desc == new_desc and out_name == new_name then
-									new_banner:set_count(output_stack:get_count() + 1)
+								if out_desc == new_desc and out_name == new_name then -- Compare FULL descriptions
+									new_banner:set_count(output_stack:get_count() + 1) -- Same? OK!
 								else
-									new_banner = nil
+									new_banner = nil -- Not same banner description.
 								end
 							else
-								new_banner = nil
+								new_banner = nil -- Output full.
 							end
 						end
-						if new_banner then
+						if new_banner then -- Consume inputs (one each) and update output.
 							dye:take_item()
 							inv:set_stack("dye", 1, dye)
 							banner:take_item()
 							inv:set_stack("banner", 1, banner)
-							if not inv:is_empty("pattern") then
-								pattern:take_item()
-								inv:set_stack("pattern", 1, pattern)
+							if p_id then
+								p_item:take_item()
+								inv:set_stack("pattern", 1, p_item)
 							end
+							mcl_banners.update_description(new_banner)
 							inv:set_stack("output", 1, new_banner)
 						end
 					end
@@ -286,7 +270,7 @@ minetest.register_node("mcl_loom:loom", {
 		update_formspec(pos)
 	end,
 
-	allow_metadata_inventory_take = function(pos, listname, index, stack, player) ---@diagnostic disable-line: unused-local
+	allow_metadata_inventory_take = function(pos, listname, _, stack, player)
 		if listname == "sorter" then return 0 end
 		local name = player:get_player_name()
 		if minetest.is_protected(pos, name) then
@@ -296,7 +280,7 @@ minetest.register_node("mcl_loom:loom", {
 			return stack:get_count()
 		end
 	end,
-	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, count, player) ---@diagnostic disable-line: unused-local
+	allow_metadata_inventory_move = function(pos, from_list, from_index, to_list, to_index, _, player)
 		if from_list == "sorter" or to_list == "sorter" then return 0 end
 		local inv = minetest.get_meta(pos):get_inventory()
 		local stack = inv:get_stack(from_list,from_index)
@@ -304,7 +288,7 @@ minetest.register_node("mcl_loom:loom", {
 	end,
 	allow_metadata_inventory_put = allow_put,
 	on_metadata_inventory_move = update_formspec,
-	on_metadata_inventory_put = function(pos, listname, index, stack, player) ---@diagnostic disable-line: unused-local
+	on_metadata_inventory_put = function(pos, listname, _, stack, _)
 		if listname == "sorter" then
 			local inv = minetest.get_meta(pos):get_inventory()
 			inv:add_item(sort_stack(stack), stack)
