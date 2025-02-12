973dc956007f19288c969032d1d8195c9d67c752
Elias Åström <ryvnf@riseup.net>
2025-02-12 18:13:34 +0100

    Rename _mcl_partial group to pathfinder_partial

--- mods/ITEMS/mcl_honey/init.lua
+++ mods/ITEMS/mcl_honey/init.lua
@@ -63,7 +63,7 @@ minetest.register_node("mcl_honey:honey_block", {
 	tiles = {"mcl_honey_block_side.png"},
 	is_ground_content = false,
 	use_texture_alpha = minetest.features.use_texture_alpha_string_modes and "blend" or true,
-	groups = { handy = 1, deco_block = 1, fall_damage_add_percent = -80, _mcl_partial = 2, },
+	groups = { handy = 1, deco_block = 1, fall_damage_add_percent = -80, pathfinder_partial = 2, },
 	paramtype = "light",
 	drawtype = "nodebox",
 	node_box = {
