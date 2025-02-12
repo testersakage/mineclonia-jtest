de9402349c7970e22221d79d924a00d584f1ee76
ellesheepy <your.sheepy@gmail.com>
2025-02-12 19:24:42 +0800

    Use meta texture if available for hud offhand slot.

--- mods/HUD/mcl_offhand/init.lua
+++ mods/HUD/mcl_offhand/init.lua
@@ -95,7 +95,9 @@ mcl_player.register_globalstep(function(player)
 	local offhand_hud = mcl_offhand[player].hud
 	local item = minetest.registered_items[offhand_item]
 	if offhand_item ~= "" and item then
-		local item_texture = item.inventory_image .. "^[resize:" .. max_offhand_px .. "x" .. max_offhand_px
+		local meta_texture = itemstack:get_meta():get_string("inventory_overlay") 
+		if not meta_texture or meta_texture == "" then meta_texture = item.inventory_image end
+		local item_texture = meta_texture .. "^[resize:" .. max_offhand_px .. "x" .. max_offhand_px
 		local position = {x = 0.5, y = 1}
 		local offset = {x = -320, y = -32}
 
