5e04b1fd58eb4a0e02cc8c169ff6f95a197c465e
Miguel_PL <miguel_pl@noreply.codeberg.org>
2025-02-12 00:46:16 +0000

    fix

--- mods/HELP/mcl_craftguide/init.lua
+++ mods/HELP/mcl_craftguide/init.lua
@@ -761,7 +761,7 @@ local function get_init_items()
 								usages_cache[ingredient] = usages_cache[ingredient] or {}
 								table.insert(usages_cache[ingredient], recipe)
 							else
-								core.log("warning", S("Invalid crafting ingredient: \"@1\" (dosent exist)", ingredient))
+								core.log("warning", "[mcl_craftguide] ingredient \"" .. ingredient .. "\" doesn't exist")
 							end
 						end
 					end
