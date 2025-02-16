679a371efde35efd60610857bffa55c597d8dd9a
ellesheepy <your.sheepy@gmail.com>
2025-02-17 02:41:20 +0800

    Remove the crafting recipe of 4x stripped bamboo to 3x stripped bamboo.

--- mods/ITEMS/mcl_bamboo/init.lua
+++ mods/ITEMS/mcl_bamboo/init.lua
@@ -73,6 +73,10 @@ mcl_trees.register_wood("bamboo",{
 	},
 })
 
+if minetest.registered_nodes["mcl_trees:stripped_bamboo"] then
+	minetest.registered_nodes["mcl_trees:stripped_bamboo"]._mcl_crafting_output = nil
+end
+
 minetest.register_abm({
 	label = "Bamboo growth",
 	nodenames = {"group:bamboo_tree"},
