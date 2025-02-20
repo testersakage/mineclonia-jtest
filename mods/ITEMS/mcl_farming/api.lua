9b15c51d86165ce0c77ee6544a3cbf9013b34d3a
JoseDouglas26 <josedouglas20002014@gmail.com>
2025-02-20 02:04:51 -0300

    Groups and more info handling

--- mods/ITEMS/mcl_farming/api.lua
+++ mods/ITEMS/mcl_farming/api.lua
@@ -15,17 +15,31 @@ local tpl_crop = {
 function mcl_farming.register_simple_crop(id, defs, overrides)
     for i = 1, defs.stages do
         local mature, premature = i == defs.stages, i == 1
+        local desc = premature and S("@1 (Stage @2)", defs.premature_desc, i) or defs.mature_desc
         local subname = id .. not mature and "_" .. i or ""
 
-        if not defs.drops and defs.mature_drop then
+        if type(defs.drops) == "nil" or not defs.drops[i] then
             defs.drops[i] = premature and defs.seed or defs.mature_drop
         end
 
         core.register_node(":mcl_farming:" .. subname, table.merge(tpl_crop, {
             _doc_items_create_entry = mature or premature,
+            _doc_items_longdesc = premature and defs.premature_longdesc or defs.mature_longdesc,
             _mcl_baseitem = defs.seed,
             _mcl_fortune_drop = mature and defs.fortune_drops,
+            description = desc,
             drop = defs.drops[i],
+            groups = table.merge(defs.groups_per_stage[i] or defs.groups or {}, {
+                attached_node = 3, destroy_by_lava_flow = 1, dig_by_piston = 1, dig_by_water = 1,
+                dig_immediate = 1, not_in_creative_inventory = 1, plant = 1, unsticky = 1
+            }),
+            inventory_image = "",
+            selection_box = {
+                fixed = {},
+                type = "fixed"
+            },
+            tiles = {},
+            wield_image = ""
         }, overrides or {}))
     end
 end
