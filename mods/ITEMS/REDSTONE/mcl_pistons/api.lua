5e12f9b7751b750edf32d088ca4db58afb2681ab
Raylight <raylight@noreply.codeberg.org>
2025-02-23 16:09:01 +0000

    Optimization: Avoid redundant calls to minetest.get_objects_inside_radius
    When not pushing upwards, moved blocks positions as well as positions above moved block are added to `move_positions` (or handled right away in the old implementation). This can result in unnecessary calls to `minetest.get_objects_inside_radius`, for example when pushing vertically stacked slime blocks sideways.
    
    Added a table to avoid processing duplicate positions. An alternative is to do it before insertion.

--- mods/ITEMS/REDSTONE/mcl_pistons/api.lua
+++ mods/ITEMS/REDSTONE/mcl_pistons/api.lua
@@ -210,12 +210,18 @@ function mcl_pistons.push(pos, movedir, maximum, player_name, piston_pos)
 
 	-- Search for and move objects. Make sure they dont get moved more than once.
 	local moved_objects = {}
+	local processed = {}
 	for id, p in ipairs(move_positions) do
-		local objects = minetest.get_objects_inside_radius(p.pos, 0.9)
-		for _, obj in ipairs(objects) do
-			if not moved_objects[obj] then
-				move_object(obj, p.node, p.is_pulled)
-				moved_objects[obj] = true
+		local h = minetest.hash_node_position(p.pos)
+		if not processed[h] then
+			processed[h] = true
+			
+			local objects = minetest.get_objects_inside_radius(p.pos, 0.9)
+			for _, obj in ipairs(objects) do
+				if not moved_objects[obj] then
+					move_object(obj, p.node, p.is_pulled)
+					moved_objects[obj] = true
+				end
 			end
 		end
 	end
