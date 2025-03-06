05573efa6379edd25494757656fc5dda770dec1a
JoseDouglas26 <josedouglas20002014@gmail.com>
2025-03-06 12:50:06 -0300

    Creaking hearts

--- mods/ITEMS/mcl_pale_garden/init.lua
+++ mods/ITEMS/mcl_pale_garden/init.lua
@@ -48,246 +48,105 @@ mcl_trees.register_wood("pale_oak", {
     fence = {tiles = {"mcl_pale_garden_planks_pale_oak.png"}},
     fence_gate = {tiles = {"mcl_pale_garden_planks_pale_oak.png"}}
 })
---[[
-local PARTICLE_DISTANCE = 20
 
-local spawn_particlespawner = {
-	texture = "",
-	texpool = {},
-	time = 2,
-	glow = 1,
-	minvel = vector.zero(),
-	maxvel = vector.zero(),
-	minacc = vector.new(-0.1, -0.1, -0.1),
-	maxacc = vector.new(0.2, 0.2, 0.2),
-	minexptime = 1.25,
-	maxexptime = 2,
-	minsize = 5.5,
-	maxsize= 7.5,
-	collisiondetection = true,
-	collision_removal = true,
+local tpl_heart = {
+	_mcl_blast_resistance = 10,
+	_mcl_hardness = 10,
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+        local tool = digger:get_wielded_item()
+
+        if not tool then return end
+
+        local silk_touch = mcl_enchanting.get_enchantment(tool, "silk_touch")
+        local fortune = mcl_enchanting.get_enchantment(tool, "fortune")
+        local creative = core.is_creative_enabled(digger:get_player_name())
+
+        if silk_touch == 1 then
+            -- Check for creaking heart variants
+            if oldnode.name ~= "mcl_pale_garden:creaking_heart" then
+                oldnode.name = "mcl_pale_garden:creaking_heart"
+            end
+
+            core.add_item(pos, oldnode.name)
+        else
+            if oldmetadata.fields["mcl_pale_garden:is_natural"] == "true" then
+                local xp_amount = math.random(20, 24)
+
+                if not creative then mcl_experience.throw_xp(pos, xp_amount) end
+            end
+
+            local clump_amount = math.random(1, 3)
+
+            if fortune >= 1 then clump_amount = clump_amount * fortune end
+
+            if not creative then
+				core.add_item(pos, "mcl_pale_garden:resin_clump " .. clump_amount)
+			end
+        end
+
+        tool:add_wear(mcl_autogroup.get_wear(tool:get_name(), "axey"))
+        digger:set_wielded_item(tool)
+    end,
+	after_place_node = function(pos, placer)
+        core.get_meta(pos):set_string("mcl_pale_garden:is_natural", "false")
+
+        return core.is_creative_enabled(placer:get_player_name())
+    end,
+	drop = "",
+	on_construct = function(pos)
+        core.get_meta(pos):set_string("mcl_pale_garden:is_natural", "true")
+    end,
+	on_place = mcl_util.rotate_axis,
+	on_rotate = screwdriver.rotate_3way,
+	paramtype2 = "facedir",
+	sounds = mcl_sounds.node_sound_wood_defaults()
 }
 
-minetest.register_node("vlf_pale_garden:inactive_creaking_heart", {
-	description = ("Creaking Heart"),
-	_doc_items_hidden = false,
-	paramtype2 = "facedir",
-	tiles = {"creaking_heart_top.png", "creaking_heart_top.png","creaking_heart.png"},
-	groups = {
-		handy = 1, axey = 1,
-		building_block = 1,
-		tree = 1, material_wood=1,
-		flammable = 3, fire_encouragement=5, fire_flammability=20,
-	},
-	sounds = mcl_sounds.node_sound_wood_defaults(),
-	on_place = mcl_util.rotate_axis,
-	on_rotate = screwdriver.rotate_3way,
-	_mcl_blast_resistance = 2,
-	_mcl_hardness = 2,
-})
+local ch_textures = {
+	"mcl_pale_garden_creaking_heart_top.png",
+	"mcl_pale_garden_creaking_heart_top.png",
+	"mcl_pale_garden_creaking_heart.png"
+}
 
-local function check_creaking_heart_activation(pos)
-    local node = minetest.get_node(pos)
-    local facedir = node.param2
+local ach_textures = {
+	"mcl_pale_garden_creaking_heart_top_active.png",
+	"mcl_pale_garden_creaking_heart_top_active.png",
+	"mcl_pale_garden_creaking_heart_active.png"
+}
 
-    local left_pos = {x = pos.x - 1, y = pos.y, z = pos.z}
-    local right_pos = {x = pos.x + 1, y = pos.y, z = pos.z}
-    local above_pos = {x = pos.x, y = pos.y + 1, z = pos.z}
-    local below_pos = {x = pos.x, y = pos.y - 1, z = pos.z}
-    local front_pos = {x = pos.x, y = pos.y, z = pos.z + 1}
-    local back_pos = {x = pos.x, y = pos.y, z = pos.z - 1}
+local function apply_overlay(textures, overlays, opacity)
+	local texture
+	local result = {}
 
-    local left_node = minetest.get_node(left_pos)
-    local right_node = minetest.get_node(right_pos)
-    local above_node = minetest.get_node(above_pos)
-    local below_node = minetest.get_node(below_pos)
-    local front_node = minetest.get_node(front_pos)
-    local back_node = minetest.get_node(back_pos)
+	for i = 1, 3 do
+		texture = textures[i] .. "^(" .. overlays[i] .. "^[opacity:" .. opacity .. ")"
+		table.insert(result, texture)
+	end
 
-    -- Table of valid block names for comparison
-    local valid_blocks = {
-        ["mcl_trees:tree_pale_oak"] = true,
-        ["mcl_trees:stripped_pale_oak"] = true,
-        ["mcl_trees:bark_stripped_pale_oak"] = true,
-        ["mcl_trees:bark_pale_oak"] = true
-    }
-
-    -- Check left and right nodes
-    if valid_blocks[left_node.name] and valid_blocks[right_node.name] then
-        if left_node.param2 == facedir and right_node.param2 == facedir then
-            return true
-        end
-    end
-
-    -- Check above and below nodes
-    if valid_blocks[above_node.name] and valid_blocks[below_node.name] then
-        if above_node.param2 == facedir and below_node.param2 == facedir then
-            return true
-        end
-    end
-
-    -- Check above and below nodes
-    if valid_blocks[front_node.name] and valid_blocks[back_node.name] then
-        if front_node.param2 == facedir and back_node.param2 == facedir then
-            return true
-        end
-    end
-
-    return false
+	return result
 end
 
-minetest.register_abm({
-    label = "Check Creaking Heart Activation",
-    nodenames = {"vlf_pale_garden:inactive_creaking_heart"},
-    interval = 2,
-    chance = 1,
-    action = function(pos, node)
-        if check_creaking_heart_activation(pos) then
-            minetest.set_node(pos, {name = "vlf_pale_garden:active_creaking_heart", param2 = node.param2})
-        end
-    end
-})
-
-minetest.register_node("vlf_pale_garden:active_creaking_heart", {
+core.register_node("mcl_pale_garden:creaking_heart", table.merge(tpl_heart, {
 	description = ("Creaking Heart"),
-	_doc_items_hidden = false,
-	paramtype2 = "facedir",
-	tiles = {"creaking_heart_top_active.png", "creaking_heart_top_active.png","creaking_heart_active.png"},
+	groups = {axey = 1, creaking_heart = 1, deco_block = 1, unmovable_by_piston = 1},
+	tiles = ch_textures
+}))
+
+core.register_node("mcl_pale_garden:creaking_heart_dormant", {
 	groups = {
-		handy = 1, axey = 1,
-		building_block = 1,
-		tree = 1, material_wood=1,
-		flammable = 3, fire_encouragement=5, fire_flammability=20,
+		axey = 1, creaking_heart = 2, not_in_creative_inventory = 1, unmovable_by_piston = 1
 	},
-	sounds = mcl_sounds.node_sound_wood_defaults(),
-	on_place = mcl_util.rotate_axis,
-	on_rotate = screwdriver.rotate_3way,
-	_mcl_blast_resistance = 2,
-	_mcl_hardness = 2,
-	on_dig = function(pos, node, digger)
-	-- Remove the creaking mob whose heart position matches the block
-	for _, object in pairs(minetest.get_objects_inside_radius(pos, 32)) do
-		local lua_entity = object:get_luaentity()
-		if lua_entity and lua_entity.name == "vlf_pale_garden:creaking_transient" and lua_entity._heart_pos and vector.equals(lua_entity._heart_pos, pos) then
-			minetest.add_particlespawner({
-				amount = 20, -- Number of particles
-				time = 1.5, -- Duration of the spawner
-				minpos = vector.subtract(lua_entity.object:get_pos(), {x = 0.5, y = 1.5, z = 0.5}),
-				maxpos = vector.add(lua_entity.object:get_pos(), {x = 0.5, y = 2.5, z = 0.5}),
-				minvel = {x = -0.5, y = -2, z = -0.5}, -- Particles only fall downwards
-				maxvel = {x = 0.5, y = -4, z = 0.5},  -- Downward velocity range
-				minacc = {x = 0, y = -7.81, z = 0}, -- Gravity effect pulling particles down
-				maxacc = {x = 0, y = -7.81, z = 0},
-				minexptime = 0.5, -- Minimum lifetime of particles
-				maxexptime = 1, -- Maximum lifetime of particles
-				minsize = 1, -- Minimum size of particles
-				maxsize = 3, -- Maximum size of particles
-				texture = "tree_creak_particle.png", -- Particle texture
-				glow = 0, -- Glow effect (optional)
-			})
-			object:remove()
-			-- Particle spawner to simulate tree-like creaking falling apart
-		end
-	end
-	minetest.node_dig(pos, node, digger)
-end,
-
+	tiles = apply_overlay(ach_textures, ch_textures, 159)
 })
 
-minetest.register_abm({
-	label = "Creaking Heart Mob Spawner",
-	nodenames = {"vlf_pale_garden:active_creaking_heart"},
-	interval = 2,
-	chance = 1,
-	action = function(pos, node)
-		-- Get current time of day
-		local time_of_day = minetest.get_timeofday()
-		local meta = minetest.get_meta(pos)
-		local has_spawned = meta:get_int("creaking_spawned") == 1
-
-		-- Only attempt to spawn if it's night and a mob hasn't already spawned
-		if (time_of_day >= 0.8 or time_of_day <= 0.2) and not has_spawned then
-			local spawn_pos = nil
-
-			for dy = 0, -10, -1 do
-				for dx = -5, 5 do
-					for dz = -5, 5 do
-						if math.abs(dx) + math.abs(dz) <= 5 then
-							local check_pos = {x = pos.x + dx, y = pos.y + dy, z = pos.z + dz}
-							local node_below = minetest.get_node(check_pos)
-							local node_above_pos = {x = check_pos.x, y = check_pos.y + 1, z = check_pos.z}
-							local node_above = minetest.get_node(node_above_pos)
-							local node_below_def = minetest.registered_nodes[node_below.name]
-							if node_below_def and node_below_def.walkable and minetest.get_item_group(node_below.name, "solid") > 0
-							and node_above.name == "air" then
-								spawn_pos = node_above_pos
-								break
-							end
-						end
-					end
-				end
-			end
-
-			-- If a valid spawn position was found, spawn the mob and mark it as spawned
-			if spawn_pos and not minetest.is_protected(spawn_pos, "") then
-				local mob = mcl_mobs.spawn(spawn_pos, "vlf_pale_garden:creaking_transient")
-				if mob then
-					local lua_entity = mob:get_luaentity()
-					if lua_entity then
-						lua_entity._heart_pos = pos
-						lua_entity.spawn_from_heart = true
-
-						-- Particle effect
-						local amount = 6
-						local name = "mcl_particles_generic.png^[colorize:#A0A0A0:255"
-						for _, pl in pairs(minetest.get_connected_players()) do
-							if vector.distance(pos, pl:get_pos()) < PARTICLE_DISTANCE then
-								table.insert(spawn_particlespawner.texpool, {
-									name = name,
-									animation = {type = "vertical_frames", aspect_w = 8, aspect_h = 8, length = 1.9},
-								})
-								minetest.add_particlespawner(table.merge(spawn_particlespawner, {
-									amount = amount,
-									minpos = vector.subtract(lua_entity.object:get_pos(), {x = 0.5, y = 0.5, z = 0.5}),
-									maxpos = vector.add(lua_entity.object:get_pos(), {x = 0.5, y = 1.5, z = 0.5}),
-									playername = pl:get_player_name(),
-								}))
-							end
-						end
-					end
-					-- Mark that a mob has been spawned this night
-					meta:set_int("creaking_spawned", 1)
-				end
-			end
-		elseif time_of_day > 0.2 and time_of_day < 0.8 and has_spawned then
-			-- Remove existing "creaking" mobs that match the heart position if day
-			meta:set_int("creaking_spawned", 0)
-			for _, object in pairs(minetest.get_objects_inside_radius(pos, 32)) do
-				local lua_entity = object:get_luaentity()
-				if lua_entity and lua_entity.name == "vlf_pale_garden:creaking_transient" and lua_entity._heart_pos and vector.equals(lua_entity._heart_pos, pos) then
-					local amount = 6
-					local name = "mcl_particles_generic.png^[colorize:#A0A0A0:255"
-					for _, pl in pairs(minetest.get_connected_players()) do
-						if vector.distance(pos, pl:get_pos()) < PARTICLE_DISTANCE then
-							table.insert(spawn_particlespawner.texpool, {
-								name = name,
-								animation = {type = "vertical_frames", aspect_w = 8, aspect_h = 8, length = 1.9},
-							})
-							minetest.add_particlespawner(table.merge(spawn_particlespawner, {
-								amount = amount,
-								minpos = vector.subtract(lua_entity.object:get_pos(), {x = 0.5, y = 0.5, z = 0.5}),
-								maxpos = vector.add(lua_entity.object:get_pos(), {x = 0.5, y = 1.5, z = 0.5}),
-								playername = pl:get_player_name(),
-							}))
-						end
-					end
-					object:remove()
-				end
-			end
-		end
-	end,
+core.register_node("mcl_pale_garden:creaking_heart_active", {
+	groups = {
+		axey = 1, creaking_heart = 3, not_in_creative_inventory = 1, unmovable_by_piston = 1
+	},
+	tiles = ach_textures
 })
 
+--[[
 minetest.register_node("vlf_pale_garden:closed_eyeblossom", {
 	description = ("Closed Eyeblossom"),
 	drawtype = "plantlike",
