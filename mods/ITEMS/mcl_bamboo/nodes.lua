70c3d59886ce725685b740e75cc3a1413bc6124c
cora <coradelamouche@gmx.ch>
2025-03-14 01:47:43 +0100

    Fix scaffolding selection boxes to match the nodeboxes

--- mods/ITEMS/mcl_bamboo/nodes.lua
+++ mods/ITEMS/mcl_bamboo/nodes.lua
@@ -180,12 +180,6 @@ minetest.register_node("mcl_bamboo:scaffolding", {
 			{-0.5, -0.5, 0.375, -0.375, 0.5, 0.5},
 		}
 	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-		},
-	},
 	is_ground_content = false,
 	walkable = false,
 	climbable = true,
@@ -270,12 +264,6 @@ minetest.register_node("mcl_bamboo:scaffolding_horizontal", {
 			{-0.5, -0.5, 0.375, -0.375, 0.5, 0.5},
 			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
 		}
-	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5},
-		},
 	},
 	groups = { handy=1, axey=1, flammable=3, building_block=1, material_wood=1, fire_encouragement=5, fire_flammability=60, not_in_creative_inventory = 1, falling_node = 1, scaffolding = 1 },
 	_mcl_after_falling = function(pos)
