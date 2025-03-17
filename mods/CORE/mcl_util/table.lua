fa0c562e142e64d94ccf87e4e613d1b3e02ce175
Mikita Wiśniewski <rudzik8@protonmail.com>
2025-03-17 20:12:41 +0700

    mcl_util: New `table` methods: `update_deep` & `merge_deep`
    * `table.update_deep` is an alternative to `table.update` that updates
      the table recursively
    * `table.merge_deep` is a copy-wrapper around `table.update_deep`
      * Same as `table.merge`, but recursive
    * General table.lua code clean-up and annotation

--- mods/CORE/mcl_util/table.lua
+++ mods/CORE/mcl_util/table.lua
@@ -1,6 +1,6 @@
--- Updates all values in t using values from to*.
+-- Updates all values in t using values from ...
 function table.update(t, ...)
-	for _, to in ipairs {...} do
+	for _, to in ipairs{...} do
 		for k, v in pairs(to) do
 			t[k] = v
 		end
@@ -8,9 +8,9 @@ function table.update(t, ...)
 	return t
 end
 
--- Updates nil values in t using values from to*.
+-- Updates nil values in t using values from ...
 function table.update_nil(t, ...)
-	for _, to in ipairs {...} do
+	for _, to in ipairs{...} do
 		for k, v in pairs(to) do
 			if t[k] == nil then
 				t[k] = v
@@ -20,11 +20,33 @@ function table.update_nil(t, ...)
 	return t
 end
 
+-- Recursively updates all values in t using values from ...
+function table.update_deep(t, ...)
+	for _, to in ipairs{...} do
+		for k, v in pairs(to) do
+			if type(t[k]) == "table" and type(v) == "table" then
+				table.update_deep(t[k], v)
+			else
+				t[k] = v
+			end
+		end
+	end
+	return t
+end
+
+-- Merges t with ..., returning a new table
 function table.merge(t, ...)
 	local t2 = table.copy(t)
 	return table.update(t2, ...)
 end
 
+-- Recursively merges t with ..., returning a new table
+function table.merge_deep(t, ...)
+	local t2 = table.copy(t)
+	return table.update_deep(t2, ...)
+end
+
+-- Reverses the order of elements inside t
 function table.reverse(t)
 	local a, b = 1, #t
 	while a < b do
@@ -33,6 +55,7 @@ function table.reverse(t)
 	end
 end
 
+-- Returns the maximum numerical index of t
 function table.max_index(t)
 	local max = 0
 	for k, _ in pairs(t) do
@@ -41,16 +64,18 @@ function table.max_index(t)
 	return max
 end
 
+-- Returns the amount of elements of t that are suitable by does_it_count (optional)
 function table.count(t, does_it_count)
 	local r = 0
 	for k, v in pairs(t) do
-		if does_it_count == nil or ( type(does_it_count) == "function" and does_it_count(k, v) ) then
+		if does_it_count == nil or (type(does_it_count) == "function" and does_it_count(k, v)) then
 			r = r + 1
 		end
 	end
 	return r
 end
 
+-- Returns a random element out of t
 function table.random_element(t)
 	local keyset = {}
 	for k, _ in pairs(t) do
