f178d03eb16b2b5a7fd5a7c7a25b805f951e869a
Raylight <raylight@noreply.codeberg.org>
2025-04-04 17:22:36 +0000

    Add util.lua

--- mods/ITEMS/REDSTONE/mcl_redstone/init.lua
+++ mods/ITEMS/REDSTONE/mcl_redstone/init.lua
@@ -32,7 +32,8 @@ core.register_on_mods_loaded(function()
 	end
 end)
 
-local modpath = core.get_modpath(core.get_current_modname())
+local modpath = minetest.get_modpath(minetest.get_current_modname())
+dofile(modpath.."/util.lua")
 dofile(modpath.."/logic.lua")
 dofile(modpath.."/eventqueue.lua")
 dofile(modpath.."/wire.lua")
