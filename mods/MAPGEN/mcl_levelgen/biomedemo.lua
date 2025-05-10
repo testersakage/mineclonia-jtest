64890178bb774497680557d55eb4ce2281d33ccf
Po Lu <hbm78z8ub@mozmail.com>
2025-05-10 11:15:02 +0800

    Import new level generator
    * mods/CORE/mcl_util/init.lua: Load table.lua in the mapgen
    environment also.
    
    * mods/MAPGEN/mcl_mapgen_core/api.lua (register_on_generated): Disable
    when mcl_levelgen is loaded.
    
    * mods/MAPGEN/mcl_levelgen/biomedemo.lua:
    * mods/MAPGEN/mcl_levelgen/biomes.lua:
    * mods/MAPGEN/mcl_levelgen/biomes.xpm:
    * mods/MAPGEN/mcl_levelgen/density_funcs.lua:
    * mods/MAPGEN/mcl_levelgen/init.lua:
    * mods/MAPGEN/mcl_levelgen/md5.lua:
    * mods/MAPGEN/mcl_levelgen/mg_register.lua:
    * mods/MAPGEN/mcl_levelgen/mod.conf (name, description):
    * mods/MAPGEN/mcl_levelgen/noise.lua:
    * mods/MAPGEN/mcl_levelgen/presets.lua:
    * mods/MAPGEN/mcl_levelgen/random.lua:
    * mods/MAPGEN/mcl_levelgen/register.lua:
    * mods/MAPGEN/mcl_levelgen/terrain.lua:
    * mods/MAPGEN/mcl_levelgen/terraindemo.lua (overworld_terrain):
    * mods/MAPGEN/mcl_mapgen_core/api.lua: Rudiments of a new noise-based
    level generator.

---
+++ mods/MAPGEN/mcl_levelgen/biomedemo.lua
@@ -0,0 +1,115 @@
+------------------------------------------------------------------------
+-- Overworld biome demonstration program.
+------------------------------------------------------------------------
+
+local biome_colors = {
+	DeepDark = "#000000",
+	WarmOcean = "#0000ac",
+	DeepFrozenOcean = "#404090",
+	LushCaves = "#9acd32",
+	MushroomIslands = "#ff00ff",
+	DeepOcean = "#000030",
+	DeepLukewarmOcean = "#000040",
+	DripstoneCaves = "#8b636c",
+	Desert = "#fa9418",
+	DeepColdOcean = "#202038",
+	Taiga = "#0b6659",
+	FrozenOcean = "#7070d6",
+	SnowyPlains = "#ffffff",
+	Plains = "#8db360",
+	WoodedMesa = "#b09765",
+	Savannah = "#bdb25f",
+	SavannahPlateau = "#a79d64",
+	DarkForest = "#40511a",
+	MangroveSwamp = "#6b8e23",
+	Grove = "#eeeee0",
+	LukewarmOcean = "#000090",
+	Ocean = "#000070",
+	Mesa = "#d94515",
+	SnowyTaiga = "#31554a",
+	ColdOcean = "#202070",
+	SnowySlopes = "#f5f5f5",
+	Swamp = "#07f9b2",
+	Jungle = "#537b09",
+	Forest = "#056621",
+	IceSpikes = "#b4dcdc",
+	FrozenPeaks = "#6495ed",
+	JaggedPeaks = "#708090",
+	ErodedMesa = "#ff6d3d",
+	River = "#0000ff",
+	BambooJungle = "#768e14",
+	OldGrowthPineTaiga = "#00cd00",
+	OldGrowthSpruceTaiga = "#818e79",
+	SunflowerPlains = "#b5db88",
+	FlowerForest = "#2d8e49",
+	StonyPeaks = "#8b7d6b",
+	StonyShore = "#a2a284",
+	FrozenRiver = "#a0a0ff",
+	Meadow = "#00cd66",
+	WindsweptForest = "#008b45",
+	WindsweptHills = "#696969",
+	WindsweptGravellyHills = "#a9a9a9",
+	Beach = "#fade55",
+	BirchForest = "#307444",
+	CherryGrove = "#ffc0cb",
+	OldGrowthBirchForest = "#29730f",
+	SparseJungle = "#7ba331",
+	SnowyBeach = "#faf0c0",
+	WindsweptSavannah = "#e5da87",
+}
+
+------------------------------------------------------------------------
+-- Main section of program.
+------------------------------------------------------------------------
+
+dofile ("init.lua")
+
+local WIDTH, HEIGHT = 1024, 1024
+local seed = mcl_levelgen.ull (0, 3228473)
+local level = mcl_levelgen.make_overworld_preset (seed)
+
+local next_color = 35
+local last_color = 126
+local color_allocations = {}
+local colordefs = {}
+local n_colors = 0
+
+local function alloc_color (biome)
+	local color = color_allocations[biome]
+	if color then
+		return color
+	end
+	local value = biome_colors[biome]
+	if not value then
+		error ("No color is defined for biome `" .. biome .. "'")
+	end
+	n_colors = n_colors + 1
+	if next_color == last_color then
+		error ("Colormap exhausted")
+	end
+	local char = string.char (next_color)
+	color_allocations[biome] = char
+	table.insert (colordefs, string.format ("\"%s c %s\",", char, value))
+	next_color = next_color + 1
+	return char
+end
+
+print ("/* XPM */")
+print ("static char *map[] = {")
+
+local rows = {}
+local floor = math.floor
+
+for z = floor (-WIDTH / 2), floor (WIDTH / 2) do
+	local row = {}
+	for x = floor (-WIDTH / 2), floor (WIDTH / 2) do
+		local sample = level:index_biomes (x, 16, z)
+		table.insert (row, alloc_color (sample))
+	end
+	table.insert (rows, "\"" .. table.concat (row) .. "\",")
+end
+
+print (string.format ("\"%d %d %d 1\",", WIDTH, HEIGHT, n_colors))
+print (table.concat (colordefs, "\n"))
+print (table.concat (rows, "\n"))
+print ("\n}\n")
