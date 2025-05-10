365cc826fdd477ce766ed4c62cecc39576a0473b
Po Lu <hbm78z8ub@mozmail.com>
2025-05-10 18:28:41 +0800

    Implement rudiments of a biome data store
    * mods/CORE/mcl_init/init.lua (mcl_vars): Adjust variables for
    the new level generator.
    
    * mods/HUD/mcl_info/init.lua (Biome): Print biome data provided
    by the level generator.
    
    * mods/MAPGEN/mcl_levelgen/biomegen.lua: New file.
    
    * mods/MAPGEN/mcl_levelgen/biomes.lua (rtree_index_closest):
    Optimize rtree indexing for the case where samples adjacent to
    each other are indexed in succession.
    
    * mods/MAPGEN/mcl_levelgen/init.lua (mcl_levelgen): Save
    `mcl_vars' in the IPC database.
    
    * mods/MAPGEN/mcl_levelgen/mg_register.lua
    (register_on_generated): Generate and provide biomes to the main
    thread.
    
    * mods/MAPGEN/mcl_levelgen/terrain.lua: Export the terrain
    generator.
    
    * mods/MAPGEN/mcl_levelgen/util.lua: New file.

--- mods/MAPGEN/mcl_levelgen/mg_register.lua
+++ mods/MAPGEN/mcl_levelgen/mg_register.lua
@@ -8,8 +8,9 @@ local mt_chunksize = math.max (1, tonumber (core.get_mapgen_setting ("chunksize"
 local chunksize = mt_chunksize * 16
 local overworld_terrain
 	= mcl_levelgen.make_terrain_generator (overworld_preset, chunksize)
+local OVERWORLD_OFFSET = mcl_levelgen.OVERWORLD_OFFSET
 
-local cids, param2s = {}, {}
+local cids, param2s, biomes = {}, {}, {}
 
 local area = nil
 
@@ -17,15 +18,31 @@ local function index (x, y, z)
 	return area:index (x, y, chunksize - z - 1)
 end
 
+local floor = math.floor
+
 core.register_on_generated (function (vmanip, minp, maxp, _)
 	local emin, emax = vmanip:get_emerged_area ()
 	area = VoxelArea (vector.subtract (emin, minp),
 			  vector.subtract (emax, minp))
 	vmanip:get_data (cids)
-	if not overworld_terrain:generate (minp.x, minp.y, -minp.z,
-					   cids, param2s, index) then
+	local block_x = minp.x / 16
+	local block_y = (OVERWORLD_OFFSET + minp.y) / 16
+	local block_z = minp.z / 16
+	assert (block_x == floor (block_x))
+	assert (block_y == floor (block_y))
+	assert (block_z == floor (block_z))
+	mcl_levelgen.generate_biomes_at_block (overworld_preset, biomes,
+					       block_x, block_y, block_z,
+					       mt_chunksize, mt_chunksize)
+	if not overworld_terrain:generate (minp.x, OVERWORLD_OFFSET + minp.y,
+					   -minp.z - chunksize, cids, param2s,
+					   index) then
 		return
 	end
 	vmanip:set_data (cids)
 	vmanip:set_param2_data (param2s)
+
+	local compressed = mcl_levelgen.encode_biomes (biomes, mt_chunksize,
+						       mt_chunksize)
+	core.save_gen_notify ("mcl_levelgen:biome_data", compressed)
 end)
