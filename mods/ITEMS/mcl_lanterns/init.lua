e2eb085854a5217b81e3e3b4fd7e4fc2773e15b6
cora <coradelamouche@gmx.ch>
2025-05-23 05:19:21 +0200

    Allow external registrations of lanterns

--- mods/ITEMS/mcl_lanterns/init.lua
+++ mods/ITEMS/mcl_lanterns/init.lua
@@ -102,7 +102,7 @@ function mcl_lanterns.register_lantern(name, def)
 	groups.attaches_to_top = 1
 	groups.attaches_to_base = 1
 
-	core.register_node(itemstring_floor, {
+	core.register_node(":"..itemstring_floor, {
 		description = def.description,
 		_doc_items_longdesc = def.longdesc,
 		drawtype = "mesh",
@@ -188,7 +188,7 @@ function mcl_lanterns.register_lantern(name, def)
 		_mcl_hardness = 3.5,
 	})
 
-	core.register_node(itemstring_ceiling, {
+	core.register_node(":"..itemstring_ceiling, {
 		description = def.description,
 		_doc_items_create_entry = false,
 		drawtype = "mesh",
