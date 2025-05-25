4b52c85779fff89fa1616495205e7786c38a8ac1
Po Lu <hbm78z8ub@mozmail.com>
2025-05-25 22:42:11 +0800

    Retroactively correct surface freezing after feature generation & bug fixes
    * mods/ITEMS/mcl_core/functions.lua (Grass spread ABM): Fix
    another crash.
    
    * mods/ITEMS/mcl_trees/lg_register.lua
    (register_tree_structure): New arguments AFTER_PLACE and
    DETAILS.
    (oak, fancy_oak, place_beehive, oak_with_beehive_005)
    (fancy_oak_with_beehive_005, trees_plains): New functions and
    features.
    
    * mods/MAPGEN/mcl_levelgen/default_features.lua
    (random_selector_place): Accept placed feature specs as well as
    named placed features.
    (freeze_layer_common): New function.  Extract surface freezing
    logic from place_freeze_top_layer.
    (place_freeze_top_layer): Retroactively correct top layer where
    the heightmap has been modified.
    
    * mods/MAPGEN/mcl_levelgen/features.lua
    (hash_heightmap_modification, unpack_heightmap_modification)
    (find_solid_surface): New functions.
    (correct_heightmaps): Amend heightmap modification list if
    appropriate.
    (copy_to_data): Correct typos and some confusion regarding
    coordinate systems.  Set vm_modified to true.
    (collect_unlit_region): New function.
    (process_features): Clear heightmap modification table and use a
    wrapper that converts region_walk's calling convention to
    insert's.
    
    * mods/MAPGEN/mcl_levelgen/nodeprops.lua: Add missing file.
    
    * mods/MAPGEN/mcl_levelgen/post_processing.lua
    (post_mapblock_run): Ensure the region being processed is loaded
    to avoid bizarre forms of truncation.
    (restore_heightmap_segment): Remove redundant call to
    `load_heightmap'.

--- mods/ITEMS/mcl_trees/lg_register.lua
+++ mods/ITEMS/mcl_trees/lg_register.lua
@@ -3,7 +3,7 @@ local tree_placement_flags = {
 	place_center_z = true,
 }
 
-local function register_tree_structure (name, schematic_set)
+local function register_tree_structure (name, schematic_set, after_place, details)
 	local schematics = {}
 	for i, schematic in ipairs (schematic_set) do
 		local name = "mcl_trees:" .. name .. "_" .. i
@@ -16,17 +16,22 @@ local function register_tree_structure (name, schematic_set)
 
 	if mcl_levelgen.is_levelgen_environment then
 		local n = #schematics
-		mcl_levelgen.register_feature ("mcl_trees:" .. name, {
-			place = function (_, x, y, z, cfg, rng)
-				local schematic = schematics[1 + rng:next_within (n)]
+		mcl_levelgen.register_feature ("mcl_trees:" .. name, table.merge ({
+			place = function (self, x, y, z, cfg, rng)
+				local i = 1 + rng:next_within (n)
+				local schematic = schematics[i]
 				mcl_levelgen.place_schematic (x, y, z, schematic, "random",
 							      true, tree_placement_flags,
 							      rng)
+				if after_place then
+					after_place (x, y, z, cfg, rng)
+				end
 			end,
-		})
-		mcl_levelgen.register_configured_feature ("mcl_trees:" .. name, {
+			tree_type = name,
+		}))
+		mcl_levelgen.register_configured_feature ("mcl_trees:" .. name, table.merge ({
 			feature = "mcl_trees:" .. name,
-		})
+		}, details))
 	end
 end
 
@@ -46,10 +51,88 @@ register_tree_structure ("spruce", {
 	modpath .. "/schematics/mcl_core_spruce_lollipop.mts",
 })
 
+register_tree_structure ("oak", {
+	modpath .. "/schematics/mcl_core_oak_v6.mts",
+	modpath .. "/schematics/mcl_core_oak_classic.mts",
+})
+
+register_tree_structure ("fancy_oak", {
+	modpath .. "/schematics/mcl_core_oak_balloon.mts",
+	modpath .. "/schematics/mcl_core_oak_large_1.mts",
+	modpath .. "/schematics/mcl_core_oak_large_2.mts",
+	modpath .. "/schematics/mcl_core_oak_large_3.mts",
+	modpath .. "/schematics/mcl_core_oak_large_4.mts",
+})
+
+local get_block = mcl_levelgen.get_block
+local set_block = mcl_levelgen.set_block
+local fix_lighting = mcl_levelgen.fix_lighting
+
+local cid_bee_nest = core.get_content_id ("mcl_beehives:bee_nest")
+
+local dirs = {
+	{1, 0,},
+	{0, 1,},
+	{-1, 0,},
+	{0, -1,},
+}
+
+local function place_beehive (x, y, z, cfg, rng)
+	local leaf_cid = cfg.leaf_cid
+	if rng:next_float () >= cfg.beehive_probability then
+		return
+	end
+	local dir = 1 + rng:next_within (4)
+	local xoff, zoff = dirs[dir][1], dirs[dir][2]
+	local x = x + xoff
+	local z = z + zoff
+	for i = 2, 8 do
+		local cid, param2 = get_block (x, y + i, z)
+		if cid == leaf_cid then
+			set_block (x, y + i - 1, z, cid_bee_nest, 0)
+			fix_lighting (x, y + i, z, x, y + i, z)
+			break
+		end
+	end
+end
+
+local get_content_id
+if mcl_levelgen.is_levelgen_environment then
+	get_content_id = core.get_content_id
+else
+	-- CIDs are unavailable when this mod is loaded, but they're
+	-- only referenced from the level generator environment.
+	get_content_id = function ()
+		return core.CONTENT_IGNORE
+	end
+end
+
+register_tree_structure ("oak_with_beehive_005", {
+	modpath .. "/schematics/mcl_core_oak_v6.mts",
+	modpath .. "/schematics/mcl_core_oak_classic.mts",
+}, place_beehive, {
+	leaf_cid = get_content_id ("mcl_trees:leaves_oak"),
+	beehive_probability = 0.05,
+})
+
+register_tree_structure ("fancy_oak_with_beehive_005", {
+	modpath .. "/schematics/mcl_core_oak_balloon.mts",
+	modpath .. "/schematics/mcl_core_oak_large_1.mts",
+	modpath .. "/schematics/mcl_core_oak_large_2.mts",
+	modpath .. "/schematics/mcl_core_oak_large_3.mts",
+	modpath .. "/schematics/mcl_core_oak_large_4.mts",
+}, place_beehive, {
+	leaf_cid = get_content_id ("mcl_trees:leaves_oak"),
+	beehive_probability = 0.05,
+})
+
 if mcl_levelgen.is_levelgen_environment then
 
 local cid_spruce_sapling
 	= core.get_content_id ("mcl_trees:sapling_spruce")
+local cid_oak_sapling
+	= core.get_content_id ("mcl_trees:sapling_oak")
+
 local is_position_hospitable
 	= mcl_levelgen.is_position_hospitable
 
@@ -78,6 +161,50 @@ mcl_levelgen.register_placed_feature ("mcl_trees:trees_snowy", {
 			end
 		end,
 	},
+})
+
+mcl_levelgen.register_configured_feature ("mcl_trees:trees_plains", {
+	feature = "mcl_levelgen:random_selector",
+	default = {
+		configured_feature = "mcl_trees:oak_with_beehive_005",
+		placement_modifiers = {},
+	},
+	features = {
+		{
+			feature = {
+				configured_feature = "mcl_trees:fancy_oak_with_beehive_005",
+				placement_modifiers = {},
+			},
+			chance = 1/3,
+		},
+	},
+})
+
+mcl_levelgen.register_placed_feature ("mcl_trees:trees_plains", {
+	configured_feature = "mcl_trees:trees_plains",
+	placement_modifiers = {
+		mcl_levelgen.build_count (W ({
+			{
+				weight = 19,
+				data = 0,
+			},
+			{
+				weight = 1,
+				data = 1,
+			},
+		})),
+		mcl_levelgen.build_in_square (),
+		mcl_levelgen.build_surface_water_depth_filter (0),
+		mcl_levelgen.build_heightmap ("motion_blocking"),
+		mcl_levelgen.build_in_biome (),
+		function (x, y, z, rng)
+			if is_position_hospitable (cid_oak_sapling, x, y, z) then
+				return { x, y, z, }
+			else
+				return nil
+			end
+		end,
+	}
 })
 
 end
