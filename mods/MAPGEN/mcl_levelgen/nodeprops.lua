301c42e5d404eda363b9eb0bd84b87de096367db
Po Lu <hbm78z8ub@mozmail.com>
2025-05-25 22:42:11 +0800

    Retroactively correct surface freezing after feature generation & bug fixes
    * mods/ITEMS/mcl_core/functions.lua (Grass spread ABM): Fix
    another crash.
    
    * mods/ITEMS/mcl_trees/lg_register.lua
    (register_tree_structure): New arguments AFTER_PLACE and
    DETAILS.
    (oak, fancy_oak, place_beehive, oak_with_beehive_005)
    (fancy_oak_with_beehive_005, trees_plains): New functions and
    features.
    
    * mods/MAPGEN/mcl_levelgen/default_features.lua
    (random_selector_place): Accept placed feature specs as well as
    named placed features.
    (freeze_layer_common): New function.  Extract surface freezing
    logic from place_freeze_top_layer.
    (place_freeze_top_layer): Retroactively correct top layer where
    the heightmap has been modified.
    
    * mods/MAPGEN/mcl_levelgen/features.lua
    (hash_heightmap_modification, unpack_heightmap_modification)
    (find_solid_surface): New functions.
    (correct_heightmaps): Amend heightmap modification list if
    appropriate.
    (copy_to_data): Correct typos and some confusion regarding
    coordinate systems.  Set vm_modified to true.
    (collect_unlit_region): New function.
    (process_features): Clear heightmap modification table and use a
    wrapper that converts region_walk's calling convention to
    insert's.
    
    * mods/MAPGEN/mcl_levelgen/nodeprops.lua: Add missing file.
    
    * mods/MAPGEN/mcl_levelgen/post_processing.lua
    (post_mapblock_run): Ensure the region being processed is loaded
    to avoid bizarre forms of truncation.
    (restore_heightmap_segment): Remove redundant call to
    `load_heightmap'.

---
+++ mods/MAPGEN/mcl_levelgen/nodeprops.lua
@@ -0,0 +1,346 @@
+local bor = bit.bor
+local lshift = bit.lshift
+
+local decompose_AABBs = mcl_util.decompose_AABBs
+
+--------------------------------------------------------------------------
+-- Node property testing.
+--------------------------------------------------------------------------
+
+local function nodename_matcher(node_or_groupname)
+	if string.sub(node_or_groupname, 1, 6) == "group:" then
+		local groups = string.split(node_or_groupname:sub(("group:"):len() + 1), ",")
+		return function(nodename)
+			for _, groupname in pairs(groups) do
+				if core.get_item_group(nodename, groupname) == 0 then
+					return false
+				end
+			end
+			return true
+		end
+	else
+		return function(nodename)
+			return nodename == node_or_groupname
+		end
+	end
+end
+-- Minetest allows shorthand box = {...} instead of {{...}}
+local function get_boxes (box_or_boxes)
+	return type (box_or_boxes[1]) == "number" and {box_or_boxes} or box_or_boxes
+end
+
+local has_boxes_prop = {collision_box = "walkable", selection_box = "pointable"}
+
+-- Required for raycast box IDs to be accurate
+local connect_sides_order = {
+	"top", "bottom", "front",
+	"left", "back", "right",
+}
+
+local connect_sides_directions = {
+	top = vector.new (0, 1, 0),
+	bottom = vector.new (0, -1, 0),
+	front = vector.new (0, 0, 1),
+	left = vector.new (-1, 0, 0),
+	back = vector.new (0, 0, -1),
+	right = vector.new (1, 0, 0),
+}
+
+local get_block
+
+local function get_node_compat (pos)
+	local cid, param2 = get_block (pos.x, pos.y, pos.z)
+	return {
+		name = cid and core.get_name_from_content_id (cid) or "ignore",
+		param2 = param2 or 0,
+	}
+end
+
+local function get_node_boxes (x, y, z)
+	local pos = vector.new (x, y, z)
+	local node = get_node_compat (pos)
+	local node_def = core.registered_nodes[node.name]
+	if not node_def or node_def[has_boxes_prop[type]] == false then
+		return {}, true
+	end
+	local boxes = {{-0.5, -0.5, -0.5, 0.5, 0.5, 0.5}}
+	local def_node_box = node_def.drawtype == "nodebox" and node_def.node_box
+	local def_box = node_def[type] or def_node_box -- will evaluate to def_node_box for type = nil
+	if not def_box then
+		return boxes, true -- default to regular box
+	end
+	local box_type = def_box.type
+	if box_type == "regular" then
+		return boxes, true
+	end
+	local fixed = def_box.fixed
+	boxes = get_boxes(fixed or {})
+	local paramtype2 = node_def.paramtype2
+	if box_type == "leveled" then
+		boxes = table.copy(boxes)
+		local level = (paramtype2 == "leveled" and node.param2 or node_def.leveled or 0) / 255 - 0.5
+		for _, box in ipairs(boxes) do
+			box[5] = level
+		end
+	elseif box_type == "wallmounted" then
+		local dir = core.wallmounted_to_dir((paramtype2 == "colorwallmounted" and node.param2 % 8 or node.param2) or 0)
+		local box
+		-- The (undocumented!) node box defaults below are taken from `NodeBox::reset`
+		if dir.y > 0 then
+			box = def_box.wall_top or {-0.5, 0.5 - 1/16, -0.5, 0.5, 0.5, 0.5}
+		elseif dir.y < 0 then
+			box = def_box.wall_bottom or {-0.5, -0.5, -0.5, 0.5, -0.5 + 1/16, 0.5}
+		else
+			box = def_box.wall_side or {-0.5, -0.5, -0.5, -0.5 + 1/16, 0.5, 0.5}
+			if dir.z > 0 then
+				box = {box[3], box[2], -box[4], box[6], box[5], -box[1]}
+			elseif dir.z < 0 then
+				box = {-box[6], box[2], box[1], -box[3], box[5], box[4]}
+			elseif dir.x > 0 then
+				box = {-box[4], box[2], box[3], -box[1], box[5], box[6]}
+			else
+				box = {box[1], box[2], -box[6], box[4], box[5], -box[3]}
+			end
+		end
+		return {assert(box, "incomplete wallmounted collisionbox definition of " .. node.name)}, true
+	end
+	if box_type == "connected" then
+		boxes = table.copy(boxes)
+		local connect_sides = connect_sides_directions -- (ab)use directions as a "set" of sides
+		if node_def.connect_sides then -- build set of sides from given list
+			connect_sides = {}
+			for _, side in ipairs(node_def.connect_sides) do
+				connect_sides[side] = true
+			end
+		end
+		local function add_collisionbox(key)
+			for _, box in ipairs(get_boxes(def_box[key] or {})) do
+				table.insert(boxes, box)
+			end
+		end
+		local matchers = {}
+		for i, nodename_or_group in ipairs(node_def.connects_to or {}) do
+			matchers[i] = nodename_matcher(nodename_or_group)
+		end
+		local function connects_to(nodename)
+			for _, matcher in ipairs(matchers) do
+				if matcher(nodename) then
+					return true
+				end
+			end
+		end
+		local connected, connected_sides
+		for _, side in ipairs(connect_sides_order) do
+			if connect_sides[side] then
+				local direction = connect_sides_directions[side]
+				local neighbor = get_node_compat (vector.add (pos, direction))
+				local connects = connects_to(neighbor.name)
+				connected = connected or connects
+				connected_sides = connected_sides or (side ~= "top" and side ~= "bottom")
+				add_collisionbox((connects and "connect_" or "disconnected_") .. side)
+			end
+		end
+		if not connected then
+			add_collisionbox("disconnected")
+		end
+		if not connected_sides then
+			add_collisionbox("disconnected_sides")
+		end
+		return boxes, false
+	end
+	if box_type == "fixed" and paramtype2 == "facedir" or paramtype2 == "colorfacedir" then
+		local param2 = paramtype2 == "colorfacedir" and node.param2 % 32 or node.param2 or 0
+		if param2 ~= 0 then
+			boxes = table.copy(boxes)
+			local axis = ({5, 6, 3, 4, 1, 2})[math.floor(param2 / 4) + 1]
+			local other_axis_1, other_axis_2 = (axis % 3) + 1, ((axis + 1) % 3) + 1
+			local rotation = (param2 % 4) / 2 * math.pi
+			local flip = axis > 3
+			if flip then axis = axis - 3; rotation = -rotation end
+			local sin, cos = math.sin(rotation), math.cos(rotation)
+			if axis == 2 then
+				sin = -sin
+			end
+			for _, box in ipairs(boxes) do
+				for off = 0, 3, 3 do
+					local axis_1, axis_2 = other_axis_1 + off, other_axis_2 + off
+					local value_1, value_2 = box[axis_1], box[axis_2]
+					box[axis_1] = value_1 * cos - value_2 * sin
+					box[axis_2] = value_1 * sin + value_2 * cos
+				end
+				if not flip then
+					box[axis], box[axis + 3] = -box[axis + 3], -box[axis]
+				end
+				local function fix(coord)
+					if box[coord] > box[coord + 3] then
+						box[coord], box[coord + 3] = box[coord + 3], box[coord]
+					end
+				end
+				fix(other_axis_1)
+				fix(other_axis_2)
+			end
+		end
+	end
+	return boxes, true
+end
+
+-- Table of node shapes that do not vary by surroundings.
+local static_node_shapes = {}
+
+-- Table of node shapes derived from cid and param2.
+local node_shape_cache = {}
+
+local EMPTY_SHAPE = decompose_AABBs ({})
+local FULL_BLOCK = decompose_AABBs ({{
+	-0.5, -0.5, -0.5,
+	0.5, 0.5, 0.5,
+}})
+
+local cid_ice
+local cid_packed_ice
+local cid_mud
+local cid_soul_sand
+local cid_honey_block
+local cid_air
+local is_cid_sapling = {}
+local is_cid_dirt = {}
+local is_cid_snow_layer, cid_snow = {}
+local mathmin = math.min
+
+local function initialize_nodeprops ()
+	cid_ice = core.get_content_id ("mcl_core:ice")
+	cid_packed_ice = core.get_content_id ("mcl_core:packed_ice")
+	cid_mud = core.get_content_id ("mcl_mud:mud")
+	cid_soul_sand = core.get_content_id ("mcl_nether:soul_sand")
+	cid_honey_block = core.get_content_id ("mcl_honey:honey_block")
+	cid_air = core.CONTENT_AIR
+
+	for i = 1, 8 do
+		local cid
+		if i == 1 then
+			cid = core.get_content_id ("mcl_core:snow")
+			cid_snow = cid
+		else
+			cid = core.get_content_id ("mcl_core:snow_" .. i)
+		end
+		is_cid_snow_layer[cid] = true
+	end
+
+	for name, def in pairs (core.registered_nodes) do
+		local cid = core.get_content_id (name)
+		if not def.walkable then
+			static_node_shapes[cid] = EMPTY_SHAPE
+		else
+			local boxes = def.collision_box or def.node_box
+
+			if not boxes or boxes.type == "regular" then
+				static_node_shapes[cid] = FULL_BLOCK
+			elseif boxes.type == "fixed" then
+				-- ALways read from the default box.
+				local fixed = boxes.fixed
+
+				if type (fixed[1]) == "number" then
+					fixed = {fixed}
+				end
+				local shape = decompose_AABBs (fixed)
+				if not shape then
+					error (string.format ("`%s''s collision box is too complex",
+							      name))
+				end
+				static_node_shapes[cid] = fixed
+			end
+		end
+
+		if def.groups.sapling and def.groups.sapling >= 1 then
+			is_cid_sapling[cid] = true
+		end
+		if def.groups.dirt and def.groups.dirt >= 1 then
+			is_cid_dirt[cid] = true
+		end
+	end
+end
+
+if core.register_on_mods_loaded then
+	core.register_on_mods_loaded (initialize_nodeprops)
+else
+	initialize_nodeprops ()
+end
+
+local function get_node_shape (x, y, z)
+	local cid, param2 = get_block (x, y, z)
+	if static_node_shapes[cid] then
+		return static_node_shapes[cid], true
+	else
+		local hash = bor (lshift (cid, 8), param2)
+		if node_shape_cache[hash] then
+			return node_shape_cache[hash], true
+		end
+
+		local value, reusable
+			= decompose_AABBs (get_node_boxes (x, y, z))
+		assert (value, cid .. "'s collision box is too complex")
+		if reusable then
+			node_shape_cache[hash] = value
+		end
+		return value, reusable
+	end
+end
+
+--------------------------------------------------------------------------
+-- Feature generation interface.
+--------------------------------------------------------------------------
+
+local supports_snow = {}
+
+function mcl_levelgen.is_position_hospitable (cid, x, y, z)
+	if is_cid_snow_layer[cid] then
+		-- Test whether the surface of the node below is not
+		-- any manner of ice and is sturdy, or mud, soul sand,
+		-- or a honey block.
+		local cid, param2 = get_block (x, y - 1, z)
+		if cid == cid_ice
+			or cid == cid_packed_ice
+			or cid == cid_air
+			or is_cid_snow_layer[cid] then
+			return false
+		elseif cid == cid_mud or cid == cid_soul_sand
+			or cid == cid_honey_block then
+			return true
+		end
+
+		local hash = bor (lshift (cid, 8), param2)
+		if supports_snow[hash] ~= nil then
+			return supports_snow[hash]
+		end
+		local shape, reusable = get_node_shape (x, y - 1, z)
+		local top_face = shape:select_face ("y", 0.5)
+		local sturdy = top_face:equal_p (FULL_BLOCK)
+		if reusable then
+			supports_snow[hash] = sturdy
+		end
+		return sturdy
+	elseif is_cid_sapling[cid] then
+		local cid, _ = get_block (x, y - 1, z)
+		return is_cid_dirt[cid]
+	end
+
+	return true
+end
+
+local is_position_hospitable = mcl_levelgen.is_position_hospitable
+
+function mcl_levelgen.can_place_snow (x, y, z)
+	local cid, param2 = get_block (x, y, z)
+	if cid == cid_air or is_cid_snow_layer[cid] then
+		return is_position_hospitable (cid_snow, x, y, z)
+	end
+	return false
+end
+
+--------------------------------------------------------------------------
+-- Late initialization.
+--------------------------------------------------------------------------
+
+function mcl_levelgen.initialize_nodeprops_in_async_env ()
+	get_block = mcl_levelgen.get_block
+end
