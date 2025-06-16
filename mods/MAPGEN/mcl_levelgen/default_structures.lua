21a63e0e00551e2d4c7b9dbfc14ca503852d6de8
Po Lu <hbm78z8ub@mozmail.com>
2025-06-17 00:00:09 +0800

    Implement Woodland Mansion
    * mods/MAPGEN/mcl_levelgen/default_structures.lua: Load
    woodland_mansion.lua.
    
    * mods/MAPGEN/mcl_levelgen/structures.lua
    (height_of_lowest_corner_including_center): New function.
    
    * mods/MAPGEN/mcl_levelgen/woodland_mansion.lua: New file.

--- mods/MAPGEN/mcl_levelgen/default_structures.lua
+++ mods/MAPGEN/mcl_levelgen/default_structures.lua
@@ -6,4 +6,4 @@ dofile (mcl_levelgen.prefix .. "/mineshaft.lua")
 -- dofile (mcl_levelgen.prefix .. "/beardifier_demo.lua")
 dofile (mcl_levelgen.prefix .. "/pillager_outpost.lua")
 dofile (mcl_levelgen.prefix .. "/stronghold.lua")
-
+dofile (mcl_levelgen.prefix .. "/woodland_mansion.lua")
