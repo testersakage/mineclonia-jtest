5329f7976bedef3f7143c59295feaa0e70451e72
Po Lu <hbm78z8ub@mozmail.com>
2025-08-10 11:09:42 +0800

    Model the flat map generator and correct mapgen spflag parsing
    * mods/MAPGEN/mcl_mapgen_models/carpathian.lua
    (carpathian_mapgen_model):
    
    * mods/MAPGEN/mcl_mapgen_models/v7.lua (v7_mapgen_model):
    
    * mods/MAPGEN/mcl_mapgen_models/valleys.lua (valleys_mapgen_model):
    
    * mods/MAPGEN/mcl_mapgen_models/common.lua (parse_flags): New
    function.  Parse mapgen flags in the settings object.
    (ersatz_model): Implement stub for get_biome_override.
    
    * mods/MAPGEN/mcl_mapgen_models/flat.lua: New file.
    
    * mods/MAPGEN/mcl_mapgen_models/init.lua (set_model): Delegate to and
    load flat.lua.

--- mods/MAPGEN/mcl_mapgen_models/init.lua
+++ mods/MAPGEN/mcl_mapgen_models/init.lua
@@ -2,11 +2,12 @@ mcl_mapgen_models = {}
 local model = nil
 
 local modpath = core.get_modpath (core.get_current_modname ())
+dofile (modpath .. "/common.lua")
 dofile (modpath .. "/v7.lua")
 dofile (modpath .. "/valleys.lua")
 dofile (modpath .. "/carpathian.lua")
 dofile (modpath .. "/v5.lua")
-dofile (modpath .. "/common.lua")
+dofile (modpath .. "/flat.lua")
 
 ------------------------------------------------------------------------
 -- Lua models of the built-in map generators.
@@ -23,6 +24,8 @@ local function set_model ()
 		model = mcl_mapgen_models.carpathian_mapgen_model ()
 	elseif name == "v5" then
 		model = mcl_mapgen_models.v5_mapgen_model ()
+	elseif name == "flat" then
+		model = mcl_mapgen_models.flat_mapgen_model ()
 	else
 		model = mcl_mapgen_models.ersatz_model ()
 	end
@@ -33,24 +36,21 @@ if core and core.get_mod_storage then
 name = core.get_mapgen_setting ("mg_name")
 core.ipc_set ("mcl_mapgen_models:mg_name", name)
 
-core.register_on_mods_loaded (function ()
-	core.after (0, function ()
-		set_model ()
-	end)
-end)
-
 mcl_info.register_debug_field ("Estimated Generation Height", {
 	level = 4,
 	func = function (_, pos)
 		if not model then
-			return "N/A"
-		else
+			set_model ()
+		end
+		if model then
 			local x = math.floor (pos.x + 0.5)
 			local z = math.floor (pos.z + 0.5)
 			local fn = model.get_column_height
 			local biome = model.get_biome_override (x, z) or "N/A"
 			return string.format ("y=%d/%d (%s)", fn (x, z, false), fn (x, z, true),
 					      biome)
+		else
+			return "N/A"
 		end
 	end,
 })
