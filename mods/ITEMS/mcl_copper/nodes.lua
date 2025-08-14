1aabbdd3e71e3e7c031942b9a84be708d36fe0bf
hezemisverydumb <hezemisverydumb@noreply.codeberg.org>
2025-08-14 20:44:02 +0500

    Add Copper Chains

--- mods/ITEMS/mcl_copper/nodes.lua
+++ mods/ITEMS/mcl_copper/nodes.lua
@@ -230,7 +230,68 @@ for n, desc in pairs(n_desc) do
 		texture_inv = "mcl_copper_lantern"..n.."_inv.png",
 		light_level = core.LIGHT_MAX,
 	})
+	core.register_node("mcl_copper:chain"..n, {
+		description = D(desc .. "Copper Chain"),
+		_doc_items_longdesc = D(desc .. "Copper Chains are metallic decoration blocks."),
+		inventory_image = "mcl_copper_chain"..n.."_inv.png",
+		tiles = {"mcl_copper_chain"..n..".png"},
+		drawtype = "mesh",
+		paramtype = "light",
+		paramtype2 = "facedir",
+		use_texture_alpha = "clip",
+		mesh = "mcl_lanterns_chain.obj",
+		is_ground_content = false,
+		sunlight_propagates = true,
+		collision_box = {
+			type = "fixed",
+			fixed = {
+				{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625},
+			}
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.0625, -0.5, -0.0625, 0.0625, 0.5, 0.0625},
+			}
+		},
+		groups = {pickaxey = 1, deco_block = 1},
+		sounds = mcl_sounds.node_sound_metal_defaults(),
+		on_place = function(itemstack, placer, pointed_thing)
+			if pointed_thing.type ~= "node" or not placer or not placer:is_player() then
+				return itemstack
+			end
 
+			local p0 = pointed_thing.under
+			local p1 = pointed_thing.above
+			local param2 = 0
+
+			local placer_pos = placer:get_pos()
+			if placer_pos then
+				local dir = {
+					x = p1.x - placer_pos.x,
+					y = p1.y - placer_pos.y,
+					z = p1.z - placer_pos.z
+				}
+				param2 = core.dir_to_facedir(dir)
+			end
+
+			if p0.y - 1 == p1.y then
+				param2 = 20
+			elseif p0.x - 1 == p1.x then
+				param2 = 16
+			elseif p0.x + 1 == p1.x then
+				param2 = 12
+			elseif p0.z - 1 == p1.z then
+				param2 = 8
+			elseif p0.z + 1 == p1.z then
+				param2 = 4
+			end
+
+			return core.item_place_node(itemstack, placer, pointed_thing, param2)
+		end,
+		_mcl_blast_resistance = 6,
+		_mcl_hardness = 5,
+	})
 	mcl_wip.register_wip_item("mcl_copper:door"..n)
 end
 
