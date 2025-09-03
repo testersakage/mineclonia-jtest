e4fadef8e01710eb0f136d9131419c6a3be94ffd
MysticTempest <molecularmarc.he@gmail.com>
2025-09-03 20:43:13 +0200

    Fix being landlocked on ice
    by tweaking the vertical boat movement check.

--- mods/ENTITIES/mcl_boats/init.lua
+++ mods/ENTITIES/mcl_boats/init.lua
@@ -451,7 +451,7 @@ function boat:on_step(dtime, moveresult)
 		else
 			-- On top of water
 			new_acce = {x = 0, y = 0, z = 0}
-			if math.abs(self.object:get_velocity().y) < 1 then
+			if self.object:get_velocity().y < 1 and not on_ice then
 				local pos = self.object:get_pos()
 				pos.y = math.floor(pos.y) + boat_y_offset
 				new_velo = get_velocity(self._v, self.object:get_yaw(), 0)
