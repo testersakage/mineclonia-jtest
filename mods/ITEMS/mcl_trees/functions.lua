ec5c2ef464c08576ef83f7423353506223e2618b
Elias Åström <ryvnf@riseup.net>
2025-10-12 17:45:04 +0200

    Evaluate pos p2 for every leaves node

--- mods/ITEMS/mcl_trees/functions.lua
+++ mods/ITEMS/mcl_trees/functions.lua
@@ -182,8 +182,8 @@ local function place_tree(place_at, schem)
 		{ place_center_x = true, place_center_y = false, place_center_z = true }
 	)
 
-	local biome_p2 = mcl_util.get_pos_p2(place_at)
 	for _, pos in pairs(core.find_nodes_in_area(vector.new(p1.x, p1.y,p1.z), vector.new(p2.x, p2.y + size.y, p2.z), {"group:leaves"})) do
+		local biome_p2 = mcl_util.get_pos_p2(pos)
 		local node = core.get_node(pos)
 		node.param2 = math.floor(node.param2 / 32) * 32 + biome_p2
 		core.set_node(pos, node)
