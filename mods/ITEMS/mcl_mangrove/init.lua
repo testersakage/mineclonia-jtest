d4e101f4044538200ef2fcef240cb2b1c5139457
josedouglas26 <josedouglas20002014@gmail.com>
2025-11-22 06:50:56 -0300

    Fixing not diggable propagule

--- mods/ITEMS/mcl_mangrove/init.lua
+++ mods/ITEMS/mcl_mangrove/init.lua
@@ -251,7 +251,7 @@ for i = 1, 5 do
 		tiles = get_hanging_propagules_textures(i),
 		groups = {
 			plant = 1, not_in_creative_inventory = 1, non_mycelium_plant = 1,
-			dig_immediate = 1, dig_by_water = 1, dig_by_piston = 1, destroy_by_lava_flow = 1,
+			dig_immediate = 3, dig_by_water = 1, dig_by_piston = 1, destroy_by_lava_flow = 1,
 			attached_node = 4, hanging_propagule = 1,
 		},
 		_on_bone_meal = i < 5 and function(_, _, _, pos, node)
