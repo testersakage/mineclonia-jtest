eeac430b889cb52b0172070c9723c06091964952
xox <abc@example.com>
2025-12-05 18:26:21 +0100

    Readd the nether portal ABM
    It also did some other stuff, so im adding it again, but slimmed down

--- mods/ITEMS/mcl_portals/portal_nether.lua
+++ mods/ITEMS/mcl_portals/portal_nether.lua
@@ -820,6 +820,17 @@ core.register_chatcommand("dumpportals", {
 	end,
 })
 
+core.register_abm({
+	label = "Nether portal teleportation and particles",
+	nodenames = { "mcl_portals:portal" },
+	interval = 1,
+	chance = 1,
+	action = function(pos, node)
+		emit_portal_particles(pos, node)
+		spawn_zombified_piglin(pos)
+	end,
+})
+
 mcl_structures.register_structure("nether_portal",{
 	nospawn = true,
 	filenames = {
