fba45e4050acf0aac1f5856566f745135307815c
joshua-stone <joshua.gage.stone@gmail.com>
2025-12-08 18:16:47 -0500

    Rename config.load_json_file to mcl_util.load_json_file

--- mods/CORE/mcl_util/config.lua
+++ mods/CORE/mcl_util/config.lua
@@ -1,6 +1,4 @@
-config = {}
-
-function config.load_json_file(modpath, name)
+function mcl_util.load_json_file(modpath, name)
 	local path = modpath .. "/" .. name .. ".json"
 	local file = assert(io.open(path, "r"))
 	local data = core.parse_json(file:read("*all"))
