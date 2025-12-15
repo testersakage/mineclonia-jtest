9373f0a6df53b2ddb2ba49a899527e843aa297c8
xox <abc@example.com>
2025-12-15 22:58:31 +0100

    Reimplement `mcl_paintings` (WIP)

--- mods/ENTITIES/mcl_paintings/init.lua
+++ mods/ENTITIES/mcl_paintings/init.lua
@@ -7,79 +7,151 @@ local S = core.get_translator(modname)
 
 local wood = "[combine:16x16:-192,0=mcl_paintings_paintings.png"
 
--- Check if there's a painting for provided painting size.
--- If yes, returns the arguments.
--- If not, returns the next smaller available painting.
-local function shrink_painting(x, y)
-	if x > 4 or y > 4 then
-		return nil
-	end
-	local xstart = x
-	local painting
-	while not painting do
-		painting = mcl_paintings.paintings[y] and mcl_paintings.paintings[y][x]
-		if type(painting) == "table" then
-			break
-		elseif type(painting) == "number" then
-			x = painting
-			painting = nil
-		else
-			x = xstart
-			y = y - 1
-		end
-		if y < 1 then
-			return nil
-		end
-	end
-	if type(painting) == "table" then
-		return x, y
-	end
+-- a painting definition has these fields
+-- width         - integer
+-- height        - integer
+-- texture       - the texture
+-- legacy_motive - the old (and extremely stupid) painting identifier, used for backwards compability
+local registered_paintings = {}
+local maximum_width = 0
+local maximum_height = 0
+
+function mcl_paintings.register_painting(name, def)
+	def.name = name
+	registered_paintings[name] = def
+	maximum_width = math.max(maximum_width, def.width)
+	maximum_height = math.max(maximum_height, def.height)
 end
 
-local function get_painting(x, y, motive)
-	local painting = mcl_paintings.paintings[y] and mcl_paintings.paintings[y][x] and mcl_paintings.paintings[y][x][motive]
-	if not painting then
-		return nil
-	end
-	local px, py = -painting.cx, -painting.cy
-	local sx, sy = 16*x, 16*y
-	return "[combine:"..sx.."x"..sy..":"..px..","..py.."=mcl_paintings_paintings.png"
+local function is_node_okay_for_placement(under_node, above_node)
+	local under_ndef = core.registered_nodes[under_node.name]
+	return above_node.name == "air" and (under_ndef and under_ndef.walkable)
 end
 
-local function get_random_painting(x, y)
-	if not mcl_paintings.paintings[y] or not mcl_paintings.paintings[y][x] then
-		return nil
+local function get_biggest_painting_for_position(pos, dir)
+	local negative_dir = -dir
+	local dir_perpendicular = dir.x ~= 0
+		and vector.new(0, 0, dir.x)
+		or  vector.new(dir.z, 0, 0)
+
+	-- A table where each element represents the Y level (starting from the bottom).
+	-- Where the value is the maximum extend of the width for a painting that wide and that high
+	local placement_ranges = {}
+	local maximum_so_far = maximum_width
+
+	for y = 0, maximum_height do
+		local i = 0
+		while i < maximum_so_far do
+			local offset_pos = vector.offset(pos, -i * dir_perpendicular.x, y, -i * dir_perpendicular.z)
+			local node_under = core.get_node(offset_pos)
+			local node_above = core.get_node(offset_pos + negative_dir)
+
+			core.debug(y, i, offset_pos, node_under.name, node_above.name)
+			if is_node_okay_for_placement(node_under, node_above) then
+				i = i + 1
+			else
+				break
+			end
+		end
+
+		maximum_so_far = math.min(i, maximum_so_far)
+		table.insert(placement_ranges, maximum_so_far)
 	end
-	local max = #mcl_paintings.paintings[y][x]
-	if max < 1 then
-		return nil
+
+	local maximum_volume = -1
+	local canditates = {}
+
+	for _, pdef in pairs(registered_paintings) do
+		if pdef.width <= placement_ranges[pdef.height] then
+			local painting_volume = pdef.width * pdef.height
+			if maximum_volume < painting_volume then
+					-- or (maximum_volume == painting_volume and math.random() > 0.5) then
+				canditates = {pdef}
+				maximum_volume = painting_volume
+				-- maximum_volume_name = name
+			elseif maximum_volume == painting_volume then
+				table.insert(canditates, pdef)
+			end
+		end
 	end
-	local r = math.random(1, max)
-	return get_painting(x, y, r), r
+
+	if #canditates == 0 then
+		-- This should never happen, since we have 1x1 paintings. But it doesn't hurt to have extra protection
+		error("No possible painting canditate found")
+	end
+
+	local random_pick = canditates[math.random(1, #canditates)]
+
+	return random_pick
 end
+
+core.register_craftitem("mcl_paintings:painting", {
+	description = S("Painting"),
+	inventory_image = "mcl_paintings_painting.png",
+	groups = {deco_block = 1},
+	on_place = function(itemstack, placer, pointed_thing)
+		if pointed_thing.type ~= "node" then
+			return itemstack
+		end
+
+		local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
+		if rc then return rc end
+
+		local dir = vector.subtract(pointed_thing.above, pointed_thing.under)
+
+		if dir.y ~= 0 then
+			return itemstack
+		end
+
+		local pdef = get_biggest_painting_for_position(pointed_thing.under, dir)
+		local wallm = core.dir_to_wallmounted(dir)
+
+		if not wallm then return itemstack end
+
+		local staticdata = {
+			_facing = wallm,
+			_painting_name = pdef.name
+		}
+
+		local obj = core.add_entity(
+			vector.subtract(pointed_thing.above, vector.multiply(dir, 0.5-5/256)) + vector.new(-dir.z * (pdef.width / 2 - 0.5), pdef.height / 2 - 0.5, -dir.x * (pdef.width / 2 - 0.5)),
+			"mcl_paintings:painting",
+			core.serialize(staticdata)
+		)
+
+		if not obj then return itemstack end
+
+		if not core.is_creative_enabled(placer:get_player_name()) then
+			itemstack:take_item()
+		end
+		return itemstack
+	end,
+})
 
 local function size_to_minmax_entity(size)
 	return -size/2, size/2
 end
 
-local function set_entity(object)
+local function set_entity(object, pdef)
+
+	if not pdef then
+		-- error("Invalid painting")
+		core.log("error", "[mcl_paintings] Painting loaded with missing painting values!")
+	end
+
+	core.debug("aa", dump(pdef))
+
 	local ent = object:get_luaentity()
 	local wallm = ent._facing
-	local xsize = ent._xsize
-	local ysize = ent._ysize
-	local exmin, exmax = size_to_minmax_entity(xsize)
-	local eymin, eymax = size_to_minmax_entity(ysize)
-	local visual_size = { x=xsize-0.0001, y=ysize-0.0001, z=1/32 }
+	local exmin, exmax = size_to_minmax_entity(pdef.width)
+	local eymin, eymax = size_to_minmax_entity(pdef.height)
+	local visual_size = { x=pdef.width-0.0001, y=pdef.height-0.0001, z=1/32 }
+
 	if not ent._xsize or not ent._ysize or not ent._motive then
 		core.log("error", "[mcl_paintings] Painting loaded with missing painting values!")
 		return
 	end
-	local painting = get_painting(xsize, ysize, ent._motive)
-	if not painting then
-		core.log("error", "[mcl_paintings] No painting found for size "
-				..xsize..","..ysize..", motive number "..ent._motive.."!")
-		return
-	end
+
 	local box
 	if wallm == 2 then
 		box = { -3/128, eymin, exmin, 1/64, eymax, exmax }
@@ -93,7 +165,7 @@ local function set_entity(object)
 	object:set_properties({
 		selectionbox = box,
 		visual_size = visual_size,
-		textures = { wood, wood, wood, wood, painting, wood },
+		textures = { wood, wood, wood, wood, pdef.texture, wood },
 	})
 
 	local dir = core.wallmounted_to_dir(wallm)
@@ -126,26 +198,48 @@ core.register_entity("mcl_paintings:painting", {
 			local data = core.deserialize(staticdata)
 			if data then
 				self._facing = data._facing
-				self._pos = data._pos
-				self._motive = data._motive
-				self._xsize = data._xsize
-				self._ysize = data._ysize
+				self._painting_name = data._painting_name
+				-- _xsize = xsize,
+				-- _ysize = ysize,
+
+				-- Putting the old mcl_painting crap to grave
+				if data._motive then
+					local successfully_converted = false
+					for pname, pdef in pairs(registered_paintings) do
+						core.debug("loopy", pname, data._motive, data._ysize, data._xsize)
+						if pdef.legacy_motive
+								-- and pdef.legacy_motive.cx == self._motive.cx
+								-- and pdef.legacy_motive.cy == self._motive.cy then
+								and pdef.height == data._ysize
+								and pdef.width == data._xsize
+								and pdef.legacy_motive == data._motive then
+
+							core.debug("here", dump(pdef))
+							successfully_converted = true
+							self._painting_name = pname
+							break
+							-- yummy nesting
+						end
+					end
+
+					if not successfully_converted then
+						self.object:remove()
+						core.log("error", "Could not migrate painting to the new system")
+					end
+				end
 			end
 		end
-		set_entity(self.object)
+		core.debug("moo", dump(self._painting_name))
+		set_entity(self.object, registered_paintings[self._painting_name])
 	end,
 	get_staticdata = function(self)
 		local data = {
 			_facing = self._facing,
-			_pos = self._pos,
-			_motive = self._motive,
-			_xsize = self._xsize,
-			_ysize = self._ysize,
+			_painting_name = self._painting_name
 		}
 		return core.serialize(data)
 	end,
 	on_punch = function(self, puncher, time_from_last_punch, tool_capabilities, dir, damage) ---@diagnostic disable-line: unused-local
-		-- Drop as item on punch
 		if puncher and puncher:is_player() then
 			local kname = puncher:get_player_name()
 			local pos = self._pos
@@ -162,120 +256,187 @@ core.register_entity("mcl_paintings:painting", {
 	end,
 })
 
-core.register_craftitem("mcl_paintings:painting", {
-	description = S("Painting"),
-	inventory_image = "mcl_paintings_painting.png",
-	groups = {deco_block = 1},
-	on_place = function(itemstack, placer, pointed_thing)
-		if pointed_thing.type ~= "node" then
-			return itemstack
-		end
-
-		local rc = mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
-		if rc then return rc end
-
-		local dir = vector.subtract(pointed_thing.above, pointed_thing.under)
-		dir = vector.normalize(dir)
-		if dir.y ~= 0 then
-			-- Ceiling/floor paintings are not supported
-			return itemstack
-		end
-		local wallm = core.dir_to_wallmounted(dir)
-		if wallm then
-			local ppos = pointed_thing.above
-			local xmax
-			local ymax = 4
-			local xmaxes = {}
-			local ymaxed = false
-			local negative = dir.x < 0 or dir.z > 0
-			-- Check maximum possible painting size
-			local t
-			for y=0,3 do
-			for x=0,3 do
-				local k = x
-				if negative then
-					k = -k
-				end
-				if dir.z ~= 0 then
-					t = {x=k,y=y,z=0}
-				else
-					t = {x=0,y=y,z=k}
-				end
-				local unode = core.get_node(vector.add(pointed_thing.under, t))
-				local anode = core.get_node(vector.add(ppos, t))
-				local udef = core.registered_nodes[unode.name]
-				local adef = core.registered_nodes[anode.name]
-				if (not (udef and udef.walkable)) or (not adef or adef.walkable) then
-					xmaxes[y+1] = x
-					if x == 0 and not ymaxed then
-						ymax = y
-						ymaxed = true
-					end
-					break
-				end
-			end
-			if not xmaxes[y] then
-				xmaxes[y] = 4
-			end
-			end
-			xmax = math.max(unpack(xmaxes))
-
-			local xsize, ysize = xmax, ymax
-			xsize, ysize = shrink_painting(xsize, ysize) ---@diagnostic disable-line: cast-local-type
-			if not xsize then
-				return itemstack
-			end
-
-			local _, exmax = size_to_minmax_entity(xsize)
-			local _, eymax = size_to_minmax_entity(ysize)
-			local pposa = vector.subtract(ppos, vector.multiply(dir, 0.5-5/256))
-			local pexmax
-			local peymax = eymax - 0.5
-			local n
-			if negative then
-				pexmax = -exmax + 0.5
-				n = -1
-			else
-				pexmax = exmax - 0.5
-				n = 1
-			end
-			if mcl_util.check_position_protection(ppos, placer) then return itemstack end
-			local ppos2
-			if dir.z ~= 0 then
-				pposa = vector.add(pposa, {x=pexmax, y=peymax, z=0})
-				ppos2 = vector.add(ppos, {x = (xsize-1)*n, y = ysize-1, z = 0})
-			else
-				pposa = vector.add(pposa, {x=0, y=peymax, z=pexmax})
-				ppos2 = vector.add(ppos, {x = 0, y = ysize-1, z = (xsize-1)*n})
-			end
-			if mcl_util.check_position_protection(ppos2, placer) then return itemstack end
-			local painting, pid = get_random_painting(xsize, ysize)
-			if not painting then
-				core.log("error", "[mcl_paintings] No painting found for size "..xsize..","..ysize.."!")
-				return itemstack
-			end
-			local staticdata = {
-				_facing = wallm,
-				_pos = ppos,
-				_motive = pid,
-				_xsize = xsize,
-				_ysize = ysize,
-			}
-			local obj = core.add_entity(pposa, "mcl_paintings:painting", core.serialize(staticdata))
-			if not obj then
-				return itemstack
-			end
-		else
-			return itemstack
-		end
-		if not core.is_creative_enabled(placer:get_player_name()) then
-			itemstack:take_item()
-		end
-		return itemstack
-	end,
+mcl_paintings.register_painting("ancient octupus", {
+	texture = "ancient_octupus.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 0,
 })
 
-mcl_wip.register_wip_item("mcl_paintings:painting")
+mcl_paintings.register_painting("snowy mountain", {
+	texture = "snowy_mountain.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("balding man", {
+	texture = "balding_man.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("poster", {
+	texture = "poster.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("notes", {
+	texture = "notes.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("viking shield", {
+	texture = "viking_shield.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 5,
+})
+
+mcl_paintings.register_painting("butcher knifes", {
+	texture = "butcher_knifes.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 6,
+})
+
+mcl_paintings.register_painting("green bottles", {
+	texture = "green_bottles.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("battle axe", {
+	texture = "battle_axe.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("cooking utencils", {
+	texture = "cooking_utencils.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("dense jungle forest", {
+	texture = "dense_jungle_forest.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("endless dunes", {
+	texture = "endless_dunes.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("green banner", {
+	texture = "endless_dunes.png",
+	width = 1,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("blue banner", {
+	texture = "endless_dunes.png",
+	width = 1,
+	height = 2,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("quest board", {
+	texture = "quest_board.png",
+	width = 4,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("support truss", {
+	texture = "support_truss.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("froggy pond", {
+	texture = "froggy_pond.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("moonshine thundra", {
+	texture = "moonshine_thundra.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("desert castle", {
+	texture = "desert_castle.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("sarmatian decoration", {
+	texture = "sarmatian_decoration.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("decorative swords", {
+	texture = "decorative_swords.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 5,
+})
+
+mcl_paintings.register_painting("gloom gloom mountain", {
+	texture = "gloom_mountain.png",
+	width = 4,
+	height = 3,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("elf utopia", {
+	texture = "elf_utopia.png",
+	width = 4,
+	height = 3,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("waterfall bridge", {
+	texture = "waterfall_bridge.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("mountain tower", {
+	texture = "mountain_tower.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("child's first drawing", {
+	texture = "childs_first_drawing.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 2,
+})
 
 core.register_craft({
 	output = "mcl_paintings:painting",
