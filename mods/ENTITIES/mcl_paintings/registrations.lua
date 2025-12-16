e53b90a4c4b6b31defa87271796063b0d9b2b5ad
xox <abc@example.com>
2025-12-16 17:42:05 +0100

    Move registration to own file

---
+++ mods/ENTITIES/mcl_paintings/registrations.lua
@@ -0,0 +1,181 @@
+mcl_paintings.register_painting("ancient octupus", {
+	texture = "ancient_octupus.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("snowy mountain", {
+	texture = "snowy_mountain.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("balding man", {
+	texture = "balding_man.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("poster", {
+	texture = "poster.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("notes", {
+	texture = "notes.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("viking shield", {
+	texture = "viking_shield.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 5,
+})
+
+mcl_paintings.register_painting("butcher knifes", {
+	texture = "butcher_knifes.png",
+	width = 1,
+	height = 1,
+	legacy_motive = 6,
+})
+
+mcl_paintings.register_painting("green bottles", {
+	texture = "green_bottles.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("battle axe", {
+	texture = "battle_axe.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("cooking utencils", {
+	texture = "cooking_utencils.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("dense jungle forest", {
+	texture = "dense_jungle_forest.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("endless dunes", {
+	texture = "endless_dunes.png",
+	width = 2,
+	height = 1,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("green banner", {
+	texture = "green_banner.png",
+	width = 1,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("blue banner", {
+	texture = "blue_banner.png",
+	width = 1,
+	height = 2,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("quest board", {
+	texture = "quest_board.png",
+	width = 4,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("support truss", {
+	texture = "support_truss.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("froggy pond", {
+	texture = "froggy_pond.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("moonshine thundra", {
+	texture = "moonshine_thundra.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 2,
+})
+
+mcl_paintings.register_painting("desert castle", {
+	texture = "desert_castle.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 3,
+})
+
+mcl_paintings.register_painting("sarmatian decoration", {
+	texture = "sarmatian_decoration.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 4,
+})
+
+mcl_paintings.register_painting("decorative swords", {
+	texture = "decorative_swords.png",
+	width = 2,
+	height = 2,
+	legacy_motive = 5,
+})
+
+mcl_paintings.register_painting("gloom gloom mountain", {
+	texture = "gloom_mountain.png",
+	width = 4,
+	height = 3,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("elf utopia", {
+	texture = "elf_utopia.png",
+	width = 4,
+	height = 3,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("waterfall bridge", {
+	texture = "waterfall_bridge.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 0,
+})
+
+mcl_paintings.register_painting("mountain tower", {
+	texture = "mountain_tower.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 1,
+})
+
+mcl_paintings.register_painting("child's first drawing", {
+	texture = "childs_first_drawing.png",
+	width = 4,
+	height = 4,
+	legacy_motive = 2,
+})
