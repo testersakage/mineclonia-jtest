6b24745dbadba8e92e0a2877d79dd875afef033d
epCode <epcode@noreply.codeberg.org>
2025-12-26 05:01:04 +0100

    Make reach to mobs correct as well.

--- mods/ENTITIES/mcl_mobs/combat.lua
+++ mods/ENTITIES/mcl_mobs/combat.lua
@@ -172,6 +172,29 @@ function mob_class:on_punch(hitter, tflp, tool_capabilities, dir)
 	local is_player = hitter and hitter:is_player()
 	local hitter_playername = is_player and hitter:get_player_name()
 	if hitter_playername and hitter_playername ~= "" then
+
+		local weapon = hitter:get_wielded_item()
+		local reach = (weapon:get_definition().range or 3)
+		local prop, p = hitter:get_properties(), hitter:get_pos()
+		local eye_p = vector.add(p, vector.new(0, prop.eye_height, 0))
+		eye_extended_pos = vector.add(eye_p, vector.multiply(hitter:get_look_dir(), reach))
+		local raycast = core.raycast(
+			eye_p,
+			eye_extended_pos,
+			true, true
+		)
+
+		local reaches_mob = false
+		for thing in raycast do
+			if thing.type == "object" and vector.distance(eye_p, thing.intersection_point) <= reach and not thing.ref:is_player() then
+				reaches_mob = true
+				break
+			end
+		end
+		
+		if not reaches_mob then return end
+		
+		
 		doc.mark_entry_as_revealed(hitter_playername, "mobs", self.name)
 		mcl_potions.update_haste_and_fatigue(hitter)
 	end
