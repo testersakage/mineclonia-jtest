4f8ccfc9c401c7953bdd6ea302b7f81ab7d99b54
xox <abc@example.com>
2026-01-05 12:00:38 +0100

    Make the redstone trim more pronounced

--- mods/ITEMS/REDSTONE/mcl_redstone/wire.lua
+++ mods/ITEMS/REDSTONE/mcl_redstone/wire.lua
@@ -270,7 +270,7 @@ do
 				update_wire_connections(pos)
 			end,
 			on_rightclick = on_rightclick,
-			_mcl_armor_trim_color = wire == 0 and "#af2c23" or nil,
+			_mcl_armor_trim_color = wire == 0 and "#e80202" or nil,
 			_mcl_armor_trim_desc = wire == 0 and S("Redstone Material") or nil
 		})
 		wireflag_tab[name] = wire
