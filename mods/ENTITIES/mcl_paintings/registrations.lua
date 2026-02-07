a6491cf414d1f5fab5b66a96bb5f0a38e8e2b4c9
Elias Åström <ryvnf@riseup.net>
2026-02-07 16:18:08 +0100

    Rename paintings

--- mods/ENTITIES/mcl_paintings/registrations.lua
+++ mods/ENTITIES/mcl_paintings/registrations.lua
@@ -1,181 +1,208 @@
-mcl_paintings.register_painting("ancient octupus", {
-	texture = "mcl_paintings_painting_ancient_octupus.png",
+mcl_paintings.register_painting("mcl_paintings:ancient_octopus", {
+	texture = "mcl_paintings_painting_ancient_octopus.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("snowy mountain", {
+mcl_paintings.register_painting("mcl_paintings:snowy_mountain", {
 	texture = "mcl_paintings_painting_snowy_mountain.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("balding man", {
+mcl_paintings.register_painting("mcl_paintings:balding_man", {
 	texture = "mcl_paintings_painting_balding_man.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 3,
 })
 
-mcl_paintings.register_painting("poster", {
+mcl_paintings.register_painting("mcl_paintings:poster", {
 	texture = "mcl_paintings_painting_poster.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 4,
 })
 
-mcl_paintings.register_painting("notes", {
+mcl_paintings.register_painting("mcl_paintings:notes", {
 	texture = "mcl_paintings_painting_notes.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 5,
 })
 
-mcl_paintings.register_painting("viking shield", {
+mcl_paintings.register_painting("mcl_paintings:viking_shield", {
 	texture = "mcl_paintings_painting_viking_shield.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 6,
 })
 
-mcl_paintings.register_painting("butcher knifes", {
+mcl_paintings.register_painting("mcl_paintings:butcher_knifes", {
 	texture = "mcl_paintings_painting_butcher_knifes.png",
 	width = 1,
 	height = 1,
 	legacy_motive = 7,
 })
 
-mcl_paintings.register_painting("green bottles", {
+mcl_paintings.register_painting("mcl_paintings:green_bottles", {
 	texture = "mcl_paintings_painting_green_bottles.png",
 	width = 2,
 	height = 1,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("battle axe", {
+mcl_paintings.register_painting("mcl_paintings:battle_axe", {
 	texture = "mcl_paintings_painting_battle_axe.png",
 	width = 2,
 	height = 1,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("cooking utencils", {
+mcl_paintings.register_painting("mcl_paintings:cooking_utencils", {
 	texture = "mcl_paintings_painting_cooking_utencils.png",
 	width = 2,
 	height = 1,
 	legacy_motive = 3,
 })
 
-mcl_paintings.register_painting("dense jungle forest", {
+mcl_paintings.register_painting("mcl_paintings:dense_jungle_forest", {
 	texture = "mcl_paintings_painting_dense_jungle_forest.png",
 	width = 2,
 	height = 1,
 	legacy_motive = 4,
 })
 
-mcl_paintings.register_painting("endless dunes", {
+mcl_paintings.register_painting("mcl_paintings:endless_dunes", {
 	texture = "mcl_paintings_painting_endless_dunes.png",
 	width = 2,
 	height = 1,
 	legacy_motive = 5,
 })
 
-mcl_paintings.register_painting("green banner", {
+mcl_paintings.register_painting("mcl_paintings:green_banner", {
 	texture = "mcl_paintings_painting_green_banner.png",
 	width = 1,
 	height = 2,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("blue banner", {
+mcl_paintings.register_painting("mcl_paintings:blue_banner", {
 	texture = "mcl_paintings_painting_blue_banner.png",
 	width = 1,
 	height = 2,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("quest board", {
+mcl_paintings.register_painting("mcl_paintings:quest_board", {
 	texture = "mcl_paintings_painting_quest_board.png",
 	width = 4,
 	height = 2,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("support truss", {
+mcl_paintings.register_painting("mcl_paintings:support_truss", {
 	texture = "mcl_paintings_painting_support_truss.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("froggy pond", {
+mcl_paintings.register_painting("mcl_paintings:froggy_pond", {
 	texture = "mcl_paintings_painting_froggy_pond.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("moonshine thundra", {
+mcl_paintings.register_painting("mcl_paintings:moonshine_thundra", {
 	texture = "mcl_paintings_painting_moonshine_thundra.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 3,
 })
 
-mcl_paintings.register_painting("desert castle", {
+mcl_paintings.register_painting("mcl_paintings:desert_castle", {
 	texture = "mcl_paintings_painting_desert_castle.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 4,
 })
 
-mcl_paintings.register_painting("sarmatian decoration", {
+mcl_paintings.register_painting("mcl_paintings:sarmatian_decoration", {
 	texture = "mcl_paintings_painting_sarmatian_decoration.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 5,
 })
 
-mcl_paintings.register_painting("decorative swords", {
+mcl_paintings.register_painting("mcl_paintings:decorative_swords", {
 	texture = "mcl_paintings_painting_decorative_swords.png",
 	width = 2,
 	height = 2,
 	legacy_motive = 6,
 })
 
-mcl_paintings.register_painting("gloom gloom mountain", {
+mcl_paintings.register_painting("mcl_paintings:gloom_gloom_mountain", {
 	texture = "mcl_paintings_painting_gloom_mountain.png",
 	width = 4,
 	height = 3,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("elf utopia", {
+mcl_paintings.register_painting("mcl_paintings:elf_utopia", {
 	texture = "mcl_paintings_painting_elf_utopia.png",
 	width = 4,
 	height = 3,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("waterfall bridge", {
+mcl_paintings.register_painting("mcl_paintings:waterfall_bridge", {
 	texture = "mcl_paintings_painting_waterfall_bridge.png",
 	width = 4,
 	height = 4,
 	legacy_motive = 1,
 })
 
-mcl_paintings.register_painting("mountain tower", {
+mcl_paintings.register_painting("mcl_paintings:mountain_tower", {
 	texture = "mcl_paintings_painting_mountain_tower.png",
 	width = 4,
 	height = 4,
 	legacy_motive = 2,
 })
 
-mcl_paintings.register_painting("volendam costume", {
+mcl_paintings.register_painting("mcl_paintings:volendam_costume", {
 	texture = "mcl_paintings_painting_volendam_costume.png",
 	width = 4,
 	height = 4,
 	legacy_motive = 3,
 })
+
+mcl_paintings.register_painting_alias("ancient octupus", "mcl_paintings:ancient_octupus")
+mcl_paintings.register_painting_alias("snowy mountain", "mcl_paintings:snowy_mountain")
+mcl_paintings.register_painting_alias("balding man", "mcl_paintings:balding_man")
+mcl_paintings.register_painting_alias("poster", "mcl_paintings:poster")
+mcl_paintings.register_painting_alias("notes", "mcl_paintings:notes")
+mcl_paintings.register_painting_alias("viking shield", "mcl_paintings:viking_shield")
+mcl_paintings.register_painting_alias("butcher knifes", "mcl_paintings:butcher_knifes")
+mcl_paintings.register_painting_alias("green bottles", "mcl_paintings:green_bottles")
+mcl_paintings.register_painting_alias("battle axe", "mcl_paintings:battle_axe")
+mcl_paintings.register_painting_alias("cooking utencils", "mcl_paintings:cooking_utencils")
+mcl_paintings.register_painting_alias("dense jungle forest", "mcl_paintings:dense_jungle_forest")
+mcl_paintings.register_painting_alias("endless dunes", "mcl_paintings:endless_dunes")
+mcl_paintings.register_painting_alias("green banner", "mcl_paintings:green_banner")
+mcl_paintings.register_painting_alias("blue banner", "mcl_paintings:blue_banner")
+mcl_paintings.register_painting_alias("quest board", "mcl_paintings:quest_board")
+mcl_paintings.register_painting_alias("support truss", "mcl_paintings:support_truss")
+mcl_paintings.register_painting_alias("froggy pond", "mcl_paintings:froggy_pond")
+mcl_paintings.register_painting_alias("moonshine thundra", "mcl_paintings:moonshine_thundra")
+mcl_paintings.register_painting_alias("desert castle", "mcl_paintings:desert_castle")
+mcl_paintings.register_painting_alias("sarmatian decoration", "mcl_paintings:sarmatian_decoration")
+mcl_paintings.register_painting_alias("decorative swords", "mcl_paintings:decorative_swords")
+mcl_paintings.register_painting_alias("gloom gloom mountain", "mcl_paintings:gloom_gloom_mountain")
+mcl_paintings.register_painting_alias("elf utopia", "mcl_paintings:elf_utopia")
+mcl_paintings.register_painting_alias("waterfall bridge", "mcl_paintings:waterfall_bridge")
+mcl_paintings.register_painting_alias("mountain tower", "mcl_paintings:mountain_tower")
+mcl_paintings.register_painting_alias("volendam costume", "mcl_paintings:volendam_costume")
