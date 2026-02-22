78ed11d82cc8cf19369aa7fe7f45c3c49658732e
Jürgen Rühle <j-r@online.de>
2026-02-22 11:55:55 +0100

    Close sign formspec if sign is destroyed
    Closing range is limited by creative hand range or player transfer distance
    (whichever is smaller).

--- mods/ITEMS/mcl_signs/init.lua
+++ mods/ITEMS/mcl_signs/init.lua
@@ -337,6 +337,7 @@ end
 
 function sign_tpl.on_destruct(pos)
 	mcl_signs.get_text_entity (pos, true)
+	mcl_signs.close_formspec (pos)
 end
 
 function sign_tpl._on_dye_place(pos,color)
@@ -461,6 +462,18 @@ function mcl_signs.show_formspec(player, pos)
 		F(S("Maximum lines: 4")) ..
 		"]button_exit[0,2.5;6,1;submit;" .. F(S("Done")) .. "]"
 	)
+end
+
+local unlimited_player_transfer_distance = core.settings:get_bool("unlimited_player_transfer_distance", true)
+local player_transfer_distance = unlimited_player_transfer_distance and 0 or tonumber(core.settings:get("player_transfer_distance")) or 0
+local mcl_hand_range_creative = tonumber(core.settings:get("mcl_hand_range_creative")) or 10
+local max_close_formspec_range = math.min(mcl_hand_range_creative + 1, player_transfer_distance > 0 and player_transfer_distance or math.huge)
+
+function mcl_signs.close_formspec(pos)
+	for player in mcl_util.connected_players(pos, max_close_formspec_range) do
+		core.close_formspec(player:get_player_name(),
+			"mcl_signs:set_text_" .. pos.x .. "_" .. pos.y .. "_" .. pos.z)
+	end
 end
 
 core.register_on_player_receive_fields(function(player, formname, fields)
