1edb71c1b507d51e787b4e7b666659d0655cc943
cora <coradelamouche@gmx.ch>
2026-03-06 00:32:27 +0100

    Fix indentation in validate-utf8 function and add check for string type

--- mods/CORE/mcl_util/misc.lua
+++ mods/CORE/mcl_util/misc.lua
@@ -249,21 +249,22 @@ function mcl_util.validate_utf8(str)
 	--function adapted from https://github.com/kikito/utf8_validator.lua/blob/master/utf8_validator.lua (Orginally MIT License)
 	-- Numbers taken from table 3-7 in www.unicode.org/versions/Unicode6.2.0/UnicodeStandard-6.2.pdf
 	-- find-based solution inspired by http://notebook.kulchenko.com/programming/fixing-malformed-utf8-in-lua
-  local i, len = 1, #str
-  while i <= len do
-    if     i == string.find(str, "[%z\1-\127]", i) then i = i + 1
-    elseif i == string.find(str, "[\194-\223][\128-\191]", i) then i = i + 2
-    elseif i == string.find(str,        "\224[\160-\191][\128-\191]", i)
-        or i == string.find(str, "[\225-\236][\128-\191][\128-\191]", i)
-        or i == string.find(str,        "\237[\128-\159][\128-\191]", i)
-        or i == string.find(str, "[\238-\239][\128-\191][\128-\191]", i) then i = i + 3
-    elseif i == string.find(str,        "\240[\144-\191][\128-\191][\128-\191]", i)
-        or i == string.find(str, "[\241-\243][\128-\191][\128-\191][\128-\191]", i)
-        or i == string.find(str,        "\244[\128-\143][\128-\191][\128-\191]", i) then i = i + 4
-    else
-      return false, i
-    end
-  end
-
-  return true
+	if type(str) == "string" then
+		local i, len = 1, #str
+		while i <= len do
+			if     i == string.find(str, "[%z\1-\127]", i) then i = i + 1
+			elseif i == string.find(str, "[\194-\223][\128-\191]", i) then i = i + 2
+			elseif i == string.find(str,        "\224[\160-\191][\128-\191]", i)
+				or i == string.find(str, "[\225-\236][\128-\191][\128-\191]", i)
+				or i == string.find(str,        "\237[\128-\159][\128-\191]", i)
+				or i == string.find(str, "[\238-\239][\128-\191][\128-\191]", i) then i = i + 3
+			elseif i == string.find(str,        "\240[\144-\191][\128-\191][\128-\191]", i)
+				or i == string.find(str, "[\241-\243][\128-\191][\128-\191][\128-\191]", i)
+				or i == string.find(str,        "\244[\128-\143][\128-\191][\128-\191]", i) then i = i + 4
+			else
+			  return false, i
+			end
+		end
+	end
+	return true
 end
