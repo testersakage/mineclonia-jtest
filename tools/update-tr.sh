54aa8c2bc57d7a42bec31bf29a744cc359e10144
cora <coradelamouche@gmx.ch>
2026-04-01 21:42:58 +0200

    Remove invalid plural lines from po files

--- tools/update-tr.sh
+++ tools/update-tr.sh
@@ -3,5 +3,4 @@
 # assumes to be started in mineclonia top level directory and that Luanti's mod
 # tools and Wuzzy's translation tools are in PATH
 
-tools/generate_translation_strings/generate.sh mod_translation_updater.py
-python tools/update_locale_files.py
+tools/generate_translation_strings/generate.sh tools/update_locale_files.py
